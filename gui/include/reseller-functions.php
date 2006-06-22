2f8e850897a97fde48390000fc6d888defed48d1
malte <malte@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2006-06-22 10:37:45 +0000

    "updated function reseller_limits_check
    "
    
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@114 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/reseller-functions.php
+++ gui/include/reseller-functions.php
@@ -1414,10 +1414,18 @@ SQL_QUERY;
   		//we want to check _before_ inserting
   		
   		if (empty($newprops)) {
-  			
+  				
+  				//try to use them from our session
+  				if (isset($_SESSION["ch_hpprops"])) {
+
+  					$props = $_SESSION["ch_hpprops"];
+  				
+  				} else {
+  				
 		        $err_msg = tr('no Valid Hostingplan Limits :'.$newprops.' submitted');
                 return;	
-  		
+  				
+  				}
 		} else {
   			
   			$props = $newprops;	
