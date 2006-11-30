c700e08bf38792787a8c98545cc2e1e9afaf45b3
shin <shin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2006-11-30 21:26:13 +0000

    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@199 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/admin/rootkit_log.php
+++ gui/admin/rootkit_log.php
@@ -1,11 +1,11 @@
 <?php
 //   -------------------------------------------------------------------------------
-//  |             VHCS(tm) - Virtual Hosting Control System                         |
-//  |              Copyright (c) 2001-2006 by moleSoftware		            		|
-//  |			http://vhcs.net | http://www.molesoftware.com		           		|
+//  |          VHCS ? (OMEGA) - Virtual Hosting Control System | Omega Version	    |
+//  |             		 Copyright (c) 2006 by ispCP     	                  		|
+//  |	   http://isp-control.net | http://www.serverozzo-project.com	   	    	|
 //  |                                                                               |
 //  | This program is free software; you can redistribute it and/or                 |
-//  | modify it under the terms of the MPL General Public License                   |
+//  | modify it under the terms of the GPL General Public License                   |
 //  | as published by the Free Software Foundation; either version 1.1              |
 //  | of the License, or (at your option) any later version.                        |
 //  |                                                                               |
