0f8cf1ae0977c4e24222b13465b92bdf2f7d5dfd
malte <malte@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2007-04-11 09:07:31 +0000

    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@478 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- docs/contrib/mysql_fix.php
+++ docs/contrib/mysql_fix.php
@@ -2,9 +2,9 @@
 
 /*
 *
-* MySQl Fix fuer VHCS 2.4.7.1 by Marcus 'crazyiven' Jaentsch
+* MySQl Fix fuer ISPCP 2.4.7.1 by Marcus 'crazyiven' Jaentsch
 *
-* Es wurde bei VHCS dazu uebergegangen statt nur einen User mit Host "localhost" auch einen mit Host "%" zu erstellen.
+* Es wurde bei ISPCP dazu uebergegangen statt nur einen User mit Host "localhost" auch einen mit Host "%" zu erstellen.
 * Leider gibt es dadurch Probleme mit Usern die VORHER erstellt wurden, weil der User mit dem Host "%" fehlt. Dieser
 * kleine Fix liest diese alten User aus der MySQL DB aus und erstellt einen 1:1 gleichen User mit Host "%" und den
 * entsprechenden Rechten
@@ -14,8 +14,8 @@
 *
 * Anleitung:
 *
-* 1) mysql_fix.php hochladen und in /var/www/vhcs2/gui ablegen (Wenn Du die GUI Adresse veraendert hast, dann dorthin!)
-* 2) Die PHP Datei einmal ausfuehren indem Du sie mit dem Browser aufrufst (http://www.Deine-VHCSAdresse.de/mysql_fix.php)
+* 1) mysql_fix.php hochladen und in /var/www/ispcp/gui ablegen (Wenn Du die GUI Adresse veraendert hast, dann dorthin!)
+* 2) Die PHP Datei einmal ausfuehren indem Du sie mit dem Browser aufrufst (http://www.Deine-ISPCPAdresse.de/mysql_fix.php)
 * 3) Die PHP Datei wieder löschen!    
 *
 *
@@ -23,9 +23,9 @@
 */
 
 /* MySQL Verbindungsdaten laden */
-include("include/vhcs2-db-keys.php");
-include("include/vhcs-config.php");
-$cfg_obj = new Config("/etc/vhcs2/vhcs2.conf");
+include("include/ispcp-db-keys.php");
+include("include/ispcp-config.php");
+$cfg_obj = new Config("/etc/ispcp/ispcp.conf");
 
 if ($cfg_obj->status == "err") {
 
