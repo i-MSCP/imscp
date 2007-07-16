f5bbb9464d5d7120448c509f5fa69a9f6f2cb260
rats <rats@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2007-07-16 22:24:12 +0000

    * Fixed serveral errors.
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@708 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/client/edit_alias.php
+++ gui/client/edit_alias.php
@@ -50,7 +50,7 @@ $tpl->assign(
 				'TR_ALIAS_NAME' => tr('Alias name'),
 				'TR_DOMAIN_IP' => tr('Domain IP'),
 				'TR_FORWARD' => tr('Forward to URL'),
-				'TR_Mount_POINT' => tr('Mount Point'),
+				'TR_MOUNT_POINT' => tr('Mount Point'),
 				'TR_MODIFY' => tr('Modify'),
 				'TR_CANCEL' => tr('Cancel'),
 				'TR_ENABLE_FWD' => tr("Enable Forward"),
