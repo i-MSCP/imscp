47258f4860577daf2818e8834af48ff66a6ae6d3
raphael <raphael@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2007-07-18 22:42:47 +0000

    Fixed #397: Running ispcp-setup in an already installed system duplicates the cronjobs
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@714 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- engine/setup/copy-errordocs.sh
+++ engine/setup/copy-errordocs.sh
@@ -6,15 +6,15 @@ export $a
 done
 
 
-chown ${APACHE_USER}:${APACHE_GROUP} ${GUI_ROOT_DIR}/errordocs/index.php
-chmod 400 ${GUI_ROOT_DIR}/errordocs/index.php
+chown ${APACHE_USER}:${APACHE_GROUP} ${GUI_ROOT_DIR}/errordocs/*.html
+chmod 400 ${GUI_ROOT_DIR}/errordocs/*.html
 
 for domain in `ls -1A ${APACHE_WWW_DIR}`; do
 	if [ -d "${APACHE_WWW_DIR}/${domain}" ] ; then
 		echo "${domain}"
-		cp -p ${GUI_ROOT_DIR}/errordocs/index.php ${APACHE_WWW_DIR}/${domain}/errors/404/
-		cp -p ${GUI_ROOT_DIR}/errordocs/index.php ${APACHE_WWW_DIR}/${domain}/errors/403/
-		cp -p ${GUI_ROOT_DIR}/errordocs/index.php ${APACHE_WWW_DIR}/${domain}/errors/401/
-		cp -p ${GUI_ROOT_DIR}/errordocs/index.php ${APACHE_WWW_DIR}/${domain}/errors/500/
+		cp -p ${GUI_ROOT_DIR}/errordocs/404.html ${APACHE_WWW_DIR}/${domain}/errors/
+		cp -p ${GUI_ROOT_DIR}/errordocs/403.html ${APACHE_WWW_DIR}/${domain}/errors/
+		cp -p ${GUI_ROOT_DIR}/errordocs/401.html ${APACHE_WWW_DIR}/${domain}/errors/
+		cp -p ${GUI_ROOT_DIR}/errordocs/500.html ${APACHE_WWW_DIR}/${domain}/errors/
 	fi
 done
