e989dc69d0e8876dc2053887ba012013de6ea323
kilburn <kilburn@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2008-05-19 17:14:00 +0000

    * Fixed #1254: Added missing files from previous commit
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1160 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ gui/admin/edit_domain.php
@@ -0,0 +1,606 @@
+<?php
+/**
+ * ispCP ω (OMEGA) a Virtual Hosting Control System
+ *
+ * @copyright 	2001-2006 by moleSoftware GmbH
+ * @copyright 	2006-2008 by ispCP | http://isp-control.net
+ * @version 	SVN: $ID$
+ * @link 		http://isp-control.net
+ * @author 		ispCP Team
+ *
+ * @license
+ *   This program is free software; you can redistribute it and/or modify it under
+ *   the terms of the MPL General Public License as published by the Free Software
+ *   Foundation; either version 1.1 of the License, or (at your option) any later
+ *   version.
+ *   You should have received a copy of the MPL Mozilla Public License along with
+ *   this program; if not, write to the Open Source Initiative (OSI)
+ *   http://opensource.org | osi@opensource.org
+ */
+
+require '../include/ispcp-lib.php';
+
+check_login(__FILE__);
+
+$tpl = new pTemplate();
+$tpl->define_dynamic('page', $cfg['ADMIN_TEMPLATE_PATH'] . '/edit_domain.tpl');
+$tpl->define_dynamic('page_message', 'page');
+$tpl->define_dynamic('ip_entry', 'page');
+
+$theme_color = $cfg['USER_INITIAL_THEME'];
+
+if (isset($cfg['HOSTING_PLANS_LEVEL']) && strtolower($cfg['HOSTING_PLANS_LEVEL']) !== 'admin') {
+	header("Location: manage_users.php");
+	die();
+}
+
+$tpl->assign(
+			array(
+				'TR_EDIT_DOMAIN_PAGE_TITLE' => tr('ispCP - Admin/Edit Domain'),
+				'THEME_COLOR_PATH' => "../themes/$theme_color",
+				'THEME_CHARSET' => tr('encoding'),
+				'ISP_LOGO' => get_logo($_SESSION['user_id'])
+			)
+		);
+
+/*
+ *
+ * static page messages.
+ *
+ */
+$tpl->assign(
+			array(
+				'TR_EDIT_DOMAIN' => tr('Edit Domain'),
+				'TR_DOMAIN_PROPERTIES' => tr('Domain properties'),
+				'TR_DOMAIN_NAME' => tr('Domain name'),
+				'TR_DOMAIN_IP' => tr('Domain IP'),
+				'TR_PHP_SUPP' => tr('PHP support'),
+				'TR_CGI_SUPP' => tr('CGI support'),
+				'TR_SUBDOMAINS' => tr('Max subdomains<br /><i>(-1 disabled, 0 unlimited)</i>'),
+				'TR_ALIAS' => tr('Max aliases<br /><i>(-1 disabled, 0 unlimited)</i>'),
+				'TR_MAIL_ACCOUNT' => tr('Mail accounts limit <br /><i>(-1 disabled, 0 unlimited)</i>'),
+				'TR_FTP_ACCOUNTS' => tr('FTP accounts limit <br /><i>(-1 disabled, 0 unlimited)</i>'),
+				'TR_SQL_DB' => tr('SQL databases limit <br /><i>(-1 disabled, 0 unlimited)</i>'),
+				'TR_SQL_USERS' => tr('SQL users limit <br /><i>(-1 disabled, 0 unlimited)</i>'),
+				'TR_TRAFFIC' => tr('Traffic limit [MB] <br /><i>(0 unlimited)</i>'),
+				'TR_DISK' => tr('Disk limit [MB] <br /><i>(0 unlimited)</i>'),
+				'TR_USER_NAME' => tr('Username'),
+				'TR_UPDATE_DATA' => tr('Submit changes'),
+				'TR_CANCEL' => tr('Cancel'),
+				'TR_YES' => tr('Yes'),
+				'TR_NO' => tr('No')
+			)
+		);
+
+gen_admin_mainmenu($tpl, $cfg['ADMIN_TEMPLATE_PATH'] . '/main_menu_manage_users.tpl');
+gen_admin_menu($tpl, $cfg['ADMIN_TEMPLATE_PATH'] . '/menu_manage_users.tpl');
+gen_page_message($tpl);
+
+if (isset($_POST['uaction']) && ('sub_data' === $_POST['uaction'])) {
+	// Process data
+	if (isset($_SESSION['edit_id'])) {
+		$editid = $_SESSION['edit_id'];
+	} else {
+		unset($_SESSION['edit_id']);
+		$_SESSION['edit'] = '_no_';
+
+		header('Location: manage_users.php');
+		die();
+	}
+
+	if (check_user_data($tpl, $sql, get_reseller_id($editid), $editid)) { // Save data to db
+		$_SESSION['dedit'] = "_yes_";
+		header("Location: manage_users.php");
+		die();
+	}
+	load_additional_data(get_reseller_id($editid), $editid);
+} else {
+	// Get user id that come for edit
+	if (isset($_GET['edit_id'])) {
+		$editid = $_GET['edit_id'];
+	}
+
+	load_user_data(get_reseller_id($editid), $editid);
+	// $_SESSION['edit_ID'] = $editid;
+	$_SESSION['edit_id'] = $editid;
+	$tpl->assign('MESSAGE', "");
+}
+
+gen_editdomain_page($tpl);
+
+// Begin function block
+
+// Get the reseller id of a domain
+function get_reseller_id($domain_id) {
+	global $sql;
+
+	$query = <<<SQL_QUERY
+	SELECT
+		a.created_by
+	FROM 
+		domain d, admin a
+	WHERE 
+		d.domain_id = ? 
+	    AND d.domain_admin_id = a.admin_id
+SQL_QUERY;
+
+	$rs    = exec_query($sql, $query, array($domain_id));
+
+	if ($rs->RecordCount() == 0) {
+		set_page_message(tr('User does not exist or you do not have permission to access this interface!'));
+		header('Location: manage_users.php');
+		die();
+	}
+
+	$data = $rs->FetchRow();
+	return $data['created_by'];
+}
+
+// Load data from sql
+function load_user_data($user_id, $domain_id) {
+	global $sql;
+
+	global $domain_name, $domain_ip, $php_sup;
+	global $cgi_supp , $sub, $als;
+	global $mail, $ftp, $sql_db;
+	global $sql_user, $traff, $disk;
+	global $username;
+
+	$query = <<<SQL_QUERY
+        SELECT
+            domain_id
+        FROM
+            domain
+        WHERE
+            domain_id = ?
+SQL_QUERY;
+
+	$rs = exec_query($sql, $query, array($domain_id));
+
+	if ($rs->RecordCount() == 0) {
+		set_page_message(tr('User does not exist or you do not have permission to access this interface!'));
+		header('Location: manage_users.php');
+		die();
+	}
+
+	list ($a, $sub,
+		$b, $als,
+		$c, $mail,
+		$d, $ftp,
+		$e, $sql_db,
+		$f, $sql_user,
+		$traff, $disk
+		) = generate_user_props($domain_id);;
+
+	load_additional_data($user_id, $domain_id);
+} //End of load_user_data()
+
+// Load additional data
+function load_additional_data($user_id, $domain_id) {
+	global $sql;
+	global $domain_name, $domain_ip, $php_sup;
+	global $cgi_supp, $username;
+	// Get domain data
+	$query = <<<SQL_QUERY
+        SELECT
+            domain_name,
+			domain_ip_id,
+			domain_php,
+			domain_cgi,
+			domain_admin_id
+        FROM
+            domain
+        WHERE
+            domain_id = ?
+SQL_QUERY;
+
+	$res = exec_query($sql, $query, array($domain_id));
+	$data = $res->FetchRow();
+
+	$domain_name = $data['domain_name'];
+	$domain_ip_id = $data['domain_ip_id'];
+	$php_sup = $data['domain_php'];
+	$cgi_supp = $data['domain_cgi'];
+	$domain_admin_id = $data['domain_admin_id'];
+	// Get IP of domain
+	$query = <<<SQL_QUERY
+        SELECT
+            ip_number,
+			ip_domain
+        FROM
+            server_ips
+        WHERE
+            ip_id = ?
+SQL_QUERY;
+
+	$res = exec_query($sql, $query, array($domain_ip_id));
+	$data = $res->FetchRow();
+
+	$domain_ip = $data['ip_number'] . '&nbsp;(' . $data['ip_domain'] . ')';
+	// Get username of domain
+	$query = <<<SQL_QUERY
+        SELECT
+            admin_name
+        FROM
+            admin
+        WHERE
+            admin_id = ?
+        AND
+            admin_type = 'user'
+SQL_QUERY;
+
+	$res = exec_query($sql, $query, array($domain_admin_id));
+	$data = $res->FetchRow();
+
+	$username = $data['admin_name'];
+} //End of load_additional_data()
+
+// Show user data
+function gen_editdomain_page(&$tpl) {
+	global $domain_name, $domain_ip, $php_sup;
+	global $cgi_supp , $sub, $als;
+	global $mail, $ftp, $sql_db;
+	global $sql_user, $traff, $disk;
+	global $username;
+	// Fill in the fileds
+	$domain_name = decode_idna($domain_name);
+
+	$username = decode_idna($username);
+
+	generate_ip_list($tpl, $_SESSION['user_id']);
+
+	if ($php_sup === 'yes') {
+		$tpl->assign(
+				array(
+					'PHP_YES' => 'selected',
+					'PHP_NO' => '',
+				)
+			);
+	} else {
+		$tpl->assign(
+				array(
+					'PHP_YES' => '',
+					'PHP_NO' => 'selected',
+				)
+			);
+	}
+
+	if ($cgi_supp === 'yes') {
+		$tpl->assign(
+				array(
+					'CGI_YES' => 'selected',
+					'CGI_NO' => '',
+				)
+			);
+	} else {
+		$tpl->assign(
+				array(
+					'CGI_YES' => '',
+					'CGI_NO' => 'selected',
+				)
+			);
+	}
+
+	$tpl->assign(
+			array(
+				'VL_DOMAIN_NAME' => $domain_name,
+				'VL_DOMAIN_IP' => $domain_ip,
+				'VL_DOM_SUB' => $sub,
+				'VL_DOM_ALIAS' => $als,
+				'VL_DOM_MAIL_ACCOUNT' => $mail,
+				'VL_FTP_ACCOUNTS' => $ftp,
+				'VL_SQL_DB' => $sql_db,
+				'VL_SQL_USERS' => $sql_user,
+				'VL_TRAFFIC' => $traff,
+				'VL_DOM_DISK' => $disk,
+				'VL_USER_NAME' => $username
+			)
+		);
+} // End of gen_editdomain_page()
+
+// Check input data
+function check_user_data(&$tpl, &$sql, $reseller_id, $user_id) {
+	global $sub, $als, $mail, $ftp, $sql_db, $sql_user, $traff, $disk, $sql, $domain_ip, $domain_php, $domain_cgi;
+
+	$sub = clean_input($_POST['dom_sub']);
+	$als = clean_input($_POST['dom_alias']);
+	$mail = clean_input($_POST['dom_mail_acCount']);
+	$ftp = clean_input($_POST['dom_ftp_acCounts']);
+	$sql_db = clean_input($_POST['dom_sqldb']);
+	$sql_user = clean_input($_POST['dom_sql_users']);
+	$traff = clean_input($_POST['dom_traffic']);
+	$disk = clean_input($_POST['dom_disk']);
+	// $domain_ip = $_POST['domain_ip'];
+	$domain_php = $_POST['domain_php'];
+	$domain_cgi = $_POST['domain_cgi'];
+
+	$ed_error = '';
+
+	if (!ispcp_limit_check($sub, -1)) {
+		$ed_error .= tr('Incorrect subdomains limit!');
+	}
+	if (!ispcp_limit_check($als, -1)) {
+		$ed_error .= tr('Incorrect aliases limit!');
+	}
+	if (!ispcp_limit_check($mail, -1)) {
+		$ed_error .= tr('Incorrect mail accounts limit!');
+	}
+	if (!ispcp_limit_check($ftp, -1)) {
+		$ed_error .= tr('Incorrect FTP accounts limit!');
+	}
+	if (!ispcp_limit_check($sql_db, -1)) {
+		$ed_error .= tr('Incorrect SQL users limit!');
+	}
+	else if ($sql_db == -1 && $sql_user != -1) {
+		$ed_error .= tr('SQL databases limit is <i>disabled</i>!');
+	}
+	if (!ispcp_limit_check($sql_user, -1)) {
+		$ed_error .= tr('Incorrect SQL databases limit!');
+	}
+	else if ($sql_user == -1 && $sql_db != -1) {
+		$ed_error .= tr('SQL users limit is <i>disabled</i>!');
+	}
+	if (!ispcp_limit_check($traff, null)) {
+		$ed_error .= tr('Incorrect traffic limit!');
+	}
+	if (!ispcp_limit_check($disk, null)) {
+		$ed_error .= tr('Incorrect disk quota limit!');
+	}
+	
+	// $user_props = generate_user_props($user_id);
+	// $reseller_props = generate_reseller_props($reseller_id);
+	list ($usub_current, $usub_max,
+		$uals_current, $uals_max,
+		$umail_current, $umail_max,
+		$uftp_current, $uftp_max,
+		$usql_db_current, $usql_db_max,
+		$usql_user_current, $usql_user_max,
+		$utraff_max, $udisk_max) = generate_user_props($user_id);
+
+	$previous_utraff_max = $utraff_max;
+
+	list ($rdmn_current, $rdmn_max,
+		$rsub_current, $rsub_max,
+		$rals_current, $rals_max,
+		$rmail_current, $rmail_max,
+		$rftp_current, $rftp_max,
+		$rsql_db_current, $rsql_db_max,
+		$rsql_user_current, $rsql_user_max,
+		$rtraff_current, $rtraff_max,
+		$rdisk_current, $rdisk_max
+		) = get_reseller_default_props($sql, $reseller_id); //generate_reseller_props($reseller_id);
+	list ($a, $b, $c, $d, $e, $f, $utraff_current, $udisk_current, $i, $h) = generate_user_traffic($user_id);
+
+	if (empty($ed_error)) {
+		calculate_user_dvals($sub, $usub_current, $usub_max, $rsub_current, $rsub_max, $ed_error, tr('Subdomain'));
+		calculate_user_dvals($als, $uals_current, $uals_max, $rals_current, $rals_max, $ed_error, tr('Alias'));
+		calculate_user_dvals($mail, $umail_current, $umail_max, $rmail_current, $rmail_max, $ed_error, tr('Mail'));
+		calculate_user_dvals($ftp, $uftp_current, $uftp_max, $rftp_current, $rftp_max, $ed_error, tr('FTP'));
+		calculate_user_dvals($sql_db, $usql_db_current, $usql_db_max, $rsql_db_current, $rsql_db_max, $ed_error, tr('SQL Database'));
+	}
+
+	if (empty($ed_error)) {
+		$query = <<<SQL_QUERY
+			SELECT
+				COUNT(su.sqlu_id) AS cnt
+			FROM
+				sql_user AS su,
+				sql_database AS sd
+			WHERE
+				su.sqld_id = sd.sqld_id
+			AND
+				sd.domain_id = ?
+SQL_QUERY;
+
+		$rs = exec_query($sql, $query, array($_SESSION['edit_id']));
+		calculate_user_dvals($sql_user, $rs->fields['cnt'], $usql_user_max, $rsql_user_current, $rsql_user_max, $ed_error, tr('SQL User'));
+	}
+
+	if (empty($ed_error)) {
+		calculate_user_dvals($traff, $utraff_current / 1024 / 1024 , $utraff_max, $rtraff_current, $rtraff_max, $ed_error, tr('Traffic'));
+		calculate_user_dvals($disk, $udisk_current / 1024 / 1024, $udisk_max, $rdisk_current, $rdisk_max, $ed_error, tr('Disk'));
+	}
+
+	if (empty($ed_error)) {
+		// Set domain's status to 'change' to update mod_cband's limit
+		if ($previous_utraff_max != $utraff_max) {
+			$query = "UPDATE domain SET domain_status = 'change' WHERE domain_id = ?";
+			exec_query($sql, $query, array($user_id));
+			check_for_lock_file();
+			send_request();
+		}
+
+		$user_props = "$usub_current;$usub_max;";
+		$user_props .= "$uals_current;$uals_max;";
+		$user_props .= "$umail_current;$umail_max;";
+		$user_props .= "$uftp_current;$uftp_max;";
+		$user_props .= "$usql_db_current;$usql_db_max;";
+		$user_props .= "$usql_user_current;$usql_user_max;";
+		$user_props .= "$utraff_max;";
+		$user_props .= "$udisk_max;";
+		// $user_props .= "$domain_ip;";
+		$user_props .= "$domain_php;";
+		$user_props .= "$domain_cgi";
+		update_user_props($user_id, $user_props);
+
+		$reseller_props = "$rdmn_current;$rdmn_max;";
+		$reseller_props .= "$rsub_current;$rsub_max;";
+		$reseller_props .= "$rals_current;$rals_max;";
+		$reseller_props .= "$rmail_current;$rmail_max;";
+		$reseller_props .= "$rftp_current;$rftp_max;";
+		$reseller_props .= "$rsql_db_current;$rsql_db_max;";
+		$reseller_props .= "$rsql_user_current;$rsql_user_max;";
+		$reseller_props .= "$rtraff_current;$rtraff_max;";
+		$reseller_props .= "$rdisk_current;$rdisk_max";
+
+		if (!update_reseller_props($reseller_id, $reseller_props)) {
+
+		    set_page_message(tr('Domain properties could not be updated!'));
+
+		    return false;
+		}
+
+		// update the sql quotas too
+		$query = "SELECT domain_name FROM domain WHERE domain_id=?";
+		$rs = exec_query($sql, $query, array($user_id));
+		$temp_dmn_name = $rs->fields['domain_name'];
+
+		$query = "SELECT COUNT(name) AS cnt FROM quotalimits WHERE name=?";
+		$rs = exec_query($sql, $query, array($temp_dmn_name));
+		if ($rs->fields['cnt'] > 0) {
+			// we need to update it
+			if ($disk == 0) {
+				$dlim = 0;
+			} else {
+				$dlim = $disk * 1024 * 1024;
+			}
+
+			$query = "UPDATE quotalimits SET bytes_in_avail = ? WHERE name=?";
+			$rs = exec_query($sql, $query, array($dlim, $temp_dmn_name));
+		}
+
+		set_page_message(tr('Domain properties updated successfully!'));
+
+		return true;
+	} else {
+		$tpl->assign('MESSAGE', $ed_error);
+		$tpl->parse('PAGE_MESSAGE', 'page_message');
+
+		return false;
+	}
+} //End of check_user_data()
+
+function calculate_user_dvals($data, $u, &$umax, &$r, $rmax, &$err, $obj) {
+	if ($rmax == 0 && $umax == -1) {
+		if ($data == -1) {
+			return;
+		} else if ($data == 0) {
+			$umax = $data;
+			return;
+		} else if ($data > 0) {
+			$umax = $data;
+			$r += $umax;
+			return;
+		}
+	} else if ($rmax == 0 && $umax == 0) {
+		if ($data == -1) {
+			if ($u > 0) {
+				$err .= tr('The <em>%s</em> service can not be disabled! ', $obj) . tr('There are <em>%s</em> records on system!', $obj);
+			} else {
+				$umax = $data;
+			}
+
+			return;
+		} else if ($data == 0) {
+			return;
+		} else if ($data > 0) {
+			if ($u > $data) {
+				$err .= tr('The <em>%s</em> service can not be limited! ', $obj) . tr('Specified number is smaller than <em>%s</em> records, present on the system!', $obj);
+			} else {
+				$umax = $data;
+				$r += $umax;
+			}
+			return;
+		}
+	} else if ($rmax == 0 && $umax > 0) {
+		if ($data == -1) {
+			if ($u > 0) {
+				$err .= tr('The <em>%s</em> service can not be disabled! ', $obj) . tr('There are <em>%s</em> records on the system!', $obj);
+			} else {
+				$r -= $umax;
+				$umax = $data;
+			}
+			return;
+		} else if ($data == 0) {
+			$r -= $umax;
+			$umax = $data;
+			return;
+		} else if ($data > 0) {
+			if ($u > $data) {
+				$err .= tr('The <em>%s</em> service can not be limited! ', $obj) . tr('Specified number is smaller than <em>%s</em> records, present on the system!', $obj);
+			} else {
+				if ($umax > $data) {
+					$data_dec = $umax - $data;
+					$r -= $data_dec;
+				} else {
+					$data_inc = $data - $umax;
+					$r += $data_inc;
+				}
+				$umax = $data;
+			}
+			return;
+		}
+	} else if ($rmax > 0 && $umax == -1) {
+		if ($data == -1) {
+			return;
+		} else if ($data == 0) {
+			$err .= tr('The <em>%s</em> service can not be unlimited! ', $obj) . tr('There are reseller limits for the <em>%s</em> service!', $obj);
+			return;
+		} else if ($data > 0) {
+			if ($r + $data > $rmax) {
+				$err .= tr('The <em>%s</em> service can not be limited! ', $obj) . tr('You are exceeding reseller limits for the <em>%s</em> service!', $obj);
+			} else {
+				$r += $data;
+
+				$umax = $data;
+			}
+
+			return;
+		}
+	} else if ($rmax > 0 && $umax == 0) {
+		// We Can't Get Here! This clone is present only for sample purposes;
+		if ($data == -1) {
+			die("FIXME: ". __FILE__ .":". __LINE__);
+		} else if ($data == 0) {
+			die("FIXME: ". __FILE__ .":". __LINE__);
+		} else if ($data > 0) {
+			die("FIXME: ". __FILE__ .":". __LINE__);
+		}
+	} else if ($rmax > 0 && $umax > 0) {
+		if ($data == -1) {
+			if ($u > 0) {
+				$err .= tr('The <em>%s</em> service can not be disabled! ', $obj) . tr('There are <em>%s</em> records on the system!', $obj);
+			} else {
+				$r -= $umax;
+				$umax = $data;
+			}
+
+			return;
+		} else if ($data == 0) {
+			$err .= tr('The <em>%s</em> service can not be unlimited! ', $obj) . tr('There are reseller limits for the <em>%s</em> service!', $obj);
+
+			return;
+		} else if ($data > 0) {
+			if ($u > $data) {
+				$err .= tr('The <em>%s</em> service can not be limited! ', $obj) . tr('Specified number is smaller than <em>%s</em> records, present on the system!', $obj);
+			} else {
+				if ($umax > $data) {
+					$data_dec = $umax - $data;
+					$r -= $data_dec;
+				} else {
+					$data_inc = $data - $umax;
+
+					if ($r + $data_inc > $rmax) {
+						$err .= tr('The <em>%s</em> service can not be limited! ', $obj) . tr('You are exceeding reseller limits for the <em>%s</em> service!', $obj);
+						return;
+					}
+
+					$r += $data_inc;
+				}
+
+				$umax = $data;
+			}
+
+			return;
+		}
+	}
+} // End of calculate_user_dvals()
+
+$tpl->parse('PAGE', 'page');
+$tpl->prnt();
+
+if ($cfg['DUMP_GUI_DEBUG']) dump_gui_debug();
+
+unset_messages();
+
+?>
---
+++ gui/themes/omega_original/admin/menu_hp.tpl
@@ -0,0 +1,57 @@
+<table style="width:100%;height:100%;border-collapse: collapse;padding:0;margin:0;">
+ <tr style="height:53px;">
+  <td style="width:100%; background-image:url({THEME_COLOR_PATH}/images/top/menu_top.jpg);padding-left:5px;vertical-align:middle;">
+   <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+     <td><img src="{THEME_COLOR_PATH}/images/icons/hosting_plans_big.gif" width="32" height="33"></td>
+     <td><span class="titlemenu">{TR_MENU_HOSTING_PLANS}</span></td>
+    </tr>
+   </table>
+  </td>
+ </tr>
+ <tr height="*">
+				  <td style="width: 100%; background-image: url({THEME_COLOR_PATH}/images/top/menu_bg.jpg); vertical-align: top;">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><a href="hp.php" class="submenu">{TR_MENU_OVERVIEW}</a></td>
+  </tr>
+  <tr>
+    <td height="3" width="193"><img src="{THEME_COLOR_PATH}/images/top/menu_separator.jpg" height="3" width="193" /></td>
+  </tr>
+  <tr>
+    <td><a href="ahp.php" class="submenu">{TR_MENU_ADD_HOSTING}</a></td>
+  </tr>
+  <tr>
+    <td height="3" width="193"><img src="{THEME_COLOR_PATH}/images/top/menu_separator.jpg" height="3" width="193" /></td>
+  </tr>
+    <tr>
+    <td>&nbsp;</td>
+  </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td class="logoutbutton"><a href="../index.php?logout"><img src="{THEME_COLOR_PATH}/images/icons/logout_interface.png" border="0" height="18" width="18" /></a><a href="../index.php?logout" class="logout">{TR_MENU_LOGOUT}</a></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="center"><img src="{ISP_LOGO}" /></td>
+    </tr>
+   </table>
+  </td>
+ </tr>
+ <tr height="53" style="vertical-align: bottom;">
+  <td class="bottom" align="center">ispCP {VERSION}<br />build: {BUILDDATE}<br />{CODENAME}</td>
+ </tr>
+</table>
