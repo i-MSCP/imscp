c8f6756dce2c02d1fa44ad13d2af6f0a9ed47d29
scitech <scitech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2008-11-18 15:55:53 +0000

    small fixes
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1397 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/client/alias_delete.php
+++ gui/client/alias_delete.php
@@ -23,92 +23,111 @@ require '../include/ispcp-lib.php';
 check_login(__FILE__);
 
 if (isset($_GET['id']) && $_GET['id'] !== '') {
-  $als_id = $_GET['id'];
-  $dmn_id = get_user_domain_id($sql, $_SESSION['user_id']);
+	$als_id = $_GET['id'];
+	$dmn_id = get_user_domain_id($sql, $_SESSION['user_id']);
 
-  $query = <<<SQL_QUERY
-        select
-             alias_id
-             alias_name
-        from
-            domain_aliasses
-        where
-            domain_id = ?
-          and
-            alias_id = ?
-SQL_QUERY;
-
-  $rs = exec_query($sql, $query, array($dmn_id, $als_id));
-  $alias_name = $rs->fields['alias_name'];
-
-  if ($rs -> RecordCount() == 0) {
-    user_goto('domains_manage.php');
-  }
-
-  // check for mail accounts
-  $query = <<<SQL_QUERY
-		select
-			count(mail_id) as cnt
-		from
-			mail_users
+	$query = "
+		SELECT
+			`alias_id`
+			`alias_name`
+		FROM
+			`domain_aliasses`
 		WHERE
-			sub_id = ?
-SQL_QUERY;
+			`domain_id` = ?
+		AND
+			`alias_id` = ?
+	";
 
-  $rs = exec_query($sql, $query, array($als_id));
+	$rs = exec_query($sql, $query, array($dmn_id, $als_id));
+	$alias_name = $rs->fields['alias_name'];
 
-  if ($rs -> fields['cnt'] > 0 ) {
-    set_page_message(tr('Domain alias you are trying to remove has email accounts !<br>First remove them!'));
-    header('Location: domains_manage.php');
-    exit(0);
-  }
+	if ($rs -> RecordCount() == 0) {
+		user_goto('domains_manage.php');
+	}
 
-  // check for ftp accounts
-  $query = <<<SQL_QUERY
-  		select
-		  	count(fg.gid) as ftpnum
-		from
-			ftp_group fg,
-            domain dmn,
-			domain_aliasses d
-		where
-			d.alias_id = ?
-		and
-			fg.groupname = dmn.domain_name
-		and
-			fg.members RLIKE d.alias_name
-        and
-            d.domain_id = dmn.domain_id
-SQL_QUERY;
+	// check for subdomains
+	$query = "
+		SELECT
+			count(`subdomain_alias_id`) as `count`
+		FROM
+			`subdomain_alias`
+		WHERE
+			`alias_id` = ?
+	";
 
-  $rs = exec_query($sql, $query, array($als_id));
-  if ($rs -> fields['ftpnum'] > 0 ) {
-    set_page_message(tr('Domain alias you are trying to remove has FTP accounts!<br>First remove them!'));
-    header('Location: domains_manage.php');
-    exit(0);
-  }
+	$rs = exec_query($sql, $query, array($als_id));
+	if ($rs -> fields['count'] > 0 ) {
+		set_page_message(tr('Domain alias you are trying to remove has subdomains!<br>First remove them!'));
+		header('Location: domains_manage.php');
+		exit(0);
+	}
 
-  check_for_lock_file();
+	// check for mail accounts
+	$query = "
+		SELECT
+			count(`mail_id`) as `cnt`
+		FROM
+			`mail_users`
+		WHERE
+			`sub_id` = ?
+		AND
+			`mail_type` LIKE '%alias_%'
+	";
 
-  $query = <<<SQL_QUERY
-        update
-            domain_aliasses
-        set
-            alias_status = 'delete'
-        where
-            alias_id = ?
-SQL_QUERY;
+	$rs = exec_query($sql, $query, array($als_id));
 
-  $rs = exec_query($sql, $query, array($als_id));
+	if ($rs -> fields['cnt'] > 0 ) {
+		set_page_message(tr('Domain alias you are trying to remove has email accounts !<br>First remove them!'));
+		header('Location: domains_manage.php');
+		exit(0);
+	}
 
-  send_request();
-  write_log($_SESSION['user_logged'].": delete alias ".$alias_name."!");
-  set_page_message(tr('Alias scheduled for deletion!'));
-  header('Location: domains_manage.php');
-  exit(0);
+	// check for ftp accounts
+	$query = "
+		SELECT
+			count(`fg`.`gid`) as `ftpnum`
+		FROM
+			`ftp_group` `fg`,
+			`domain` `dmn`,
+			`domain_aliasses` `d`
+		WHERE
+			`d`.`alias_id` = ?
+		AND
+			`fg`.`groupname` = `dmn`.`domain_name`
+		AND
+			`fg`.`members` RLIKE `d`.`alias_name`
+		AND
+			`d`.`domain_id` = `dmn`.`domain_id`
+	";
+
+	$rs = exec_query($sql, $query, array($als_id));
+	if ($rs -> fields['ftpnum'] > 0 ) {
+		set_page_message(tr('Domain alias you are trying to remove has FTP accounts!<br>First remove them!'));
+		header('Location: domains_manage.php');
+		exit(0);
+	}
+
+	check_for_lock_file();
+
+	$query = "
+		UPDATE
+			`domain_aliasses`
+		SET
+			`alias_status` = 'delete'
+		WHERE
+			`alias_id` = ?
+	";
+
+	$rs = exec_query($sql, $query, array($als_id));
+
+	send_request();
+	write_log($_SESSION['user_logged'].": delete alias ".$alias_name."!");
+	set_page_message(tr('Alias scheduled for deletion!'));
+	header('Location: domains_manage.php');
+	exit(0);
 } else {
-  header('Location: domains_manage.php');
-  exit(0);
+	header('Location: domains_manage.php');
+	exit(0);
 }
 
 ?>
