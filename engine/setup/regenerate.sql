486e3ba45ac90c89fc662cdea0318fcb8f14de6d
scitech <scitech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2009-02-12 20:05:38 +0000

    Updated update script
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1485 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- engine/setup/regenerate.sql
+++ engine/setup/regenerate.sql
@@ -11,6 +11,9 @@ UPDATE `domain_aliasses` SET `alias_status` = 'change' WHERE `alias_status` = 'o
 UPDATE `subdomain` SET `subdomain_status` = 'change' WHERE `subdomain_status` = 'ok';
 UPDATE `subdomain_alias` SET `subdomain_alias_status` = 'change' WHERE `subdomain_alias_status` = 'ok';
 UPDATE `mail_users` SET `status` = 'change' WHERE `status` = 'ok';
+UPDATE htaccess SET status = 'change' WHERE `status` = 'ok';
+UPDATE htaccess_groups SET status = 'change' WHERE `status` = 'ok';
+UPDATE htaccess_users SET status = 'change' WHERE `status` = 'ok';
 -- END: Regenerate config files
 
 COMMIT;
