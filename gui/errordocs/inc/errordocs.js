ecac36316a10cbadc1529bae9fb36c47dde759d4
haeber <haeber@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2009-02-21 02:23:00 +0000

    optimized CSS and JS files (they don't need the hugh license text - costs to much traffic, long loading) + coding style
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1531 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/errordocs/inc/errordocs.js
+++ gui/errordocs/inc/errordocs.js
@@ -1,8 +1,8 @@
 
 function get_path() {
-    if (document.location.pathname != undefined) {
-        return document.location.pathname.replace( /[<]/g, "&lt;").replace(/[>]/g, "&gt;");
-    } else {
-        return "&nbsp;";
-    }
+	if (document.location.pathname != undefined) {
+		return document.location.pathname.replace( /[<]/g, "&lt;").replace(/[>]/g, "&gt;");
+	} else {
+		return "&nbsp;";
+	}
 }
--- gui/themes/omega_original/css/tooltip.js
+++ gui/themes/omega_original/css/tooltip.js
@@ -1,26 +1,4 @@
-/**
-* ispCP ω (OMEGA) a Virtual Hosting Control System
-*
-* @copyright	2006-2008 by ispCP | http://isp-control.net
-* @link http://isp-control.net
-* @author ispCP Team (2007)
-* @license
-*    This program is free software; you can redistribute it and/or
-*    modify it under the terms of the GPL General Public License
-*    as published by the Free Software Foundation; either version 2.0
-*    of the License, or (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GPL General Public License for more details.
-*
-*    You may have received a copy of the GPL General Public License
-*    along with this program.
-*
-*    An on-line copy of the GPL General Public License can be found
-*    http://www.fsf.org/licensing/licenses/gpl.txt
-*/
+
 function showTip(id, e) {
 	x = (document.all) ? window.event.x + document.body.scrollLeft : e.pageX;
 	y = (document.all) ? window.event.y + document.body.scrollTop  : e.pageY;
