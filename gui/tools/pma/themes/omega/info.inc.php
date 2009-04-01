4c2620147c050f9cbf39c2fde0fcb36e6ee7d5ce
haeber <haeber@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2009-04-01 21:41:26 +0000

    * preparation for svn externals of gui/tools
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1634 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/tools/pma/themes/omega/info.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Theme information */
-$theme_name = 'omega';
-$theme_version = 2;
-$theme_generation = 2;
-?>
--- gui/tools/webmail/plugins/bookmarks/bookmarks_data.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// This file purposely left empty.  Refer to the installation instructions
-// in the README file for more information
-
-require("bookmarks_data_text.php");
-
-?>
