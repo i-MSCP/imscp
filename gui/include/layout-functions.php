c68603ebea4abb2037035ddd9544476dc9c0b58a
haeber <haeber@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2009-04-08 20:43:57 +0000

    * sorry: fixed two more regressions
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1643 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/layout-functions.php
+++ gui/include/layout-functions.php
@@ -20,11 +20,10 @@
 if (isset($_SESSION['user_id'])
 	&& !isset($_SESSION['logged_from'])
 	&& !isset($_SESSION['logged_from_id'])) {
-		list($user_def_lang, $user_def_layout) = get_user_gui_props($sql, $_SESSION['user_id']);
+	list($user_def_lang, $user_def_layout) = get_user_gui_props($sql, $_SESSION['user_id']);
 
-		$_SESSION['user_theme'] = $user_def_layout;
-		$_SESSION['user_def_lang'] = $user_def_lang;
-	}
+	$_SESSION['user_theme'] = $user_def_layout;
+	$_SESSION['user_def_lang'] = $user_def_lang;
 }
 
 // THEME_COLOR management stuff.
