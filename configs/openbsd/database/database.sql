73d07bbc9b6ccf8b718a91586268ed42c1a2e0a4
scitech <scitech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2009-08-30 11:38:42 +0000

    Update for ispcp-update
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@1954 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- configs/openbsd/database/database.sql
+++ configs/openbsd/database/database.sql
@@ -91,7 +91,7 @@ INSERT INTO `config` (`name`, `value`) VALUES
 ('PREVENT_EXTERNAL_LOGIN_RESELLER', '1'),
 ('PREVENT_EXTERNAL_LOGIN_CLIENT', '1'),
 ('DATABASE_REVISION', '22'),
-('CRITICAL_UPDATE_REVISION', '2'),
+('CRITICAL_UPDATE_REVISION', '3'),
 
 -- --------------------------------------------------------
 
