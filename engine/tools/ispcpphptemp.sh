d304bacc13e22c66b02d53401579bd909e39cf54
kilburn <kilburn@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2009-10-22 10:06:55 +0000

    Fixed #2046 - ispcpphptemp.sh script fails to read per-domain session.gc_maxlifetime directives
    I've also taken the opportunity to remove some code duplication, improve cross-platform (BSD) compatibility and make some minor optimizations.
    
    
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@2108 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- engine/tools/ispcpphptemp.sh
+++ engine/tools/ispcpphptemp.sh
@@ -1,68 +1,64 @@
 #!/bin/bash
+#
+# ispCP ω (OMEGA) a Virtual Hosting Control Panel
+# Copyright (c) 2006-2009 by isp Control Panel
+# http://isp-control.net
+#
+#
+# License:
+#	This program is free software;  you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You may have received a copy of theGNU General Public License
+#	along with this program; if not, write to the
+#	Free Software Foundation, Inc.,
+#	59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# The ispCP ω Home Page is at:
+#
+#	http://isp-control.net
+#
+# @version $Id$
 
-# read needed entries from ispcp.conf
+# Load the required entries from ispcp's configuration
 if [ -f /usr/local/etc/ispcp/ispcp.conf ]
 then
-	for a in `cat /usr/local/etc/ispcp/ispcp.conf  | grep -E '(^ROOT_DIR|^APACHE_WWW_DIR|^PHP_STARTER_DIR)' | sed -e 's/ //g'`; do
-		export $a
-	done
+	CONF=/usr/local/etc/ispcp/ispcp.conf
 else
-	for a in `cat /etc/ispcp/ispcp.conf | grep -E '(^ROOT_DIR|^APACHE_WWW_DIR|^PHP_STARTER_DIR)' | sed -e 's/ //g'`; do
-		export $a
-	done
+	CONF=/etc/ispcp/ispcp.conf
 fi
-
-
-WEBDIRS=`ls -d ${APACHE_WWW_DIR}/* `
-
-for wdir in ${WEBDIRS}; do
-	tmpdir="${wdir}/phptmp"
-	fcgidir=${PHP_STARTER_DIR}`echo ${wdir} | awk "-F${APACHE_WWW_DIR}" '{print $2}'`
-
-	if [ ! -f "${fcgidir}/php4/php.ini" ] && [ ! -f "${fcgidir}/php5/php.ini" ]; then
-		continue;
-	fi
-
-	cur=0
-	max=1440
-
-	for ini in "${fcgidir}/php4/php.ini ${fcgidir}/php5/php.ini"; do
-		if [ ! -f "$ini" ]; then
-			continue;
-		fi
-
-		cur=$(sed -n -e 's/^[[:space:]]*session.gc_maxlifetime[[:space:]]*=[[:space:]]*\([0-9]\+\).*$/\1/p' ${ini} 2>/dev/null || true);
-
-		if [ -z "$cur" ]; then
-			cur=0
-		fi
-
-		if [ "$cur" -gt "$max" ]; then
-			max=$cur
-		fi
-	done
-
-	max=$(($max/60))
-
-	nice -n 19 find ${tmpdir} -type f -cmin +${max} -print0 | xargs -r -0 rm
-
-	# Per-domain php.ini should be used instead of this:
-	#if [ -f "/usr/lib/php4/maxlifetime" ] ; then
-	#	nice -n 19 find ${tmpdir} -type f -cmin +$(/usr/lib/php4/maxlifetime) -print0 | xargs -r -0 rm
-	#else
-	#	nice -n 19 find ${tmpdir} -type f -cmin +24 -print0 | xargs -r -0 rm
-	#fi
+for a in `cat $CONF  | grep -E '(^ROOT_DIR|^APACHE_WWW_DIR|^PHP_STARTER_DIR)' | sed -e 's/ //g'`; do
+	export $a
 done
 
-tmpdir="${ROOT_DIR}/gui/phptmp"
-fcgidir=${PHP_STARTER_DIR}/master
-if [ -f "${fcgidir}/php4/php.ini" ] || [ -f "${fcgidir}/php5/php.ini" ];then
-	cur=0
-	max=1440
-	for ini in "${fcgidir}/php4/php.ini ${fcgidir}/php5/php.ini"; do
+# -r is a GNU-xargs option (BSD doesn't have it, behaving always as if it was specified)
+export XARGS="xargs$(echo '' |xargs -r 2>/dev/null && echo ' -r')"
+
+# Ensure that apache dir ends with exactly one slash
+APACHE_WWW_DIR=${APACHE_WWW_DIR%/}/
+
+# Removes old (according to the php session.gc_maxfiletime directive) files
+# from the given temporary directory. Arguments:
+#   - Temporary directory.
+#   - Fcgi directory containing php{4,5} php.ini files.
+function removeOldFiles {
+	if [ ! -d "$1" ]; then
+		return 1;
+	fi
+
+	max=0
+	for ini in "$2/php4/php.ini" "$2/php5/php.ini"; do
 		if [ ! -f "$ini" ]; then
 			continue;
 		fi
+
 		cur=$(sed -n -e 's/^[[:space:]]*session.gc_maxlifetime[[:space:]]*=[[:space:]]*\([0-9]\+\).*$/\1/p' ${ini} 2>/dev/null || true);
 		if [ -z "$cur" ]; then
 			cur=0
@@ -71,6 +67,19 @@ if [ -f "${fcgidir}/php4/php.ini" ] || [ -f "${fcgidir}/php5/php.ini" ];then
 			max=$cur
 		fi
 	done
+
+	if [ "$max" -eq "0" ]; then
+		# PHP default max lifetime
+		max=1440
+	fi
 	max=$(($max/60))
-	nice -n 19 find ${tmpdir} -type f -cmin +${max} -print0 | xargs -r -0 rm
-fi
+	nice -n 19 find $1 -type f -cmin +${max} -print0 | ${XARGS} -0 rm
+}
+
+# Remove older files from hosted domain's temporary folders
+for wdir in ${APACHE_WWW_DIR}*; do
+	removeOldFiles "${wdir}/phptmp" "${PHP_STARTER_DIR}${wdir#$APACHE_WWW_DIR}"
+done
+
+# And finally remove older files from panel's temporary folder
+removeOldFiles "${ROOT_DIR}/gui/phptmp" "${PHP_STARTER_DIR}/master"
