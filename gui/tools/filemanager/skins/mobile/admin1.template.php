059325e42ea368ed90697012eb84baa447c68879
benedikt <benedikt@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-03-20 09:51:03 +0000

    * [TOOLS] Fixed possible vulnerability. See: http://packetstormsecurity.org/1003-exploits/ispcp-rfi.txt
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/branches/omega-1.0.5@2707 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/tools/filemanager/skins/mobile/admin1.template.php
+++ gui/tools/filemanager/skins/mobile/admin1.template.php
@@ -1,1 +1,7 @@
-<?php require_once($net2ftp_globals["application_skinsdir"] . "/blue/admin1.template.php"); ?>
+<?php 
+// ispCP Modification to avoid vulnerability 
+// http://packetstormsecurity.org/1003-exploits/ispcp-rfi.txt
+// Make sure this file is included by net2ftp, not accessed directly
+defined("NET2FTP") or die("Direct access to this location is not allowed.");
+// End of Modification
+require_once($net2ftp_globals["application_skinsdir"] . "/blue/admin1.template.php"); ?>
