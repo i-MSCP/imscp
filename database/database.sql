b9ee33dc1bb88ced21de7b2e8f3822897bb64ac8
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-04-04 15:18:56 +0000

    * [DATABASE] Fixed Unknown column `tos` after new install
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@2792 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- database/database.sql
+++ database/database.sql
@@ -313,6 +313,7 @@ CREATE TABLE `hosting_plans` (
   `value` varchar(255) collate utf8_unicode_ci default NULL,
   `payment` varchar(255) collate utf8_unicode_ci default NULL,
   `status` int(10) unsigned NOT NULL default '0',
+  `tos`	BLOB NOT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
