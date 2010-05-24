c031196cc09004caedb6f0a54499dc111852ff1a
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-05-24 15:47:16 +0000

    * [GUI] Added interface for `ispCP_ConfigHandler` objects * [GUI] Added documentation for both `Config` and `ConfigHandler` classes. * [GUI] Changed name of `ConfigHandler` class to `ConfigHandlerFile` to reflect the type of storage handle by this configuration object * [GUI] Changed the ConfigHandlerFile class to implement the singleton design pattern * [GUI] Improved the Config class to allow to handle any type of ispCP_ConfigHandler objects (like db storage, xml storage..) * [CHANGELOG] Added missing entry for r2932
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@2934 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/class.Config.php
+++ gui/include/class.Config.php
@@ -26,127 +26,260 @@
  */
 
 /**
- * This class will parse the config file ispcp.conf and save the variables
- * in a stratic array.
- * 
- * @since 	r154
- * @todo no complete Singleton (missing private constructor, clone)
+ * Interface for a Configuration Handler objects
+ *
+ * This interface describes all the methods that a ispCP_ConfigHandler objects
+ * must implement.
+ *
+ * Note: All ispCP_ConfigHandler must implement the Singleton design pattern
+ *
+ * @Since 1.0.6
+ * @author Laurent Declercq (nuxwin) <laurent.declercq@ispcp.net>
  */
-class Config {
-    private static $instance = NULL;
-
-    public static function getInstance() {
-        if (self::$instance === NULL) {
-            self::$instance = new ConfigHandler();
-        }
-        return self::$instance;
-    }
+interface ispCP_ConfigHandler {
 
 	/**
-	 * Compability wrapper for "get"
-	 * @static
-	 * @param string $param
-	 * @return mixed
+	 * Get a Config_Handler object
+     *
+     * Returns a reference to a Config_Handler object, only creating it
+	 * if it doesn't already exist.
+	 *
+	 * @param mixed $params for the ispCP_ConfigHandler object
+	 * @return ispCP_ConfigHandler
 	 */
-	public static function get($param) {
-		return self::getInstance()->get($param);
-	}
+	public static function getInstance($params);
 
 	/**
-	 * Compability wrapper for "set"
-	 * @static
-	 * @param string $param
-	 * @param mixed $value
+	 * Setter method to register a new configuration value
+	 *
+	 * @param $index Key name of the parameter to be registered
+	 * @param mixed $value Parameter value
 	 * @return void
 	 */
-	public static function set($param, $value) {
-		self::getInstance()->set($param, $value);
+	public function set($index, $value);
+
+	/**
+	 * Getter method to retrieve a configuration value
+	 *
+	 * @param string $index Key name of the parameter
+	 * @return mixed Configuration parameter value
+	 */
+	public function get($index);
+
+	/**
+	 * Method to check if a configuration parameter exists
+	 *
+	 * @param $index Key name of the configuration parameter
+	 * @return boolean TRUE if the parameter exists, FALSE otherwise
+	 */
+	public function exists($index);
+}
+
+/**
+ * This class wraps the creation and manipulation of an ispCP_ConfigHandler
+ * objects
+ *
+ * @since 	r154
+ * @todo add ArrayObject support
+ */
+final class Config {
+
+	/**
+	 * References to ispCP_ConfigHandler objects indexed by they class name
+	 *
+	 * @staticvar array
+	 */
+	private static $_instances = array();
+
+	/**
+	 * Get a Config_Handler object
+	 *
+	 * Returns a reference to a Config_Handler object, only creating it
+	 * if it doesn't already exist.
+	 *
+	 * The default ispCP_ConfigHandler object is set to ConfigHandlerFile
+	 *
+	 * @param string $type Type of Config_Handler object that should be returned
+	 * @param mixed Options that are passed to the ispCP_ConfigHandler object
+	 * 	constructor
+	 * @return ispCP_ConfigHandler object
+	 */
+	public static function getInstance($type = 'ConfigHandlerFile',
+		$params = null) {
+
+		if(!array_key_exists($type, self::$_instances)) {
+
+			$refl = new ReflectionClass($type);
+
+    		if (!$refl->implementsInterface('ispCP_ConfigHandler')) {
+				throw new Exception(
+					'Only objects that implement the `ispCP_ConfigHandler` ' .
+					'interface can be handling by the ' . __CLASS__ . ' class!'
+				);
+			}
+
+			self::$_instances[$type] = call_user_func(
+				array($type, 'getInstance'),
+				$params
+			);
+		}
+
+		return self::$_instances[$type];
+	}
+
+	/**
+	 * Wrapper for getter method of a ispCP_ConfigHandler object
+	 *
+	 * @static
+	 * @param string $param Key name of the configuration parameters
+	 * @return mixed configuration parameter value
+	 */
+	public static function get($index, $type = 'ConfigHandlerFile') {
+		return self::getInstance($type)->get($index);
+	}
+
+	/**
+	 * Wrapper for getter method of a ispCP_ConfigHandler object
+	 *
+	 * @static
+	 * @param string $param Key name of the configuration parameter
+	 * @param mixed $value Value name of the configuration parameter
+	 * @return void
+	 */
+	public static function set($index, $value, $type = 'ConfigHandlerFile') {
+		self::getInstance($type)->set($index, $value);
 	}
 }
- 
-class ConfigHandler {	
+
+/**
+ * Class that implements ispCP_ConfigHandler interface
+ *
+ * ispCP_ConfigHandler Object to handle configuration parameters that are stored
+ * in a flat file where each pair of key values are separated by the equal sign.
+ *
+ * By default, this object parse the default ispCP configuration file.
+ *
+ * @See ispCP_ConfigHandler
+ */
+class ConfigHandlerFile implements ispCP_ConfigHandler {
+
 	/**
-	 * Config filename.
-	 * 
-	 * @var String file name
+	 * Configuration file path
+	 *
+	 * @var string Configuration file path
 	 */
-	private $file;
+	private $file_path;
 	
 	/**
-	 * Array with all options parsed from config file.
-	 * 
-	 * @var Array key - value pair
+	 * Array that contain all configuration parameters parsed from the
+	 * configuration file.
+	 *
+	 * @var Array of key - value pair
 	 */
-	private $values;
-
+	private $values = array();
+	
+	/**
+	 * Reference to an object of this class
+	 *
+	 * @var Reference to an object of this class
+	 */
+	private static $_instance = null;
+	
 	/**
 	 * Loads the ispCP config file (default directory: /etc/ispcp/ispcp.conf)
-	 * 
+	 *
 	 * @param String $cfg path to ispcp.conf
 	 */
-	public function __construct() {
-		switch (PHP_OS) {
-			case 'FreeBSD':
-			case 'OpenBSD':
-			case 'NetBSD':
-				$path = '/usr/local/etc/ispcp/ispcp.conf';
-				break;
-				
-			default: 
-				$path = '/etc/ispcp/ispcp.conf';
-				break;
+	private function __construct($path_file = null) {
+
+		if(is_null($path_file)) {
+			switch (PHP_OS) {
+				case 'FreeBSD':
+				case 'OpenBSD':
+				case 'NetBSD':
+					$path_file = '/usr/local/etc/ispcp/ispcp.conf';
+					break;
+				default: 
+					$path_file = '/etc/ispcp/ispcp.conf';
+			}
 		}
-		$this->file = $path;
-	
+
+		$this->file_path = $path_file;
+
 		if (!$this->parseFile()) {
-			throw new Exception('Cannot open the ispcp.conf config file!');
+			throw new Exception(
+				"Unable to open the configuration file `$path_file`!");
 		}
 	}
 
 	/**
-	 * 
-	 * @param unknown_type $param
+	 * Returns a reference to an object of this class, only creating it
+	 * if it doesn't already exist
+	 *
+	 * @param string $path_file Path of configuration file
 	 */
-	public function get($param) {
-		if (!$this->exists($param)) {
-			throw new Exception("Config variable '".$param."' is missing!");
+	public static function getInstance($path_file) {
+
+		if(self::$_instance == null) {
+			self::$_instance = new self($path_file);
 		}
 
-		return $this->values[$param];
+		return self::$_instance;
 	}
 
 	/**
-	 * 
-	 * @param unknown_type $param
-	 * @param unknown_type $value
+	 * Getter method to retrieve a configuration parameter value
+	 *
+	 * @param string $param Key name of the configuration parameter
 	 */
-	public function set($param, $value) {
-		$this->values[$param] = $value;
+	public function get($index) {
+
+		if (!$this->exists($index)) {
+			throw new Exception("Config variable `$index` is missing!");
+		}
+
+		return $this->values[$index];
 	}
 
 	/**
-	 * Checks if a key exists
-	 * 
-	 * @param String $param
-	 * @return boolean
+	 * Setter method to set a new configuration parameter
+	 *
+	 * @param string $param Key name of the configuration parameter
+	 * @param mixed $value Value of the configuration parameter
 	 */
-	public function exists($param) {
-		return isset($this->values[$param]);
+	public function set($index, $value) {
+
+		$this->values[$index] = $value;
 	}
 
 	/**
-	 * Opens the config file and parses its KEY = Value pairs into the $_values
-	 * Array.
-	 * 
-	 * @return boolean true on success
+	 * Checks if a key of a configuration parameters exists
+	 *
+	 * @param string $param Key name of the configuration parameter
+	 * @return boolean TRUE if the configuration parameter is registered, FALSE
+	 * 	otherwise
+	 */
+	public function exists($index) {
+		
+		return isset($this->values[$index]);
+	}
+
+	/**
+	 * Opens a configuration file and parses its KEY = Value pairs into the
+	 * $_values array.
+	 *
+	 * @return boolean TRUE on success, FALSE otherwise
 	 */
 	private function parseFile() {
-		$fd = @file_get_contents($this->file);
+
+		$fd = @file_get_contents($this->file_path);
+
 		if ($fd === false) {
 			return false;
 		}
 
-		$lines = explode("\n", $fd);
+		$lines = explode(PHP_EOL, $fd);
+
 		foreach ($lines as $line) {
 			if (!empty($line) && $line[0] != '#' && strpos($line, '=')) {
 				list($key, $value) = explode('=', $line, 2);
