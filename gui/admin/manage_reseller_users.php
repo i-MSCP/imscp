1f815aa869645ae08b786c88b96c1b082342de46
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-06-03 09:59:40 +0000

    * [GUI] Fixed Undefined variable: $theme_color in 			admin/manage_reseller_users.php  * [CHANGELOG] typo
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@2965 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/admin/manage_reseller_users.php
+++ gui/admin/manage_reseller_users.php
@@ -496,7 +496,7 @@ function check_ip_sets($dest, $users, &$err) {
 $tpl->assign(
 	array(
 		'TR_ADMIN_MANAGE_RESELLER_USERS_PAGE_TITLE' => tr('ispCP - Admin/Manage users/User assignment'),
-		'THEME_COLOR_PATH' => "../themes/$theme_color",
+		'THEME_COLOR_PATH' => "../themes/{$cfg->USER_INITIAL_THEME}",
 		'THEME_CHARSET' => tr('encoding'),
 		'ISP_LOGO' => get_logo($_SESSION['user_id'])
 	)
