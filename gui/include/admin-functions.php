eef8c74b89065d6b206be0fe12e8361ca3fed43d
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-06-14 05:49:22 +0000

    * [GUI] Small fix
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@2995 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/admin-functions.php
+++ gui/include/admin-functions.php
@@ -904,7 +904,7 @@ function generate_reseller_props($reseller_id) {
 
 	if ($rs->RowCount() == 0) {
 		//return array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-		return array_fill(0, 17, 0);
+		return array_fill(0, 18, 0);
 	}
 
 	return array(
@@ -1128,7 +1128,7 @@ function generate_user_props($user_id) {
 
 	if ($rs->RowCount() == 0) {
 		//return array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-		array_fill(0, 13, 0);
+		array_fill(0, 14, 0);
 	}
 
 	$sub_current = records_count('subdomain', 'domain_id', $user_id);
