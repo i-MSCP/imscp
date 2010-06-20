2d5585f63ba2c9783b7a88dc378ff1a31868caaf
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-06-20 22:35:50 +0000

    * [GUI] Added ispCP_Exception and ispCP_Exception_Production classes
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@3035 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ gui/include/ispCP/Exception.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ispCP ω (OMEGA) a Virtual Hosting Control System
+ *
+ * @copyright 	2006-2010 by ispCP | http://isp-control.net
+ * @version 	SVN: $Id$
+ * @link 		http://isp-control.net
+ * @author 		Laurent Declercq (nuxwin) <laurent.declercq@ispcp.net>
+ *
+ * @license
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is "ispCP - ISP Control Panel".
+ *
+ * The Initial Developer of the Original Code is ispCP Team.
+ * Portions created by Initial Developer are Copyright (C) 2006-2010 by
+ * isp Control Panel. All Rights Reserved.
+ */
+
+
+/**
+ * ispCP default Exception
+ *
+ * @Since 1.0.6
+ * @author Laurent Declercq (nuxwin) <laurent.declercq@ispcp.net>
+ * @version 1.0.0
+ */
+class ispCP_Exception extends Exception {}
