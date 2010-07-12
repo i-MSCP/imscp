1c1ed0cc54f328589b7e8d00c62435e398454e06
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-07-12 12:04:52 +0000

    * [GUI] Added some phpdoc tags; changed license emplacement according phpdoc requirements
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@3073 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/ispCP/Database/ResultSet.php
+++ gui/include/ispCP/Database/ResultSet.php
@@ -2,12 +2,6 @@
 /**
  * ispCP ω (OMEGA) a Virtual Hosting Control System
  *
- * @copyright 	2006-2010 by ispCP | http://isp-control.net
- * @version 	SVN: $Id$
- * @link 		http://isp-control.net
- * @author 		ispCP Team
- *
- * @license
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
@@ -23,11 +17,20 @@
  * The Initial Developer of the Original Code is ispCP Team.
  * Portions created by Initial Developer are Copyright (C) 2006-2010 by
  * isp Control Panel. All Rights Reserved.
+ *
+ * @category	ispCP
+ * @package		ispCP_Database
+ * @copyright 	2006-2010 by ispCP | http://isp-control.net
+ * @version 	SVN: $Id$
+ * @link		http://isp-control.net ispCP Home Site
+ * @license		http://www.mozilla.org/MPL/ MPL 1.1
  */
 
 /**
  * DatabaseResult class -  Wrap the PDOStatement class
  *
+ * @category	ispCP
+ * @package		ispCP_Database
  * @author  ispCP Team
  */
 final class ispCP_Database_ResultSet {
