f63a12478b84bb2d8673078df5995cc30c1ff0d2
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-07-16 01:13:46 +0000

    * [GUI] Merged login functions in include/login-functions.php
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@3093 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/login-functions.php
+++ gui/include/login-functions.php
@@ -29,10 +29,96 @@
  */
 
 /**
- * Checks if an username exists
+ * Should be documented
  *
- * @param  String $username Username to be checked
- * @return TRUE, if username exists, FALSE otherwise
+ * @return void
+ */
+function do_session_timeout() {
+
+	$cfg = ispCP_Registry::get('Config');
+	$sql = ispCP_Registry::get('Db');
+
+	$ttl = time() - $cfg->SESSION_TIMEOUT * 60;
+
+	$query = "
+		DELETE FROM
+			`login`
+		WHERE
+			`lastaccess` < ?
+		;
+	";
+
+	exec_query($sql, $query, $ttl);
+
+	if (!session_exists(session_id())) {
+		unset($_SESSION['user_logged']);
+		unset_user_login_data();
+	}
+}
+
+/**
+ * Checks if an session already exists and the IP address is matching
+ *
+ * @param string $sess_id User session id from cookie
+ * @return TRUE if session is valid
+ */
+function session_exists($sess_id) {
+
+	$sql = ispCP_Registry::get('Db');
+
+	$ip = getipaddr();
+
+	$query = "
+		SELECT
+			`session_id`,
+			`ipaddr`
+		FROM
+			`login`
+		WHERE
+			`session_id` = ?
+		AND
+			`ipaddr` = ?
+		;
+	";
+
+	$res = exec_query($sql, $query, array($sess_id, $ip));
+
+	return ($res->recordCount() == 1);
+}
+
+/**
+ * Returns the user's Ip address
+ *
+ * @return string User's Ip address
+ * @todo adding proxy detection
+ */
+function getipaddr() {
+
+	return $_SERVER['REMOTE_ADDR'];
+}
+
+/**
+ * Should be documented
+ *
+ * @return void
+ */
+function init_login() {
+
+	$cfg = ispCP_Registry::get('Config');
+
+	// just make sure to expire counters in case BRUTEFORCE is turned off
+	unblock($cfg->BRUTEFORCE_BLOCK_TIME);
+
+	if ($cfg->BRUTEFORCE) {
+		is_ipaddr_blocked(null, 'bruteforce', true);
+	}
+}
+
+/**
+ * Checks if an user account name exists
+ *
+ * @param  string $username User Account name
+ * @return boolean TRUE if the user Account name exists, FALSE otherwise
  */
 function username_exists($username) {
 
@@ -56,10 +142,10 @@ function username_exists($username) {
 }
 
 /**
- * Returns the stored data related to the username
+ * Returns the stored data related to an user account
  *
- * @param String $username Username that data should be returend
- * @return Array of user-related data
+ * @param string $username User Account name
+ * @return array An array that contains user account related data
  */
 function get_userdata($username) {
 
@@ -81,10 +167,10 @@ function get_userdata($username) {
 }
 
 /**
- * Checks if the user account (domain name) is still vaild
+ * Checks if an user account is expired
  *
- * @param String $username Username that data should be checked
- * @return TRUE if still valid, FALSE otherwise
+ * @param  string $username User account name
+ * @return boolean TRUE if the user account is not expired, FALSE otherwise
  */
 function is_userdomain_expired($username) {
 
@@ -124,10 +210,10 @@ function is_userdomain_expired($username) {
 }
 
 /**
- * Checks if the user account's status is 'ok'
+ * Checks if an user account's status is 'ok'
  *
- * @param string $username Username that data should be checked
- * @return TRUE if status is 'ok', FALSE otherwise
+ * @param string $username User account name to be checked
+ * @return boolean TRUE if the user account's status is 'ok', FALSE otherwise
  */
 function is_userdomain_ok($username) {
 
@@ -162,10 +248,11 @@ function is_userdomain_ok($username) {
 }
 
 /**
- * @todo describe function
+ * Unblock Ip adress
  *
- * @param  int $timeout
- * @param  string $type
+ * @param  $timeout
+ * @param string $type
+ * @return void
  */
 function unblock($timeout = null, $type = 'bruteforce') {
 
@@ -232,12 +319,21 @@ function unblock($timeout = null, $type = 'bruteforce') {
 }
 
 /**
- * @todo describe function
+ *
  *
  * @param string $ipaddr
  * @param string $type
  * @param boolean $autodeny
  * @return TRUE, if...
+ */
+
+/**
+ * Checks if an user Ip address is blocked
+ *
+ * @param string $ipaddr Ip Address to be checked
+ * @param string $type Checking type (bruteforce|captcha)
+ * @param bool $autodeny
+ * @return boolean TRUE if the user Ip address is blocked, FALSE otherwise
  */
 function is_ipaddr_blocked($ipaddr = null, $type = 'bruteforce',
 	$autodeny = false) {
@@ -308,7 +404,7 @@ function is_ipaddr_blocked($ipaddr = null, $type = 'bruteforce',
 }
 
 /**
- * @todo describe function
+ * Dertermine if the user should wait for login
  *
  * @param string $ipaddr
  * @param boolean $displayMessage
@@ -487,7 +583,7 @@ function check_ipaddr($ipaddr = null, $type = 'bruteforce') {
 
 		exec_query($sql, $query, $ipaddr);
 	} else {
-		$backButtonDestination = $cfg->BASE_SERVER_VHOST_PREFIX . 
+		$backButtonDestination = $cfg->BASE_SERVER_VHOST_PREFIX .
 			$cfg->BASE_SERVER_VHOST;
 
 		write_log(
@@ -506,9 +602,9 @@ function check_ipaddr($ipaddr = null, $type = 'bruteforce') {
 }
 
 /**
- * @todo describe function
+ * Block an user Ip Address
  *
- * @param string $ipaddr
+ * @param string $ipaddr User Ip Address
  * @param string $type
  */
 function block_ipaddr($ipaddr, $type = 'General') {
@@ -526,7 +622,9 @@ function block_ipaddr($ipaddr, $type = 'General') {
 }
 
 /**
- * @todo describe function
+ * Display an informational deny message
+ *
+ * @return void
  */
 function deny_access() {
 
@@ -545,66 +643,517 @@ function deny_access() {
 }
 
 /**
- * Returns the user's IP address
+ * Should be documented
  *
- * @return user's IP address
+ * @param  $uname User account name
+ * @param  $upass User account password
+ * @return boolean
+ * @todo use more secure hash algorithm (see PHP mcrypt extension)
  */
-function getipaddr() {
-	return $_SERVER['REMOTE_ADDR'];
-}
-
-/**
- * @todo describe function
- */
-function do_session_timeout() {
+function register_user($uname, $upass) {
 
 	$cfg = ispCP_Registry::get('Config');
 	$sql = ispCP_Registry::get('Db');
 
-	$ttl = time() - $cfg->SESSION_TIMEOUT * 60;
+	$backButtonDestination = $cfg->BASE_SERVER_VHOST_PREFIX .
+		$cfg->BASE_SERVER_VHOST;
 
-	$query = "
-		DELETE FROM
-			`login`
-		WHERE
-			`lastaccess` < ?
-		;
-	";
+	check_ipaddr();
 
-	exec_query($sql, $query, $ttl);
+	if (!username_exists($uname)) {
+		write_log("Login error, <b><i>".$uname."</i></b> unknown username");
+		system_message(
+			tr('You entered an incorrect username/password.'),
+			$backButtonDestination
+		);
 
-	if (!session_exists(session_id())) {
-		unset($_SESSION['user_logged']);
-		unset_user_login_data();
+		return false;
+	}
+
+	$udata = array();
+	$udata = get_userdata($uname);
+
+	if ((databaseUpdate::getInstance()->checkUpdateExists() ||
+		($cfg->MAINTENANCEMODE)) && $udata['admin_type'] != 'admin') {
+
+		write_log(
+			"Login error, <b><i>" . $uname .
+				"</i></b> system currently in maintenance mode"
+		);
+
+		system_message(
+			tr('System is currently under maintenance! Only administrators can login.')
+		);
+
+		return false;
+	}
+
+	if (crypt($upass, $udata['admin_pass']) == $udata['admin_pass']
+		|| md5($upass) == $udata['admin_pass']) {
+
+		if (isset($_SESSION['user_logged'])) {
+			write_log(
+				tr(
+					"%s user already logged or session sharing problem! Aborting...",
+					$uname
+				)
+			);
+
+			system_message(
+				tr('User already logged or session sharing problem! Aborting...')
+			);
+
+			unset_user_login_data();
+
+			return false;
+		}
+
+		if (!is_userdomain_ok($uname)) {
+			write_log(tr("%s's account status is not ok!", $uname));
+			system_message(tr("%s's account status is not ok!", $uname));
+
+			return false;
+		}
+
+		if ($udata['admin_type'] == 'user' && is_userdomain_expired($uname)) {
+			write_log(tr("%s's domain expired!", $uname));
+			system_message(tr("%s's domain expired!", $uname));
+
+			return false;
+		}
+
+		$sess_id = session_id();
+
+		$query = "
+			UPDATE
+				`login`
+			SET
+				`user_name` = ?,
+				`lastaccess` = ?
+			WHERE
+				`session_id` = ?
+			;
+		";
+
+		exec_query($sql, $query, array($uname, time(), $sess_id));
+
+		$_SESSION['user_logged'] = $udata['admin_name'];
+		$_SESSION['user_pass'] = $udata['admin_pass'];
+		$_SESSION['user_type'] = $udata['admin_type'];
+		$_SESSION['user_id'] = $udata['admin_id'];
+		$_SESSION['user_email'] = $udata['email'];
+		$_SESSION['user_created_by'] = $udata['created_by'];
+		$_SESSION['user_login_time'] = time();
+
+		write_log($uname." logged in.");
+
+		return true;
+	} else {
+		write_log($uname . ' entered incorrect password.');
+		system_message(
+			tr('You entered an incorrect username/password.'),
+			$backButtonDestination
+		);
+
+		return false;
 	}
 }
 
 /**
- * Checks if an session already exists and the IP address is matching
+ * Check user login
  *
- * @param string $sess_id User session id from cookie
- * @return TRUE if session is valid
+ * @return boolean
  */
-function session_exists($sess_id) {
+function check_user_login() {
 
+	$cfg = ispCP_Registry::get('Config');
 	$sql = ispCP_Registry::get('Db');
 
-	$ip = getipaddr();
+	$sess_id = session_id();
+	// kill timed out sessions
+	do_session_timeout();
+	$user_logged = isset($_SESSION['user_logged'])
+		? $_SESSION['user_logged'] : false;
 
+	if (!$user_logged) {
+		return false;
+	}
+
+	$user_pass = $_SESSION['user_pass'];
+	$user_type = $_SESSION['user_type'];
+	$user_id = $_SESSION['user_id'];
+
+	// verify session data with database
 	$query = "
 		SELECT
-			`session_id`,
-			`ipaddr`
+			*
 		FROM
-			`login`
+			`admin`, `login`
 		WHERE
-			`session_id` = ?
+			admin.`admin_name` = ?
 		AND
-			`ipaddr` = ?
+			admin.`admin_pass` = ?
+		AND
+			admin.`admin_type` = ?
+		AND
+			admin.`admin_id` = ?
+		AND
+			login.`session_id` = ?
 		;
 	";
 
-	$res = exec_query($sql, $query, array($sess_id, $ip));
+	$rs = exec_query(
+			$sql,
+			$query,
+			array($user_logged, $user_pass, $user_type, $user_id, $sess_id)
+	);
 
-	return ($res->recordCount() == 1);
+	if ($rs->recordCount() != 1) {
+		write_log("Detected session manipulation on ".$user_logged."'s session!");
+		unset_user_login_data();
+
+		return false;
+	}
+
+	if ((databaseUpdate::getInstance()->checkUpdateExists() ||
+		($cfg->MAINTENANCEMODE)) && $user_type != 'admin') {
+		unset_user_login_data(true);
+		write_log(
+			"System is currently in maintenance mode. Logging out <b><i>" .
+				$user_logged . "</i></b>"
+		);
+
+		user_goto('/index.php');
+	}
+
+	// if user login data correct - update session and lastaccess
+	$_SESSION['user_login_time'] = time();
+
+	$query = "
+		UPDATE
+			`login`
+		SET
+			`lastaccess` = ?
+		WHERE
+			`session_id` = ?
+		;
+	";
+
+	exec_query($sql, $query, array(time(), $sess_id));
+
+	return true;
+}
+
+/**
+ * check for valid user login and valid file request/call
+ *
+ * @param string $fName Full file path (ie. the magic __FILE__ constant value)
+ * @param boolean $preventExternalLogin Check HTTP Referer for valid
+ * request/call (ie. to prevent login from external websites)
+ */
+function check_login($fName = null, $preventExternalLogin = true) {
+
+	// session-type check:
+	if (!check_user_login()) {
+
+		if (is_xhr()) {
+			header('HTTP/1.0 403 Forbidden');
+			exit();
+		}
+
+		user_goto('/index.php');
+	}
+
+	if ($fName != null) {
+		$levels = explode('/', realpath(dirname($fName)));
+		$level = $levels[count($levels) - 1];
+
+		switch ($level) {
+			case 'user':
+				$level = 'client';
+				break;
+			case 'admin':
+			case 'reseller':
+				if ($level != $_SESSION['user_type']) {
+					write_log(
+						'Warning! user |'. $_SESSION['user_logged'] .
+							'| requested |' . $_SERVER["REQUEST_URI"] .
+								'| with REQUEST_METHOD |' .
+									$_SERVER["REQUEST_METHOD"] . '|'
+					);
+
+					user_goto('/index.php');
+				}
+				break;
+		}
+	}
+
+	// prevent external login / check for referer
+	if ($preventExternalLogin) {
+		if (isset($_SERVER['HTTP_REFERER']) &&
+			!empty($_SERVER['HTTP_REFERER'])) {
+
+			$info = parse_url($_SERVER['HTTP_REFERER']);
+
+			if (isset($info['host']) && !empty($info['host'])) {
+				// Check if $_SERVER['HTTP_REFERER'] equals $_SERVER['HTTP_HOST']
+				// w/ port number stipped
+				$http_host = $_SERVER['HTTP_HOST'];
+
+				if ($info['host'] != substr(
+						$http_host,
+						0,
+						(int) (strlen($http_host) - strlen(strrchr($http_host, ':'))))
+					|| $info['host'] != $_SERVER['SERVER_NAME']) {
+
+					set_page_message(
+						tr('Request from foreign host was blocked!')
+					);
+
+					if (!(substr(
+							$_SERVER['SCRIPT_FILENAME'],
+							(int)-strlen($_SERVER['REDIRECT_URL']),
+							strlen($_SERVER['REDIRECT_URL'])
+						) === $_SERVER['REDIRECT_URL'])) {
+						redirect_to_level_page();
+					}
+				}
+			}
+		}
+	}
+}
+
+/**
+ * Switch between user's interfaces
+ *
+ * This function allows to switch bettwen user's interfaces for admin and
+ * reseller user accounts.
+ *
+ * @param  $from_id User's id that want switch to an other user's interface
+ * @param  $to_id User identifier that represents the destination interface
+ * @return void
+ */
+function change_user_interface($from_id, $to_id) {
+
+	$sql = ispCP_Registry::get('Db');
+
+	$index = null;
+
+	while (1) { // used to easily exit
+		$query = '
+			SELECT
+				`admin_id`,
+				`admin_name`,
+				`admin_pass`,
+				`admin_type`,
+				`email`,
+				`created_by`
+			FROM
+				`admin`
+			WHERE
+				binary `admin_id` = ?
+			;
+		';
+
+		$rs_from = exec_query($sql, $query, $from_id);
+		$rs_to = exec_query($sql, $query, $to_id);
+
+		if (($rs_from->recordCount()) != 1 || ($rs_to->recordCount()) != 1) {
+			set_page_message(
+				tr('User does not exist or you do not have permission to access this interface!')
+			);
+			break;
+		}
+
+		$from_udata = $rs_from->fetchRow();
+		$to_udata = $rs_to->fetchRow();
+
+		if (!is_userdomain_ok($to_udata['admin_name'])) {
+			set_page_message(
+				tr(
+					"%s's account status is not ok!",
+					decode_idna($to_udata['admin_name'])
+				)
+			);
+			break;
+		}
+
+		$to_admin_type = strtolower($to_udata['admin_type']);
+		$from_admin_type = strtolower($from_udata['admin_type']);
+
+		$allowed_changes = array();
+
+		$allowed_changes['admin']['admin'] = 'manage_users.php';
+		$allowed_changes['admin']['BACK'] = 'manage_users.php';
+		$allowed_changes['admin']['reseller'] = 'index.php';
+		$allowed_changes['admin']['user'] = 'index.php';
+		$allowed_changes['reseller']['user'] = 'index.php';
+		$allowed_changes['reseller']['BACK'] = 'users.php';
+
+		if (!isset($allowed_changes[$from_admin_type][$to_admin_type]) ||
+			($to_admin_type == $from_admin_type &&
+				$from_admin_type != 'admin')) {
+
+			if (isset($_SESSION['logged_from_id'])
+				&& $_SESSION['logged_from_id'] == $to_id) {
+				$index = $allowed_changes[$to_admin_type]['BACK'];
+			} else {
+				set_page_message(
+					tr('You do not have permission to access this interface!')
+				);
+				break;
+			}
+		}
+
+		$index = $index ? $index : $allowed_changes[$from_admin_type][$to_admin_type];
+
+		unset_user_login_data();
+
+		if (($to_admin_type != 'admin' && ((isset($_SESSION['logged_from_id']) &&
+			$_SESSION['logged_from_id'] != $to_id) ||
+			!isset($_SESSION['logged_from_id']))) ||
+			($from_admin_type == 'admin' && $to_admin_type == 'admin')) {
+
+			$_SESSION['logged_from'] = $from_udata['admin_name'];
+			$_SESSION['logged_from_id'] = $from_udata['admin_id'];
+
+		}
+
+		// Ticket 830 - remove the 'logged_from' if back from user
+		if ($from_admin_type == 'user') {
+			// maybe integrated in the construction above...
+			unset($_SESSION['logged_from']);
+			unset($_SESSION['logged_from_id']);
+		}
+
+		// we gonna kill all sessions and globals if user get back to admin level
+		unset($_SESSION['admin_name']);
+		unset($_SESSION['admin_id']);
+
+		unset($GLOBALS['admin_name']);
+		unset($GLOBALS['admin_id']);
+
+		// no more sessions and globals to kill - they were always killed -
+		// rest in peace
+
+		$_SESSION['user_logged'] = $to_udata['admin_name'];
+		$_SESSION['user_pass'] = $to_udata['admin_pass'];
+		$_SESSION['user_type'] = $to_udata['admin_type'];
+		$_SESSION['user_id'] = $to_udata['admin_id'];
+		$_SESSION['user_email'] = $to_udata['email'];
+		$_SESSION['user_created_by'] = $to_udata['created_by'];
+		$_SESSION['user_login_time'] = time();
+
+		$query = '
+			INSERT INTO login
+				(`session_id`, `ipaddr`, `user_name`, `lastaccess`)
+			VALUES
+				(?, ?, ?, ?)
+			;
+		';
+
+		exec_query(
+			$sql,
+			$query,
+			array(
+				session_id(),
+				getipaddr(),
+				$to_udata['admin_name'],
+				$_SESSION['user_login_time']
+			)
+		);
+
+		write_log(
+			sprintf(
+				"%s changes into %s's interface",
+				decode_idna($from_udata['admin_name']),
+				decode_idna($to_udata['admin_name'])
+			)
+		);
+		break;
+	}
+
+	redirect_to_level_page($index);
+}
+
+/**
+ * Unset user login data
+ *
+ * @param bool $ignorePreserve
+ * @return void
+ */
+function unset_user_login_data($ignorePreserve = false) {
+
+	$sql = ispCP_Registry::get('Db');
+
+	if (isset($_SESSION['user_logged'])) {
+
+		$sess_id = session_id();
+
+		$admin_name = $_SESSION['user_logged'];
+
+		$query = "
+			DELETE FROM
+				`login`
+			WHERE
+				`session_id` = ?
+			AND
+				`user_name` = ?
+			;
+		";
+
+		$rs = exec_query($sql, $query, array($sess_id, $admin_name));
+
+	}
+
+	$preserve_list = array('user_def_lang', 'user_theme');
+	$preserve_vals = array();
+
+	if (!$ignorePreserve) {
+		foreach ($preserve_list as $p) {
+			if (isset($_SESSION[$p])) {
+				$preserve_vals[$p] = $_SESSION[$p];
+			}
+		}
+	}
+
+	$_SESSION = array();
+
+	foreach ($preserve_list as $p) {
+		if (isset($preserve_vals[$p])) {
+			$_SESSION[$p] = $preserve_vals[$p];
+		}
+	}
+}
+
+/**
+ * Redirects to user level page
+ *
+ * @param  $file
+ * @param bool $force
+ * @return bool
+ */
+function redirect_to_level_page($file = null, $force = false) {
+
+	if (!isset($_SESSION['user_type']) && !$force)
+		return false;
+
+	if (!$file) {
+		$file = 'index.php';
+	}
+
+	$user_type = isset($_SESSION['user_type']) ? $_SESSION['user_type'] : '';
+
+	switch ($user_type) {
+		case 'user':
+			$user_type = 'client';
+		case 'admin':
+		case 'reseller':
+			header('Location: /' . $user_type . '/' . $file);
+			break;
+		default:
+			header('Location: /index.php');
+	}
+
+	exit();
 }
