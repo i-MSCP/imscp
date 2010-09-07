ac4cd6cf8b268c102687cf36df55a2858d54b16b
ShadowJumper <ShadowJumper@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-09-07 18:36:03 +0000

    *[TOOLS] Updatet phpMyAdmin to Version 3.3.7
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@3258 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/tools/pma/libraries/Config.class.php
+++ gui/tools/pma/libraries/Config.class.php
@@ -92,7 +92,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.3.6');
+        $this->set('PMA_VERSION', '3.3.7');
         /**
          * @deprecated
          */
