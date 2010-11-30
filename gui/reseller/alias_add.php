80f6029efecfc5e81fba745c259e406a097c8da3
thecry <thecry@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-11-30 11:37:54 +0000

    * [GUI] Typo
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@3905 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/reseller/alias_add.php
+++ gui/reseller/alias_add.php
@@ -286,7 +286,15 @@ function add_domain_alias(&$sql, &$err_al) {
 
 	update_reseller_c_props(get_reseller_id($cr_user_id));
 
-	$query = 'SELECT `email` FROM `admin` WHERE `admin_id` = ? LIMIT 1';
+	$query = "
+		SELECT
+			`email`
+		FROM
+			`admin`
+		WHERE
+			`admin_id` = ?
+		LIMIT 1
+	";
 	$rs = exec_query($sql, $query, who_owns_this($cr_user_id, 'dmn_id'));
 	$user_email = $rs->fields['email'];
 
