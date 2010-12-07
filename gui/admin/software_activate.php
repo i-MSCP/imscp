ed4157c20769278efe8be5f66daeffff27fd0666
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-07 19:57:20 +0000

    - GUI: Some fixes
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4029 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/admin/software_activate.php
+++ gui/admin/software_activate.php
@@ -2,7 +2,7 @@
 /**
  * i-MSCP a internet Multi Server Control Panel
  *
- * The contents of this file are subject to the Mozilla Public License
+ * "The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
@@ -12,14 +12,14 @@
  * License for the specific language governing rights and limitations
  * under the License.
  *
- * The Original Code is "i-MSCP - Multi Server Control Panel".
+ * The Original Code is i-MSCP - Multi Server Control Panel.
  *
  * The Initial Developer of the Original Code is i-MSCP Team.
- * Portions created by Initial Developer are Copyright (C) 2010 by
- * i-MSCP a internet Multi Server Control Panel. All Rights Reserved
+ * Portions created by Initial Developer are Copyright (C) 2010
+ * i-MSCP - internet Multi Server Control Panel. All Rights Reserved.
  *
  * @category i-MSCP
- * @copyright 2006-2010 by ispCP | http://i-mscp.net
+ * @copyright 2010 by ispCP | http://i-mscp.net
  * @author Sacha Bay <sascha.bay@i-mscp.net>
  * @version SVN: $Id$
  * @link http://i-mscp.net i-MSCP Home Site
