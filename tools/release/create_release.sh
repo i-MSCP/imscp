bf0eac9a40da394f9fa68859bac57c830115b793
thecry <thecry@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-08 10:09:13 +0000

    - MAINTSCRIPTS Fixed: #48 Licence header for create_release.sh
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4035 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- tools/release/create_release.sh
+++ tools/release/create_release.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# ispCP ω (OMEGA) a Virtual Hosting Control System
+#
+# @copyright 	2006-2010 by ispCP | http://isp-control.net
+# @link 		http://isp-control.net
+# @author 		Jochen Manz <jochen.manz@gmx.de>
+# @copyright    2010 by internet Multi Server Control Panel - http://i-mscp.net
+#
+# @license
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+# License for the specific language governing rights and limitations
+# under the License.
+
 # Variables
 CURRENTVERSION="1.0.0"
 TARGETVERSION="1.0.0"
