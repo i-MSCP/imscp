51c2c6863ef94bfc372ee589f64029c57f267759
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-11 15:00:43 +0000

    - GUI: Added login page (draft)
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/v1-rewrite@4088 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/application/controllers/IndexController.php
+++ gui/application/controllers/IndexController.php
@@ -39,6 +39,7 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+	    $this->_helper->layout->setLayout('simple');
     }
 
     public function indexAction()
