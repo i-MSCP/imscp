e82ca038665703c6853211644d255a81a5456e15
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-13 19:14:46 +0000

    * GUI:   - Updated all license headers (previous was wrong)   - Some fix
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/v1-rewrite@4110 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/application/controllers/ErrorController.php
+++ gui/application/controllers/ErrorController.php
@@ -1,36 +1,29 @@
 <?php
 /**
  * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010 by internet Multi Server Control Panel
  *
- * Copyright (C) 2010 by internet Multi Server Control Panel - http://i-mscp.net
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>
- *
- * The Original Code is "i-MSCP - internet Multi Server Control Panel".
- *
- * The Initial Developer of the Original Code is i-MSCP Team.
- * Portions created by Initial Developer are Copyright (C) 2010 by
- * internet Multi Server Control Panel. All Rights Reserved.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * @category    i-MSCP
  * @copyright   2010 by i-MSCP | http://i-mscp.net
- * @author      Zend Tools
  * @author      i-MSCP Team
  * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
  * @version     SVN: $Id$
- * @link        http://i-mscp.net i-MSCP Home Site
- * @license     http://www.gnu.org/licenses/ GPL v2
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
  */
 
 class ErrorController extends Zend_Controller_Action
--- gui/public/index.php
+++ gui/public/index.php
@@ -1,61 +1,55 @@
 <?php
-/* i-MSCP - internet Multi Server Control Panel
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010 by internet Multi Server Control Panel
  *
- * Copyright (C) 2010 by internet Multi Server Control Panel - http://i-mscp.net
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>
- *
- * The Original Code is "i-MSCP - internet Multi Server Control Panel".
- *
- * The Initial Developer of the Original Code is i-MSCP Team.
- * Portions created by Initial Developer are Copyright (C) 2010 by
- * internet Multi Server Control Panel. All Rights Reserved.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * @category    i-MSCP
  * @copyright   2010 by i-MSCP | http://i-mscp.net
  * @author      i-MSCP Team
  * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
  * @version     SVN: $Id$
- * @link        http://i-mscp.net i-MSCP Home Site
- * @license     http://www.gnu.org/licenses/ GPL v2
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
  */
 
 // Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define path to public directory
-defined('PUBLIC_PATH')
-    || define('PUBLIC_PATH', realpath(APPLICATION_PATH . '/../public'));
+defined('PUBLIC_PATH') || define('PUBLIC_PATH', realpath(APPLICATION_PATH . '/../public'));
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+	|| define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 
 // Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
+set_include_path(
+	implode(
+		PATH_SEPARATOR,
+		array(
+            realpath(APPLICATION_PATH . '/../library'),
+            get_include_path(),
+		)
+	)
+);
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-
+$application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
 $application->bootstrap()->run();
