83a601aa614e42efd7fa6b4330304cf3d2640b23
sci2tech <sci2tech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-20 00:53:16 +0000

    Some classes add
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/v1-rewrite@4140 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ backend/PerlLib/iMSCP/Config.pm
@@ -0,0 +1,121 @@
+# i-MSCP a internet Multi Server Control Panel
+#
+# Copyright (C) 2010 by internet Multi Server Control Panel - http://i-mscp.net
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+#
+# The Original Code is "i-MSCP internet Multi Server Control Panel".
+#
+# The Initial Developer of the Original Code is i-MSCP Team.
+# Portions created by Initial Developer are Copyright (C) 22010 by
+# internet Multi Server Control Panel. All Rights Reserved.
+#
+# @category		i-MSCP
+# @copyright	2010 by i-MSCP | http://i-mscp.net
+# @author		Daniel Andreca <sci2tech@i-mscp.net>
+# @version		SVN: $Id: imscp-build 3933 2010-12-01 19:35:32Z sci2tech $
+# @link			http://i-mscp.net i-MSCP Home Site
+# @license		http://www.gnu.org/licenses/ GPL v2
+
+package iMSCP::Config;
+
+use strict;
+use warnings;
+use Tie::File;
+use Log::Message::Simple;
+use Switch;
+
+use vars qw/@ISA/;
+@ISA = ("Common::SingletonClass");
+use Common::SingletonClass;
+
+
+sub TIEHASH {
+	my $self = shift;
+	$self = $self->new(@_);
+	debug((caller(0))[3].': Starting...');
+	debug((caller(0))[3].': Tieing ...');
+	$self->_loadConfig();
+	$self->_parseConfig($self->{conf});
+	debug((caller(0))[3].': Ending...');
+	return $self;
+}
+sub FETCH {
+	my $self	= shift;
+	my $config	= shift;
+	debug((caller(0))[3].': Starting...');
+	debug(sprintf((caller(0))[3].': Fetching %s...', $config));
+	if (!exists($self->{configValues}->{$config})){
+		iMSCP::Exception->new()->exception(sprintf('Accessing non existing config value %s', $config));
+	}
+	debug((caller(0))[3].': Ending...');
+	return ${$self->{configValues}->{$config}}
+}
+sub STORE {
+	my $self	= shift;
+	my $config	= shift;
+	my $value	= shift;
+	debug((caller(0))[3].': Starting...');
+	if(!exists($self->{configValues}->{$config})){
+		iMSCP::Exception->new()->exception(sprintf('Accessing non existing config value %s', $config));
+	} else {
+		$self->_replaceConfig($config, $value);
+	}
+	debug((caller(0))[3].': Ending...');
+}
+sub _loadConfig{
+	my $self	= shift;
+	debug((caller(0))[3].': Starting...');
+	switch ($^O) {
+		case /bsd$/ {
+			$self->{confFile} = '/usr/local/etc/imscp/imscp.conf';
+		} else {
+			$self->{confFile} = '/etc/imscp/imscp.xml';
+		}
+	}
+	debug((caller(0))[3].': Config file ' . $self->{confFile});
+	iMSCP::Exception->new()->exception("Can`t use ".$self->{confFile}) unless -f $self->{confFile};
+	my $xml = new XML::Simple (NoAttr=>1, RootName=>'config');
+	$self->{conf} = $xml->XMLin($self->{confFile}, ForceArray => 1);
+	debug((caller(0))[3].': Ending...');
+}
+sub _parseConfig{
+	my $self = shift;
+	my $hash = shift;
+	debug((caller(0))[3].': Starting...');
+	foreach (keys %{$hash}){
+		if(ref($hash->{$_}[0]) eq 'HASH'){
+			debug((caller(0))[3].": $_ is a hash. Going recursive");
+			$self->_parseConfig(\%{$hash->{$_}[0]})
+		} else {
+			debug((caller(0))[3].": We have a value $_ -> $hash->{$_}[0]");
+			$self->{configValues}->{$_} = \$hash->{$_}[0];
+		}
+	}
+	debug((caller(0))[3].': Ending...');
+}
+
+sub _replaceConfig{
+	my $self	= shift;
+	my $config	= shift;
+	my $value	= shift;
+	debug((caller(0))[3].': Starting...');
+	${$self->{configValues}->{$config}} = $value;
+	XML::Simple->new(NoAttr=>1, RootName=>'config',  XMLDecl => '<?xml version="1.0" encoding="UTF-8"?>', OutputFile => $self->{confFile})->XMLout($self->{conf});
+	debug((caller(0))[3].': Ending...');
+}
+
+1;
+
+__END__
