0c66ebff174ebafd9d32e14b54c1fba8e8fe0658
sci2tech <sci2tech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-23 20:57:12 +0000

    License add
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4176 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- configs/common/database/schema.sql
+++ configs/common/database/schema.sql
@@ -1,3 +1,31 @@
+--!/usr/bin/perl
+--
+-- i-MSCP - internet Multi Server Control Panel
+-- Copyright (C) 2010 by internet Multi Server Control Panel
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--
+-- @category	i-MSCP
+-- @copyright	2010 by i-MSCP | http://i-mscp.net
+-- @author		Laurent Declercq <laurent.declercq@i-mscp.net>
+-- @version		SVN: $Id: imscp-build 3933 2010-12-01 19:35:32Z sci2tech $
+-- @link		http://i-mscp.net i-MSCP Home Site
+-- @license		http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+--
+-- --------------------------------------------------------
+
 create database `{DATABASE_NAME}` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 use `{DATABASE_NAME}`;
