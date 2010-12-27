d72390033caee46472a19d1429e8c30d3da02575
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2010-12-27 04:06:21 +0000

    * Fixed logout link in the layout  * Some fixes
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4212 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- frontend/application/modules/core/controllers/AuthController.php
+++ frontend/application/modules/core/controllers/AuthController.php
@@ -37,15 +37,18 @@ class Core_AuthController extends Zend_Controller_Action
     public function indexAction()
     {
 	    $auth = Zend_Auth::getInstance();
+
 	    // If we're already logged in, just redirect
 	    if ($auth->hasIdentity()) {
 		    $this->successRedirect();
 	    }
+
         $request = $this->getRequest();
 	    $this->view->message = null;
 	    if ($request->isPost()) {
-		    $username = $request->getParam('uname');
-		    $password = $request->getParam('upass');
+		    $username = $request->getParam('username');
+		    $password = $request->getParam('password');
+
 		    if ($username == 'admin' && $password == 'password') {
 			    $auth->getStorage()->write(1);
 			    $this->successRedirect();
