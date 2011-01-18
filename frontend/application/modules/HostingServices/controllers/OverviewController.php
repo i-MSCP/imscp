0df71fa0f8cf1526b50348536852ba95deba5b1a
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-01-18 13:23:42 +0000

    * Added some CSS for actions box and more (Work in progress...)  * Added two new icons  * Added HostingServices module  * Added several controllers with some actions in HostingServices module (Overview, Account, ServicePlan and Subscription)  * Added several views associated with new controllers  * Added HostingServices/config/routes/account.ini routes configuration file  * Changed delete icon  * Updated core/config/routes/user.ini routes configurations file (all disabled for now)  * Updated admin level menu
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4321 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ frontend/application/modules/HostingServices/controllers/OverviewController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010 - 2011 by internet Multi Server Control Panel
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category    iMSCP
+ * @package     iMSCP_HostingServices
+ * @subpackage  Controllers_Overview
+ * @copyright   2010 - 2011 by i-MSCP | http://i-mscp.net
+ * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
+ * @version     SVN: $Id$
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ */
+
+/**
+ * Account controller
+ *
+ * @category    iMSCP
+ * @package     iMSCP_Core
+ * @subpackage  Controllers_Overview
+ * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
+ * @copyright   2010 - 2011 by i-MSCP | http://i-mscp.net
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ * @since       1.0.0
+ * @version     1.0.0
+ */
+class HostingServices_OverviewController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function overviewAction()
+    {
+        // action body
+    }
+}
