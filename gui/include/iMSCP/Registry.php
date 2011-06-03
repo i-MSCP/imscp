5cdc1a052201c3d709b7c947ee69574514248380
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-06-03 04:53:59 +0000

    iMSCP_Registry: Code style, Documentation
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4389 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/iMSCP/Registry.php
+++ gui/include/iMSCP/Registry.php
@@ -21,149 +21,155 @@
  * Portions created by the i-MSCP Team are Copyright (C) 2010 by
  * i-MSCP - internet Multi Server Control Panel. All Rights Reserved.
  *
- * @category	i-MSCP
- * @package		iMSCP_Registry
- * @copyright	2006-2010 by ispCP | http://isp-control.net
- * @copyright 	2010 by i-MSCP | http://i-mscp.net
- * @author		Laurent Declercq <laurent.declercq@i-mscp.net>
- * @version		SVN: $Id$
- * @link		http://i-mscp.net i-MSCP Home Site
- * @license		http://www.mozilla.org/MPL/ MPL 1.1
+ * @category    i-MSCP
+ * @package     iMSCP_Registry
+ * @copyright   2006-2010 by ispCP | http://isp-control.net
+ * @copyright   2010-2011 by i-MSCP | http://i-mscp.net
+ * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
+ * @version     SVN: $Id$
+ * @link        http://i-mscp.net i-MSCP Home Site
+ * @license     http://www.mozilla.org/MPL/ MPL 1.1
  */
 
 /**
  * Class to store shared data (Better than global variables usage)
  *
- * @package		iMSCP_Registry
- * @author		Laurent Declercq <laurent.declercq@i-mscp.net>
+ * @package     iMSCP_Registry
+ * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
  * @since       1.0.7 (ispCP)
- * @version		1.0.7
+ * @version     1.0.7
  */
-class iMSCP_Registry {
+class iMSCP_Registry
+{
+    /**
+     * Instance of this class that provides storage for shared data
+     *
+     * @var iMSCP_Registry
+     */
+    protected static $_instance = null;
 
-	/**
-	 * Instance of this class that provides storage for shared data
-	 *
-	 * @var iMSCP_Registry
-	 */
-	protected static $_instance = null;
+    /**
+     * This class implements the Singleton design pattern
+     */
+    protected function __construct()
+    {
+    }
 
-	/**
-	 * This class implements the Singleton design pattern
-	 *
-	 * @return void
-	 */
-	protected function __construct(){}
+    /**
+     * This class implements the Singleton design pattern
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
 
-	/**
-	 * This class implements the Singleton design pattern
-	 *
-	 * @return void
-	 */
-	private function __clone(){}
+    /**
+     * Get an iMSCP_Registry instance
+     *
+     * Returns an {@link iMSCP_Registry} instance, only creating it if it doesn't
+     * already exist.
+     *
+     * @return iMSCP_Registry An iMSCP_Registry instance
+     */
+    public static function getInstance()
+    {
+        if (self::$_instance == null) {
+            self::$_instance = new self;
+        }
 
-	/**
-	 * Get an iMSCP_Registry instance
-	 *
-	 * Returns an {@link iMSCP_Registry} instance, only creating it if it doesn't already exist.
-	 *
-	 * @return iMSCP_Registry An iMSCP_Registry instance
-	 */
-	public static function getInstance() {
+        return self::$_instance;
+    }
 
-		if(self::$_instance == null) {
-			self::$_instance = new self;
-		}
+    /**
+     * Getter method to retrieve registered data
+     *
+     * <b>Note:</b> If you want get a reference for data that is not an object, you
+     * should always use this method and not accessed it directly like an object
+     * member.
+     *
+     * @throws iMSCP_Exception
+     * @param string $index Data key name
+     * @return mixed Registered data
+     */
+    public static function &get($index)
+    {
+        $instance = self::getInstance();
 
-		return self::$_instance;
-	}
+        if (!$instance->isRegistered($index)) {
+            throw new iMSCP_Exception("Data `$index` is not registered.");
+        }
 
-	/**
-	 * Getter method to retrieve registered data
-	 *
-	 * <b>Note:</b> If you want get a reference for data that is not an object, you should always use this method and
-	 * not accessed it directly like an object member.
-	 *
-	 * @throws iMSCP_Exception
-	 * @param string $index Data key name
-	 * @return mixed Registered data
-	 */
-	public static function &get($index) {
+        return $instance->$index;
+    }
 
-		$instance = self::getInstance();
+    /**
+     * Overloading on inaccessible members
+     *
+     * This method raises an {@link iMSCP_Exception} if a member is inaccessible for
+     * reading.
+     *
+     * @throws iMSCP_Exception
+     * @param string $index Data key name
+     * @return void
+     */
+    public function __get($index)
+    {
+        throw new iMSCP_Exception("Data `$index` is not registered.");
+    }
 
-		if (!$instance->isRegistered($index)) {
-			throw new iMSCP_Exception("Error: Data `$index` is not registered!");
-		}
+    /**
+     * Setter method to register data
+     *
+     * For conveniences reasons, this method return the data registered.
+     *
+     * @param string $index Data key name
+     * @param mixed $value Data value
+     * @return mixed Registered Data
+     */
+    public static function &set($index, $value)
+    {
+        $instance = self::getInstance();
+        $instance->$index = $value;
 
-		return $instance->$index;
-	}
+        return $instance->$index;
+    }
 
-	/**
-	 * Overloading on inaccessible members
-	 *
-	 * This method raises an {@link iMSCP_Exception} if a member is inaccessible for reading.
-	 *
-	 * @throws iMSCP_Exception
-	 * @param string $index Data key name
-	 * @return void
-	 */
-	public function __get($index) {
+    /**
+     * Setter method to register data by reference.
+     *
+     * This method take sense for the singleton objects that provide a method to
+     * recreate the instance. When the object instance is reseted, the alias that iq
+     * registered in the registry will refer to the new instance. It's not the case
+     * with data that were registered with the {@link set()} method because
+     * registered values for objects are objects identifiers and not real aliases.
+     *
+     * See the {@link http://www.php.net/manual/en/language.oop5.references.php Php
+     * documentation} for more information about this issue.
+     *
+     * @param string $index Data key name
+     * @param mixed $value Data value
+     * @return mixed Registered data
+     */
+    public static function &setAlias($index, &$value)
+    {
+        $instance = self::getInstance();
 
-		throw new iMSCP_Exception("Error: Data `$index` is not registered!");
-	}
+        // Small workaround to avoid call of magic __get()
+        // See http://bugs.php.net/bug.php?id=52157
+        $instance->$index = '';
 
-	/**
-	 * Setter method to register data
-	 *
-	 * For conveniences reasons, this method return the data registered.
-	 *
-	 * @param string $index Data key name
-	 * @param mixed $value Data value
-	 * @return mixed Registered Data
-	 */
-	public static function &set($index, $value) {
+        return $instance->$index = &$value;
+    }
 
-		$instance = self::getInstance();
-		$instance->$index = $value;
-
-		return $instance->$index;
-	}
-
-	/**
-	 * Setter method to register data by reference.
-	 *
-	 * This method take sense for the singleton objects that provide a method to recreate the instance. When the object
-	 * instance is reseted, the alias that iq registered in the registry will refer to the new instance. It's not the
-	 * case with data that were registered with the {@link set()} method because registered values for objects are
-	 * objects identifiers and not real aliases.
-	 *
-	 * See the {@link http://www.php.net/manual/en/language.oop5.references.php Php documentation} for more information
-	 * about this issue.
-	 *
-	 * @param string $index Data key name
-	 * @param mixed $value Data value
-	 * @return mixed Registered data
-	 */
-	public static function &setAlias($index, &$value) {
-
-		$instance = self::getInstance();
-
-		// Small workaround to avoid call of magic __get()
-		// See http://bugs.php.net/bug.php?id=52157
-		$instance->$index = '';
-
-		return $instance->$index = &$value;
-	}
-
-	/**
-	 * Check if data is registered
-	 *
-	 * @param string $index Data key name
-	 * @return boolean TRUE if data is registered, FALSE otherwise
-	 */
-	public static function isRegistered($index) {
-
-		return array_key_exists($index, self::getInstance());
-	}
+    /**
+     * Check if data is registered
+     *
+     * @param string $index Data key name
+     * @return boolean TRUE if data is registered, FALSE otherwise
+     */
+    public static function isRegistered($index)
+    {
+        return array_key_exists($index, self::getInstance());
+    }
 }
