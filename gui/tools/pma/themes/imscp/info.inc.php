5d42a9de513080bcfc7b6dedb3a319581a08618a
sci2tech <sci2tech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-06-06 11:20:26 +0000

    Add pma theme
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4426 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ gui/tools/pma/themes/imscp/info.inc.php
@@ -0,0 +1,15 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ *
+ *
+ * @version $Id$
+ * @package phpMyAdmin-theme
+ * @subpackage i-MSCP internet Multi Server Control Panel
+ */
+
+$theme_name = 'i-MSCP internet Multi Server Control Panel';
+$theme_version = 1;
+$theme_generation = 1;
+?>
