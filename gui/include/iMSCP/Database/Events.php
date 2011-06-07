7be05d30194d47426daa8af0a861e3be6095106c
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-06-07 22:58:40 +0000

    iMSCP_Database:  - Added class that describes all events thrown in iMSCP database component.
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4443 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ gui/include/iMSCP/Database/Events.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * i-MSCP - internet Project Management System
+ * Copyright (C) 2010-2011 by i-MSCP team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @package     iMSCP
+ * @package     iMSCP_Database
+ * @subpackage  Events
+ * @copyright   2010-2011 by i-MSCP team
+ * @author      Laurent Declercq <ldeclercq@l.declercq@nuxwin.com>
+ * @version     SVN: $Id$
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ */
+
+/**
+ * Contains all events thrown in the iMSCP_Database component
+ *
+ * @package     iMSCP
+ * @package     iMSCP_Database
+ * @subpackage  Events
+ * @author      Laurent Declercq <l.declercq@nuxwin.com>
+ * @version     0.0.1
+ */
+final class iMSCP_Database_Events
+{
+    /**
+     * The onBeforeConnection event occurs before connection to the database is made.
+     *
+     * @var string
+     */
+    const onBeforeConnection = 'onBeforeConnection';
+
+    /**
+     * The onAfterConnection event occurs after connection to the database is made.
+     *
+     * @var string
+     */
+    const onAfterConnection = 'onAfterConnection';
+
+    /**
+     * The onBeforePrepare event occurs before an SQL statement was prepared for execution.
+     *
+     * @var string
+     */
+    const onBeforePrepare = 'onBeforePrepare';
+
+    /**
+     * The onAfterPrepare event occurs after an SQL statement was prepared for execution.
+     *
+     * @var string
+     */
+    const onAfterPrepare = 'onAfterPrepare';
+
+    /**
+     * The onBeforeExecute event occurs before a prepared SQL statement was executed.
+     *
+     * @var string
+     */
+    const onBeforeExecute = 'onBeforeExecute';
+
+    /**
+     * The onAfterExecute event occurs after a prepared SQL statement was executed.
+     *
+     * @var string
+     */
+    const onAfterExecute = 'onAfterExecute';
+}
