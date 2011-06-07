9dcb081403cdaaab706f9c9aa549964a9315f80c
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-06-07 23:23:42 +0000

    Database component:  - Added iMSCP_Database_Events_Database class.  - Added iMSCP_Database_Events_Statement class.
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4445 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ gui/include/iMSCP/Database/Events/Statement.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2011 by i-MSCP team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category    iMSCP
+ * @package     iMSCP_Database
+ * @package     Events
+ * @copyright   2010-2011 by i-MSCP team
+ * @author      Laurent Declercq <ldeclercq@nuxwin.com>
+ * @version     SVN: $Id$
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ */
+
+/** @see iMSCP_Database_Events_Database */
+require_once 'iMSCP/Database/Events/Database.php';
+
+/**
+ * Base class for events thrown in the iMSCP_Database component.
+ *
+ * @category    iMSCP
+ * @package     iMSCP_Database
+ * @package     Events
+ * @author      Laurent Declercq <l.declercq@nuxwin.com>
+ * @version     0.0.1
+ */
+class iMSCP_Database_Events_Statement extends iMSCP_Database_Events_Database
+{
+    /**
+     * @var PDOStatement
+     */
+    protected $_statement;
+
+    /**
+     * Constructor.
+     *
+     * @param PDOStatement $statement
+     * @param iMSCP_Database $db
+     */
+    public function __construct(PDOStatement $statement, iMSCP_Database $db)
+    {
+        $this->_statement = $statement;
+        parent::__construct($db, $statement->queryString);
+    }
+
+    /**
+     * Returns a PDOstatement.
+     *
+     * @return PDOStatement
+     */
+    public function getStatement()
+    {
+        return $this->_statement;
+    }
+}
