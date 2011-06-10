e491c534f94bef8ce977f4ce0b7dc0a65b5361fd
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-06-10 11:41:12 +0000

    DebugBar component (testing version):  - CSS fix, code style
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4502 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/iMSCP/Debug/Bar.php
+++ gui/include/iMSCP/Debug/Bar.php
@@ -186,8 +186,10 @@ class iMSCP_Debug_Bar
 
         $templateEngine = $this->_event->getTemplateEngine();
         $response = $templateEngine->getLastParseResult();
-        $response = preg_replace('@(</head>)@i', $this->_buildHeader() . PHP_EOL . '$1', $response);
-        $response = str_ireplace('</body>','<div id="iMSCPdebug_debug">' . $xhtml . '</div></body>', $response);
+        $response = preg_replace(
+            '@(</head>)@i', $this->_buildHeader() . PHP_EOL . '$1', $response);
+        $response = str_ireplace(
+            '</body>', '<div id="iMSCPdebug_debug">' . $xhtml . '</div></body>', $response);
         $templateEngine->replaceLastParseResult($response);
     }
 
@@ -203,11 +205,10 @@ class iMSCP_Debug_Bar
 
         return ('
             <style type="text/css" media="screen">
-                #iMSCPdebug_debug { font: 11px/1.4em Lucida Grande, Lucida Sans Unicode, sans-serif; position:fixed; bottom:0px; left:0px; color:#000; z-index: 255;}
+                #iMSCPdebug_debug { font: 11px/1.4em Lucida Grande, Lucida Sans Unicode, sans-serif; position:fixed; bottom:5px; left:0px; color:#000; z-index: 255;}
                 #iMSCPdebug_debug ol {margin:10px 0px; padding:0 25px}
                 #iMSCPdebug_debug li {margin:0 0 10px 0;}
-
-                #iMSCPdebug_debug .clickable {cursor:pointer}
+                #iMSCPdebug_debug .clickable { cursor:pointer }
                 #iMSCPdebug_toggler { font-weight:bold; background:#BFBFBF; }
                 .iMSCPdebug_span { border: 1px solid #999; border-right:0px; background:#DFDFDF; padding: 5px 5px; }
                 .iMSCPdebug_last { border: 1px solid #999; }
