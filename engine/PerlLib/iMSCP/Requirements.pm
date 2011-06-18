67c0b08f3dd486de6a1d8ab133bb2f27acad622c
sci2tech <sci2tech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-06-18 16:12:36 +0000

    Increase php version
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4614 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- engine/PerlLib/iMSCP/Requirements.pm
+++ engine/PerlLib/iMSCP/Requirements.pm
@@ -61,7 +61,7 @@ sub _init{
 	};
 
 	$self->{programs} = {
-		'php'	=> {version	=> 'php -v',	regexp	=> 'PHP ([\d.]+)',	minversion => '5.3.0'},
+		'php'	=> {version	=> 'php -v',	regexp	=> 'PHP ([\d.]+)',	minversion => '5.3.3'},
 		'perl'	=> {version	=> 'perl -v',	regexp	=> 'v([\d.]+)',	minversion => '5.10.1'}
 	};
 }
