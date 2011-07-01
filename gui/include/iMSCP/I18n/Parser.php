08f6faa75e430e669156306a33ee92dd7ddff834
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-07-01 18:17:06 +0000

    i18n: Added parser library for gettext files (*.po, *.mo)
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4746 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ gui/include/iMSCP/I18n/Parser.php
@@ -0,0 +1,276 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2011 by i-MSCP Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category	iMSCP
+ * @package		iMSCP_I18n
+ * @subpackage	Parser
+ * @copyright	2010-2011 i-MSCP Team
+ * @author		Laurent Declercq <l.declercq@nuxwin.com>
+ * @version		SVN:$Id$
+ * @link		http://www.i-mscp.net i-MSCP Home Site
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ */
+
+/**
+ * Base class to parse gettex files (*.po, *.mo)
+ *
+ * @author Laurent Declercq <l.declercq@nuxwin.com>
+ * @version 0.0.1
+ */
+abstract class iMSCP_I18n_Parser
+{
+	/**
+	 * Headers.
+	 *
+	 * @var int
+	 */
+	const HEADERS = 1;
+
+	/**
+	 * Translation table.
+	 *
+	 * @var int
+	 */
+	const TRANSLATION_TABLE = 2;
+
+	/**
+	 * File handle.
+	 *
+	 * @var resource
+	 */
+	protected $_fh;
+
+	/**
+	 * Path to the gettext file.
+	 *
+	 * @var string
+	 */
+	protected $_filePath;
+
+	/**
+	 * Headers from gettext file.
+	 *
+	 * @var string
+	 */
+	protected $_headers = '';
+
+	/**
+	 * Translation table.
+	 *
+	 * @var array
+	 */
+	protected $_translationTable = array();
+
+	/**
+	 * Constructor.
+	 *
+	 * @throws iMSCP_i18n_Exception When file is not readable
+	 * @param $filePath Path to Machine object file
+	 */
+	public function __construct($filePath)
+	{
+		$filePath = (string)$filePath;
+
+		if (!is_readable($filePath)) {
+			require_once 'iMSCP/I18n/Exception.php';
+			throw new iMSCP_i18n_Exception("$filePath is not readable");
+		}
+
+		$this->_filePath = $filePath;
+	}
+
+	/**
+	 * Returns headers.
+	 *
+	 * @return string A string that contains headers
+	 */
+	public function getHeaders()
+	{
+		if (empty($this->_headers)) {
+			$this->_headers = $this->_parse(self::HEADERS);
+		}
+
+		return $this->_headers;
+	}
+
+	/**
+	 * Returns translation table.
+	 *
+	 * @return array
+	 */
+	public function getTranslationTable()
+	{
+		if (empty($this->_translationTable)) {
+			$this->_translationTable = $this->_parse(self::TRANSLATION_TABLE);
+		}
+
+		return $this->_translationTable;
+	}
+
+	/**
+	 * Retruns project id version.
+	 *
+	 * @return string Header value
+	 */
+	public function getProjectIdVersion()
+	{
+		return $this->_getHeaderValue('Project-Id-Version:');
+	}
+
+	/**
+	 * Returns report msgid bugs value.
+	 *
+	 * @return string Header value
+	 */
+	public function getReportMsgidBugs()
+	{
+		return $this->_getHeaderValue('Report-Msgid-Bugs-To:');
+	}
+
+	/**
+	 * Returns pot creation date.
+	 *
+	 * @return string Header value
+	 */
+	public function getPotCreationDate()
+	{
+		return $this->_getHeaderValue('POT-Creation-Date:');
+	}
+
+	/**
+	 * Returns po creation date.
+	 *
+	 * @return string Header value
+	 */
+	public function getPoRevisionDate()
+	{
+		return $this->_getHeaderValue('PO-Revision-Date:');
+	}
+
+	/**
+	 * Returns last translator.
+	 *
+	 * @return string Header value
+	 */
+	public function getLastTranslator()
+	{
+		return $this->_getHeaderValue('Last-Translator:');
+	}
+
+	/**
+	 * Returns language team.
+	 *
+	 * @return string Header value
+	 */
+	public function getLanguageTeam()
+	{
+		return $this->_getHeaderValue('Language-Team:');
+	}
+
+	/**
+	 * Returns mime version.
+	 *
+	 * @return string Header value
+	 */
+	public function getMimeVersion()
+	{
+		return $this->_getHeaderValue('MIME-Version:');
+	}
+
+	/**
+	 * Returns content type.
+	 *
+	 * @return string Header value
+	 */
+	public function getContentType()
+	{
+		return $this->_getHeaderValue('Content-Type:');
+	}
+
+	/**
+	 * Returns content transfer encoding.
+	 *
+	 * @return string Header value
+	 */
+	public function getContentTransferEncoding()
+	{
+		return $this->_getHeaderValue('Content-Transfer-Encoding:');
+	}
+
+	/**
+	 * Returns language.
+	 *
+	 * @return string
+	 */
+	public function getLanguage()
+	{
+		return $this->_getHeaderValue('Language:');
+	}
+
+	/**
+	 * Returns Plural Forms value.
+	 *
+	 * @return string
+	 */
+	public function getPluralForms()
+	{
+		return $this->_getHeaderValue('Plural-Forms:');
+	}
+
+	/**
+	 * Returns number of translated strings.
+	 *
+	 * @abstract
+	 * @return int Number of translated strings
+	 */
+	abstract public function getNumberOfTranslatedStrings();
+
+	/**
+	 * Parse file.
+	 *
+	 * @abstract
+	 * @param $part part file to parse (ALL|HEADER|TRANSLATION_TABLE
+	 * @return void
+	 */
+	abstract protected function _parse($part);
+
+	/**
+	 * Returns given header value.
+	 *
+	 * @param string $header header name
+	 * @return string header
+	 */
+	protected function _getHeaderValue($header)
+	{
+		$headers = $this->getHeaders();
+		$header = substr($headers, strpos($headers, $header));
+
+		return substr($header, ($start = strpos($header, ':') + 2),
+			(strpos($header, chr(10)) - $start));
+	}
+
+	/**
+	 * Destructor.
+	 *
+	 */
+	public function __destruct()
+	{
+		fclose($this->_fh);
+	}
+}
---
+++ gui/include/iMSCP/I18n/Parser/Mo.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2011 by i-MSCP Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category	iMSCP
+ * @package		iMSCP_I18n
+ * @subpackage	Parser
+ * @copyright	2010-2011 i-MSCP Team
+ * @author		Laurent Declercq <l.declercq@nuxwin.com>
+ * @version		SVN:$Id$
+ * @link		http://www.i-mscp.net i-MSCP Home Site
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ */
+
+/**
+ * Class to parse gettext machine object files.
+ * @see http://www.gnu.org/software/gettext/manual/gettext.html#MO-Files
+ * @author Laurent Declercq <l.declercq@nuxwin.com>
+ * @version 0.0.1
+ */
+class iMSCP_I18n_Parser_Mo extends iMSCP_I18n_Parser
+{
+	/**
+	 * Byte ordering.
+	 *
+	 * @var string
+	 */
+	protected $_order;
+
+	/**
+	 * Number of strings in the file.
+	 *
+	 * @var int
+	 */
+	protected $_nbString;
+
+	/**
+	 * Index table of original strings (msgid)
+	 *
+	 * @var array
+	 */
+	protected $_msgidIndexTable;
+
+	/**
+	 * Index table of translated strings (msgstr)
+	 *
+	 * @var array
+	 */
+	protected $_msgstrIndexTable;
+
+	/**
+	 * Returns number of stranslated strings.
+	 *
+	 * @return int Number of translated strings
+	 */
+	public function getNumberOfTranslatedStrings()
+	{
+		if(null === $this->_nbString) {
+			$this->getHeaders();
+		}
+
+		return $this->_nbString - 1;
+	}
+
+	/**
+	 * Parse a machine object file.
+	 *
+	 * @throws iMSCP_i18n_Exception When file cannot be opened
+	 * @throws iMSCP_i18n_Exception When file have bad magic number
+	 * @throws iMSCP_i18n_Exception When file part to parse is unknow
+	 * @param int $part Part to parse - Can be either iMSCP_I18n_Parser::HEADERS or
+	 * 									iMSCP_I18n_Parser::TRANSLATION_TABLE
+	 * @return Array|string An array that represent a translation table or a string
+	 * 						that represent the headers
+	 */
+	protected function _parse($part)
+	{
+		if ($this->_fh === null) {
+			if (!($this->_fh = fopen($this->_filePath, 'rb'))) {
+				require_once 'iMSCP/I18n/Exception.php';
+				throw new iMSCP_i18n_Exception("Unable to open $this->_filePath");
+			}
+		}
+
+		if ($this->_order === null) {
+			// Magic number (byte 0 to 4)
+			$value = unpack('V', fread($this->_fh, 4));
+			$magicNumber = array_shift($value);
+
+			if ($magicNumber == (int)0x0950412de ||
+				dechex($magicNumber) == 'ffffffff950412de'
+			) {
+				$this->_order = 'V'; // low endian
+			} elseif($magicNumber == (int)0x0de120495) {
+				$this->_order = 'N'; // big endian
+			} else {
+				require_once 'iMSCP/I18n/Exception.php';
+				throw new iMSCP_i18n_Exception("Bad magic number in $this->_filePath");
+			}
+
+			// Skipping the revision number (byte 4 to 8)
+			fseek($this->_fh, 4, SEEK_CUR);
+
+			// number of strings (byte 8 to 12)
+			$value = unpack($this->_order, fread($this->_fh, 4));
+			$this->_nbString = array_shift($value);
+
+			// offset of table with original strings (byte 12 to 16)
+			$value = unpack($this->_order, fread($this->_fh, 4));
+			$msgidtableOffset = array_shift($value);
+
+			// offset of table with translation strings (byte 16 to 20)
+			$value = unpack($this->_order, fread($this->_fh, 4));
+			$msgstrTableOffset = array_shift($value);
+
+			// two integers per string (offset and size)
+			$count = $this->_nbString * 2;
+
+			// index of original strings
+			fseek($this->_fh, $msgidtableOffset);
+			$this->_msgidIndexTable = unpack(
+				$this->_order . $count, fread($this->_fh, ($count * 4)));
+
+			// index of translated strings
+			fseek($this->_fh, $msgstrTableOffset);
+			$this->_msgstrIndexTable = unpack(
+				$this->_order . $count, fread($this->_fh, ($count * 4)));
+
+		}
+
+		switch ((int)$part) {
+			case self::HEADERS:
+				$nbString = 1;
+				$index = 0;
+				break;
+			case self::TRANSLATION_TABLE:
+				$nbString = $this->_nbString;
+				$index = 1;
+				break;
+			default:
+				require_once 'iMSCP/I18n/Exception.php';
+				throw new iMSCP_i18n_Exception("Unknown part type to parse");
+		}
+
+		$parseResult = null;
+
+		for ($index; $index < $nbString; $index++) {
+			fseek($this->_fh, $this->_msgidIndexTable[($index * 2) + 2]);
+
+			if (!($length = $this->_msgidIndexTable[$index * 2 + 1])) {
+				$msgid = '__headers__';
+			} else {
+				$msgid = fread($this->_fh, $length);
+			}
+
+			fseek($this->_fh, $this->_msgstrIndexTable[$index * 2 + 2]);
+
+			if (!$length = $this->_msgstrIndexTable[$index * 2 + 1])
+				$msgstr = '';
+			else
+				$msgstr = fread($this->_fh, $length);
+
+			if ($msgid == '__headers__') {
+				$parseResult = $msgstr;
+			} else {
+				$parseResult[$msgid] = $msgstr;
+			}
+		}
+
+		return $parseResult;
+	}
+}
