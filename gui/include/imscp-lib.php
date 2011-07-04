fc720c0972e265ec1f409561f7ac10015d55fc19
nuxwin <nuxwin@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-07-04 04:24:28 +0000

    Gui: Removed check on SPL - As of PHP 5.3.0 this extension can no longer be disabled and is therefore always available.
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@4785 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

--- gui/include/imscp-lib.php
+++ gui/include/imscp-lib.php
@@ -53,10 +53,7 @@ ini_set('display_errors', 1);
  * Check PHP version (5.2.6 or newer ) and SPL availability
  */
 if (version_compare(phpversion(), '5.3.2', '<') === true) {
-	die('Your PHP version is ' . phpversion() . ". i-MSCP requires PHP 5.3.3 or newer.\n");
-} elseif(!extension_loaded('SPL')) {
-    die("Standard PHP Library (SPL) was not detected on your system.\n " .
-        'See http://php.net/manual/en/book.spl.php for more information');
+	die('Your PHP version is ' . phpversion() . ". i-MSCP requires PHP 5.3.2 or newer.\n");
 }
 
 // Define path for the i-MSCP include directory
