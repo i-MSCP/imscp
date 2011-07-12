1bce4520d336121b982e1662727cbd32e936e9f6
sci2tech <sci2tech@15a38060-5cfe-4f5c-bf9d-b5d42b1378a7>
2011-07-12 05:37:36 +0000

    Ubuntu method
    git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/branches/autoinstall@4910 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

---
+++ library/ubuntu_autoinstall.pm
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+# i-MSCP - internet Multi Server Control Panel
+# Copyright (C) 2010 by internet Multi Server Control Panel
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# @category		i-MSCP
+# @copyright	2010 - 2011 by i-MSCP | http://i-mscp.net
+# @author		Daniel Andreca <sci2tech@gmail.com>
+# @version		SVN: $Id: imscp-setup 4677 2011-06-23 19:01:39Z sci2tech $
+# @link			http://i-mscp.net i-MSCP Home Site
+# @license		http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+
+package iMSCP::ubuntu_autoinstall;
+
+use strict;
+use warnings;
+
+use iMSCP::Debug;
+use iMSCP::Execute qw/execute/;
+
+use vars qw/@ISA/;
+@ISA = ('Common::SingletonClass', 'iMSCP::debian_autoinstal');
+use Common::SingletonClass;
+use iMSCP::debian_autoinstal;
+
+sub _init{
+
+	my $self = shift;
+	debug((caller(0))[3].': Starting...');
+
+	$self->{nonfree} = 'multiverse';
+
+	debug((caller(0))[3].': Ending...');
+	0;
+}
+
+
+1;
