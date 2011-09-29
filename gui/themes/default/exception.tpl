i-MSCP/imscp

commit 697c17eb19c9e738cd292ce16febba4427b3d701

Some template improvements
git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@5345 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

gui/themes/default/exception.tpl
@@ -1,34 +1,25 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-	<head>
-		<title>{TR_EXCEPTION_PAGE_TITLE}</title>
-		<meta name="robots" content="nofollow, noindex">
-		<meta http-equiv="Content-Type" content="text/html; charset={THEME_CHARSET}">
-		<meta http-equiv="Content-Style-Type" content="text/css">
-		<link href="{THEME_COLOR_PATH}/css/login-imscp.css" rel="stylesheet" type="text/css">
-		<link href="{THEME_COLOR_PATH}/css/box.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<div class="header">
-			<div id="logo">
-				<div id="logoInner">
-					<img src="/themes/default/images/imscp_logo32.png" alt="{productLongName}" />
-					<span>{productLongName}</span>
-				</div>
-			</div>
-			<div id="copyright">
-				<div id="copyrightInner">
-					<a href="{productLink}" target="blank" tabindex="8">{productCopyright}</a>
-				</div>
+<!-- INCLUDE "header.tpl" -->
+<body>
+	<div class="header">
+		<div id="logo">
+			<div id="logoInner">
+				<img src="/themes/default/images/imscp_logo32.png" alt="{productLongName}" />
+				<span>{productLongName}</span>
 			</div>
 		</div>
-		<div id="box">
-			<div id="boxHeader"></div>
-			<div id="content">
-				<h1>{MESSAGE_TITLE}</h1>
-				<p>{MESSAGE}</p>
-				<a href="{BACK_BUTTON_DESTINATION}" target="_self">{TR_BACK}</a>
+		<div id="copyright">
+			<div id="copyrightInner">
+				<a href="{productLink}" target="blank" tabindex="8">{productCopyright}</a>
 			</div>
 		</div>
-	</body>
+	</div>
+	<div id="box">
+		<div id="boxHeader"></div>
+		<div id="content">
+			<h1>{MESSAGE_TITLE}</h1>
+			<p>{MESSAGE}</p>
+			<a href="{BACK_BUTTON_DESTINATION}" target="_self">{TR_BACK}</a>
+		</div>
+	</div>
+</body>
 </html>

commit a5eabacd179d26d5f366c784396cefa586057e30

Fixed: Exception layout is broken since r5345 - Revert back
git-svn-id: https://i-mscp.svn.sourceforge.net/svnroot/i-mscp/trunk@5346 15a38060-5cfe-4f5c-bf9d-b5d42b1378a7

gui/themes/default/exception.tpl
@@ -1,25 +1,34 @@
-<!-- INCLUDE "header.tpl" -->
-<body>
-	<div class="header">
-		<div id="logo">
-			<div id="logoInner">
-				<img src="/themes/default/images/imscp_logo32.png" alt="{productLongName}" />
-				<span>{productLongName}</span>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<title>{TR_EXCEPTION_PAGE_TITLE}</title>
+		<meta name="robots" content="nofollow, noindex">
+		<meta http-equiv="Content-Type" content="text/html; charset={THEME_CHARSET}">
+		<meta http-equiv="Content-Style-Type" content="text/css">
+		<link href="{THEME_COLOR_PATH}/css/login-imscp.css" rel="stylesheet" type="text/css">
+		<link href="{THEME_COLOR_PATH}/css/box.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="header">
+			<div id="logo">
+				<div id="logoInner">
+					<img src="/themes/default/images/imscp_logo32.png" alt="{productLongName}" />
+					<span>{productLongName}</span>
+				</div>
+			</div>
+			<div id="copyright">
+				<div id="copyrightInner">
+					<a href="{productLink}" target="blank" tabindex="8">{productCopyright}</a>
+				</div>
 			</div>
 		</div>
-		<div id="copyright">
-			<div id="copyrightInner">
-				<a href="{productLink}" target="blank" tabindex="8">{productCopyright}</a>
+		<div id="box">
+			<div id="boxHeader"></div>
+			<div id="content">
+				<h1>{MESSAGE_TITLE}</h1>
+				<p>{MESSAGE}</p>
+				<a href="{BACK_BUTTON_DESTINATION}" target="_self">{TR_BACK}</a>
 			</div>
 		</div>
-	</div>
-	<div id="box">
-		<div id="boxHeader"></div>
-		<div id="content">
-			<h1>{MESSAGE_TITLE}</h1>
-			<p>{MESSAGE}</p>
-			<a href="{BACK_BUTTON_DESTINATION}" target="_self">{TR_BACK}</a>
-		</div>
-	</div>
-</body>
+	</body>
 </html>