1b06aacc260fea494a28f5ee5dd15c8a65d92557
Laurent Declercq <l.declercq@nuxwin.com>
2011-11-25 14:31:37 +0100

    GUI: Re-factorization

--- gui/public/client/pma_auth.php
+++ gui/public/client/pma_auth.php
@@ -18,13 +18,13 @@
  * Portions created by Initial Developer are Copyright (C) 2010-2011
  * i-MSCP - internet Multi Server Control Panel. All Rights Reserved.
  *
- * @category    iMSCP
+ * @category	iMSCP
  * @package		iMSCP_Core
- * @subpackage  Client
- * @copyright   2010-2011 by ispCP | http://i-mscp.net
- * @author      Laurent Declercq <l.declercq@nuxwin.com>
- * @link        http://i-mscp.net i-MSCP Home Site
- * @license     http://www.mozilla.org/MPL/ MPL 1.1
+ * @subpackage	Client
+ * @copyright	2010-2011 by ispCP | http://i-mscp.net
+ * @author		Laurent Declercq <l.declercq@nuxwin.com>
+ * @link		http://i-mscp.net i-MSCP Home Site
+ * @license		http://www.mozilla.org/MPL/ MPL 1.1
  */
 
 /***********************************************************************************************************************
@@ -43,34 +43,27 @@
  * @author Laurent Declercq <l.declercq@nuxwin.com>
  * @access private
  * @param  int $dbUserId Database user unique identifier
- * @return array Array that contains login credentials or FALSE on failure
+ * @return mixed Array that contains login credentials, FALSE otherwise
  */
 function _getLoginCredentials($dbUserId)
 {
-	// @todo Should be optimized
 	$query = "
 		SELECT
 			`sqlu_name`, `sqlu_pass`
 		FROM
-			`sql_user`, `sql_database`, `domain`
+			`sql_user` `t1`
+		INNER JOIN
+			`domain` `t2` ON(`t2`.`domain_admin_id` = ?)
+		INNER JOIN
+			`sql_database` `t3` ON(`t3`.`domain_id` = `t2`.`domain_id`)
 		WHERE
-			`sql_user`.`sqld_id` = `sql_database`.`sqld_id`
+			`t1`.`sqld_id` = `t3`.`sqld_id`
 		AND
-			`sql_user`.`sqlu_id` = ?
-		AND
-			`sql_database`.`domain_id` = `domain`.`domain_id`
-		AND
-			`domain`.`domain_admin_id` = ?
+			`t1`.`sqlu_id` = ?
 	";
-	$stmt = exec_query($query, array($dbUserId, $_SESSION['user_id']));
+	$stmt = exec_query($query, array((int)$_SESSION['user_id'], $dbUserId));
 
-	if($stmt->rowCount() == 1) {
-		return array(
-			$stmt->fields['sqlu_name'],
-			$stmt->fields['sqlu_pass']);
-	} else {
-		return false;
-	}
+	return $stmt->fetchRow(PDO::FETCH_NUM);
 }
 
 /**
@@ -83,7 +76,7 @@ function _getLoginCredentials($dbUserId)
  */
 function _pmaCreateCookies($cookies)
 {
-	foreach($cookies as $cookie) {
+	foreach ($cookies as $cookie) {
 		header("Set-Cookie: $cookie", false);
 	}
 }
@@ -120,18 +113,18 @@ function pmaAuth($dbUserId)
 {
 	$credentials = _getLoginCredentials($dbUserId);
 
-	if($credentials) {
+	if ($credentials) {
 		$data = http_build_query(
 			array(
-				'pma_username' => $credentials[0],
-				'pma_password' => stripcslashes($credentials[1])));
+				 'pma_username' => $credentials[0],
+				 'pma_password' => stripcslashes($credentials[1])));
 	} else {
-		set_page_message(tr('Unknown SQL user id.'), 'error');
+		set_page_message(tr('Wrong SQL user identifier.'), 'error');
 		return false;
 	}
 
 	// Prepares PhpMyadmin absolute Uri to use
-	if(isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS'])) {
+	if (!empty($_SERVER['HTTPS'])) {
 		$port = ($_SERVER['SERVER_PORT'] != '443') ? ':' . $_SERVER['SERVER_PORT'] : '';
 		$pmaUri = "https://{$_SERVER['SERVER_NAME']}$port/pma/";
 	} else {
@@ -142,22 +135,22 @@ function pmaAuth($dbUserId)
 	// Set stream context (http) options
 	stream_context_get_default(
 		array(
-			'http' => array(
-				'method' => 'POST',
-				'header' => "Host: {$_SERVER['SERVER_NAME']}$port\r\n" .
-					"Content-Type: application/x-www-form-urlencoded\r\n" .
-					'Content-Length: ' . strlen($data) . "\r\n" .
-					"Connection: close\r\n\r\n",
-				'content' => $data,
-				'user_agent' => 'Mozilla/5.0',
-				'max_redirects' => 1)));
+			 'http' => array(
+				 'method' => 'POST',
+				 'header' => "Host: {$_SERVER['SERVER_NAME']}$port\r\n" .
+							 "Content-Type: application/x-www-form-urlencoded\r\n" .
+							 'Content-Length: ' . strlen($data) . "\r\n" .
+							 "Connection: close\r\n\r\n",
+				 'content' => $data,
+				 'user_agent' => 'Mozilla/5.0',
+				 'max_redirects' => 1)));
 
 	// Gets the headers from PhpMyAdmin
 	$headers = get_headers($pmaUri, true);
 
-	if($headers && isset($headers['Location'])) {
+	if ($headers && isset($headers['Location'])) {
 		_pmaCreateCookies($headers['Set-Cookie']);
-        redirectTo(_pmaSetLanguage($headers['Location']));
+		redirectTo(_pmaSetLanguage($headers['Location']));
 	}
 
 	set_page_message(tr('An error occurred while the authentication attempt.'), 'error');
@@ -176,18 +169,11 @@ iMSCP_Events_Manager::getInstance()->dispatch(iMSCP_Events::onClientScriptStart)
 // Check for login
 check_login(__FILE__);
 
-// If the feature is disabled, redirects in silent way
-if (!customerHasFeature('sql')) {
-    redirectTo('index.php');
-}
-
 /**
  *  Dispatches the request
  */
-if(isset($_GET['id'])) {
-	if(!pmaAuth((int) $_GET['id'])) {
-		redirectTo('sql_manage.php');
-	}
-} else {
-	redirectTo('/index.php');
+if(!customerHasFeature('sql')) {
+   redirectTo('index.php');
+} elseif(!isset($_GET['id']) || !pmaAuth((int)$_GET['id'])) {
+	redirectTo('sql_manage.php');
 }
