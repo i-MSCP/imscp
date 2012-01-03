e53ece58ec170be8847c6e78bdcae5cf7fb5df63
Laurent Declercq <l.declercq@nuxwin.com>
2012-01-03 09:59:07 +0100

    GUI: Customers SSL feature  - i18n issues  - Return back to referer if SSL feature is disabled and domain entity has not already a certificat  - DocBlocks + small refactorization

--- gui/public/client/cert_view.php
+++ gui/public/client/cert_view.php
@@ -27,51 +27,50 @@
  *
  */
 
-// Include core library
-require_once 'imscp-lib.php';
-iMSCP_Events_Manager::getInstance()->dispatch(iMSCP_Events::onClientScriptStart);
+/****************************************************************************
+ * Script functions
+ */
 
-check_login(__FILE__);
-
-/** @var $cfg iMSCP_Config_Handler_File */
-$cfg = iMSCP_Registry::get('config');
-
-$tpl = new iMSCP_pTemplate();
-$tpl->define_dynamic('layout', 'shared/layouts/ui.tpl');
-$tpl->define_dynamic('page', 'client/cert_view.tpl');
-$tpl->define_dynamic('page_message', 'page');
-$tpl->define_dynamic('cert_enable', 'page');
-
-if(!$cfg->ENABLE_SSL){
-	$tpl->assign(array('CERT_ENABLE' => ''));
-	set_page_message(tr('SSL is disabled. You can not add / change certificate'), 'info');
-}
-
-
-function getFullName($type, $id){
-	switch($type){
+/**
+ * Get full name for given domain entity.
+ *
+ * @param string $type Domain entity type to update (dmn, als,sub, alssub)
+ * @param int $id Domain entity unique identifier
+ * @return array
+ */
+function client_getFullName($type, $id)
+{
+	switch ($type) {
 		case 'dmn':
-			$query = 'SELECT `domain_name` AS `name`, `domain_admin_id` FROM `domain` WHERE `domain_id` = ?';
+			$query = 'SELECT `domain_name` `name`, `domain_admin_id` FROM `domain` WHERE `domain_id` = ?';
 			break;
 		case 'als':
-			$query = 'SELECT `alias_name` AS `name`, `domain_admin_id` FROM `domain_aliasses` LEFT JOIN `domain` USING(`domain_id`) WHERE `alias_id` = ?';
+			$query = 'SELECT `alias_name` `name`, `domain_admin_id` FROM `domain_aliasses` LEFT JOIN `domain` USING(`domain_id`) WHERE `alias_id` = ?';
 			break;
 		case 'sub':
-			$query = 'SELECT CONCAT(`subdomain_name`, \'.\', `domain_name`) AS `name`, `domain_admin_id` FROM `subdomain` LEFT JOIN `domain` USING(`domain_id`) WHERE `subdomain_id` = ?';
+			$query = 'SELECT CONCAT(`subdomain_name`, \'.\', `domain_name`) `name`, `domain_admin_id` FROM `subdomain` LEFT JOIN `domain` USING(`domain_id`) WHERE `subdomain_id` = ?';
 			break;
-		case 'alssub':
-			$query = 'SELECT CONCAT(`subdomain_alias_name`, \'.\', `alias_name`) AS `name`, `domain_admin_id` FROM `subdomain_alias` LEFT JOIN `domain_aliasses` USING(`alias_id`) LEFT JOIN `domain` USING(`domain_id`) WHERE `subdomain_alias_id` = ?';
+		default: // alssub
+			$query = 'SELECT CONCAT(`subdomain_alias_name`, \'.\', `alias_name`) `name`, `domain_admin_id` FROM `subdomain_alias` LEFT JOIN `domain_aliasses` USING(`alias_id`) LEFT JOIN `domain` USING(`domain_id`) WHERE `subdomain_alias_id` = ?';
 			break;
 	}
 	$stmt = exec_query($query, array($id));
+
 	return array($stmt->fields['name'], $stmt->fields['domain_admin_id']);
 }
 
-function updateStatus($type, $id){
+/**
+ * Udate status for given domain entity.
+ *
+ * @param string $type Domain entity type to update (dmn, als,sub, alssub)
+ * @param int $id Domain entity unique identifier
+ */
+function client_updateEntityStatus($type, $id)
+{
 	/** @var $cfg iMSCP_Config_Handler_File */
 	$cfg = iMSCP_Registry::get('config');
 
-	switch($type){
+	switch ($type) {
 		case 'dmn':
 			$query = 'UPDATE `domain` SET `domain_status` = ? WHERE `domain_id` = ?';
 			break;
@@ -81,154 +80,146 @@ function updateStatus($type, $id){
 		case 'sub':
 			$query = 'UPDATE `subdomain` SET `subdomain_status` = ? WHERE `subdomain_id` = ?';
 			break;
-		case 'alssub':
+		default: // alssub
 			$query = 'UPDATE `subdomain_alias` SET `subdomain_alias_status` = ? WHERE `subdomain_alias_id` = ?';
-			break;
 	}
-	$stmt = exec_query($query, array($cfg->ITEM_CHANGE_STATUS, $id));
-}
-/**
- * @param $tpl
- * @param $id
- * @param $type
- * @return
- */
-function gen_page_data($tpl, $id, $type) {
 
+	exec_query($query, array($cfg->ITEM_CHANGE_STATUS, $id));
+}
+
+/**
+ * Generate page.
+ *
+ * @param iMSCP_pTemplate $tpl Template engine instance
+ * @param int $id Domain entity unique identifier
+ * @param string $type Domain entity type
+ */
+function client_generatePage($tpl, $id, $type)
+{
 	/** @var $cfg iMSCP_Config_Handler_File */
 	$cfg = iMSCP_Registry::get('config');
 
-	list($name, $owner) = getFullName($type, $id);
+	list($name, $owner) = client_getFullName($type, $id);
 
-	if($owner !== $_SESSION['user_id']){
+	if ($owner !== $_SESSION['user_id']) {
 		set_page_message(tr('Wrong request.'), 'error');
 		redirectTo('domains_manage.php');
 	}
 
-	if(isset($_POST['Send']) && $cfg->ENABLE_SSL){
-		if($_POST['pass'] != $_POST['pass_rep']){
-			set_page_message(tr('Passwords do not match.'), 'error');
+	if (isset($_POST['send']) && $cfg->ENABLE_SSL) {
+		if ($_POST['pass'] != $_POST['pass_rep']) {
+			set_page_message(tr("Passwords doesn't not matches."), 'error');
 		}
-		if(!is_resource(@openssl_x509_read($_POST['cert_cert']))){
+
+		if (!is_resource(@openssl_x509_read($_POST['cert_cert']))) {
 			set_page_message(tr('Invalid certificate.'), 'error');
 		}
-		if($k = @openssl_pkey_get_private(array($_POST['key_cert'] , $_POST['pass'])) === false){
+
+		if (($k = @openssl_pkey_get_private(array($_POST['key_cert'], $_POST['pass']))) === false) {
 			set_page_message(tr('Invalid key or password.'), 'error');
 		}
-		if($k && @openssl_x509_check_private_key($_POST['cert_cert'] , $k)!== true){
-			set_page_message(tr('Certificate do not match key.'), 'error');
+
+		if ($k && @openssl_x509_check_private_key($_POST['cert_cert'], $k) !== true) {
+			set_page_message(tr("Certificate doesn't match key."), 'error');
 		}
-		if(!empty($_POST['ca_cert']) && !is_resource(@openssl_x509_read($_POST['ca_cert']))){
+
+		if (!empty($_POST['ca_cert']) && !is_resource(@openssl_x509_read($_POST['ca_cert']))) {
 			set_page_message(tr('Invalid intermediate certificate.'), 'error');
 		}
-		if(!Zend_Session::namespaceIsset('pageMessages')) {
+
+		if (!Zend_Session::namespaceIsset('pageMessages')) {
 			$query = "DELETE FROM `ssl_certs` WHERE `type` = ? AND `id` = ?";
 			exec_query($query, array($type, $id));
+
 			$query = "
-				INSERT INTO
-					`ssl_certs`
-				(
-					`id`,
-					`type`,
-					`password`,
-					`key`,
-					`cert`,
-					`ca_cert`,
-					`status`
+				INSERT INTO `ssl_certs` (
+					id`, `type`, `password`, `key`, `cert`, `ca_cert`, `status`
 				) VALUES (
 					?, ?, ?, ?, ?, ?, ?
-				);
-			";
-			exec_query(
-				$query,
-				array(
-					$id,
-					$type,
-					$_POST['pass'],
-					$_POST['key_cert'],
-					$_POST['cert_cert'],
-					$_POST['ca_cert'],
-					$cfg->ITEM_ADD_STATUS
 				)
-			);
-			updateStatus($type, $id);
-
-			set_page_message(tr('Certificate was saved.'), 'info');
-			write_log($_SESSION['user_logged'] . ": adds new certificate for: " . $name, E_USER_NOTICE);
+			";
+			exec_query($query, array($id, $type, $_POST['pass'], $_POST['key_cert'], $_POST['cert_cert'], $_POST['ca_cert'], $cfg->ITEM_ADD_STATUS));
+			client_updateEntityStatus($type, $id);
+			set_page_message(tr('Certificate successfully scheduled for addition or modification.'), 'success');
+			write_log($_SESSION['user_logged'] . ": added new certificate for: " . $name, E_USER_NOTICE);
 			send_request();
 		}
-	} elseif(isset($_POST['Delete'])){
+	} elseif (isset($_POST['delete'])) {
 		$query = " UPDATE `ssl_certs` SET `status` = ? WHERE `type` = ? AND `id` = ? ";
 		exec_query($query, array($cfg->ITEM_DELETE_STATUS, $type, $id));
-		updateStatus($type, $id);
-
-		set_page_message(tr('Certificate was deleted.'), 'info');
-		write_log($_SESSION['user_logged'] . ": delete certificate for: " . $name, E_USER_NOTICE);
+		client_updateEntityStatus($type, $id);
+		set_page_message(tr('Certificate sucessfully scheduled for deletion.'), 'success');
+		write_log($_SESSION['user_logged'] . ": deleted certificate for: " . $name, E_USER_NOTICE);
 		send_request();
 	}
 
-	$query = "
-		SELECT * FROM `ssl_certs`
-		WHERE `type` = ? AND `id` = ?
-	";
+	$query = "SELECT * FROM `ssl_certs` WHERE `type` = ? AND `id` = ?";
 	$stmt = exec_query($query, array($type, $id));
 
-	if (!$stmt->fields['status']) {
-		$status = tr('Certificate do not exists');
-	} elseif (
-		in_array(
-			$stmt->fields['status'],
-			array(
-				$cfg->ITEM_OK_STATUS,
-				$cfg->ITEM_DELETE_STATUS,
-				$cfg->ITEM_ADD_STATUS,
-				$cfg->ITEM_CHANGE_STATUS
-			)
-		)
-	) {
+	if (!$stmt->rowCount()) {
+		if ($cfg->ENABLE_SSL) {
+			$status = tr('No certificate found.');
+		} else {
+			$tpl->assign('CERT_ENABLE', '');
+			set_page_message(tr('SSL feature is disabled. You cannot add / change certificate'), 'warning');
+		}
+	} elseif (in_array($stmt->fields['status'], array($cfg->ITEM_OK_STATUS, $cfg->ITEM_DELETE_STATUS, $cfg->ITEM_ADD_STATUS, $cfg->ITEM_CHANGE_STATUS))) {
 		$status = translate_dmn_status($stmt->fields['status']);
 	} else {
-		$status = tr('Error'). ': '. $stmt->fields['status'];
+		$status = tr('Error') . ': ' . $stmt->fields['status'];
 	}
 
-	$tpl->assign(
-		array(
-			'DOMAIN_NAME' => $name,
-			'KEY_CERT' => isset($_POST['Send']) && isset($_POST['key_cert']) ? $_POST['key_cert'] : ($stmt->fields['key'] ? $stmt->fields['key'] : ''),
-			'CERT' => isset($_POST['Send']) && isset($_POST['cert_cert']) ? $_POST['cert_cert'] : ($stmt->fields['cert'] ? $stmt->fields['cert'] : ''),
-			'CA_CERT' => isset($_POST['Send']) && isset($_POST['ca_cert']) ? $_POST['ca_cert'] : ($stmt->fields['ca_cert'] ? $stmt->fields['ca_cert'] : ''),
-			'STATUS' => $status
-		)
-	);
+	if (isset($status)) {
+		$tpl->assign(
+			array(
+				'DOMAIN_NAME' => $name,
+				'KEY_CERT' => isset($_POST['send']) && isset($_POST['key_cert'])
+					? $_POST['key_cert'] : ($stmt->fields['key'] ? $stmt->fields['key'] : ''),
+				'CERT' => isset($_POST['send']) && isset($_POST['cert_cert'])
+					? $_POST['cert_cert'] : ($stmt->fields['cert'] ? $stmt->fields['cert'] : ''),
+				'CA_CERT' => isset($_POST['send']) && isset($_POST['ca_cert'])
+					? $_POST['ca_cert'] : ($stmt->fields['ca_cert'] ? $stmt->fields['ca_cert'] : ''),
+				'STATUS' => $status));
+	} else {
+		redirectTo('domains_manage.php');
+	}
 }
 
+/****************************************************************************
+ * Main script
+ */
+
+// Include core library
+require_once 'imscp-lib.php';
+
+iMSCP_Events_Manager::getInstance()->dispatch(iMSCP_Events::onClientScriptStart);
+
+check_login(__FILE__);
+
+/** @var $cfg iMSCP_Config_Handler_File */
+$cfg = iMSCP_Registry::get('config');
+
+$tpl = new iMSCP_pTemplate();
+$tpl->define_dynamic(
+	array(
+		'layout' => 'shared/layouts/ui.tpl',
+		'page' => 'client/cert_view.tpl',
+		'page_message' => 'layout',
+		'cert_enable' => 'page'));
+
+if (!isset($_GET['id']) || !isset($_GET['type']) || !in_array($_GET['type'], array('dmn', 'als', 'sub', 'alssub'))) {
+	set_page_message(tr('Wrong request.'), 'error');
+	redirectTo('domains_manage.php');
+} else {
+	$id = intval($_GET['id']);
+	$type = $_GET['type'];
+}
 
 $tpl->assign(
 	array(
 		'TR_PAGE_TITLE' => tr('i-MSCP - Client/Manage certificates'),
 		'THEME_CHARSET' => tr('encoding'),
-		'ISP_LOGO' => layout_getUserLogo()
-	)
-);
-
-if (
-	!isset($_GET['id']) ||
-	!isset($_GET['type']) ||
-	!in_array($_GET['type'], array('dmn', 'als', 'sub', 'alssub'))
-) {
-	set_page_message(tr('Wrong request.'), 'error');
-	redirectTo('domains_manage.php');
-} else {
-	$id		= intval($_GET['id']);
-	$type	= $_GET['type'];
-}
-
-gen_page_data($tpl, $id, $type);
-
-generateNavigation($tpl);
-
-$tpl->assign(
-	array(
+		'ISP_LOGO' => layout_getUserLogo(),
 		'TR_CERTIFICATE_DATA' => tr('Certificate data'),
 		'TR_CERT_FOR' => tr('Certificate for'),
 		'TR_STATUS' => tr('Status'),
@@ -241,10 +232,10 @@ $tpl->assign(
 		'TR_SAVE' => tr('Save'),
 		'TR_CANCEL' => tr('Cancel'),
 		'ID' => $id,
-		'TYPE' => $type
-	)
-);
+		'TYPE' => $type));
 
+generateNavigation($tpl);
+client_generatePage($tpl, $id, $type);
 generatePageMessage($tpl);
 
 $tpl->parse('LAYOUT_CONTENT', 'page');
--- gui/themes/default/client/cert_view.tpl
+++ gui/themes/default/client/cert_view.tpl
@@ -1,12 +1,11 @@
+
 			<form name="cert_edit" method="post" action="cert_view.php?id={ID}&type={TYPE}">
-				<table>
+				<table class="firstColFixed">
 					<tr>
 						<th colspan="2">{TR_CERTIFICATE_DATA}</th>
 					</tr>
 					<tr>
-						<td>
-							{TR_CERT_FOR}
-						</td>
+						<td>{TR_CERT_FOR}</td>
 						<td>{DOMAIN_NAME}</td>
 					</tr>
 					<tr>
@@ -22,35 +21,23 @@
 						<td><input id="pass_rep" type="password" name="pass_rep" value="" /></td>
 					</tr>
 					<tr>
-						<td>
-							<label for="key_cert">{TR_CERTIFICATE_KEY}</label>
-						</td>
-						<td>
-							<textarea name="key_cert" id="key_cert" cols="35" rows="5" style="height:150px;">{KEY_CERT}</textarea>
-						</td>
+						<td><label for="key_cert">{TR_CERTIFICATE_KEY}</label></td>
+						<td><textarea name="key_cert" id="key_cert">{KEY_CERT}</textarea></td>
 					</tr>
 					<tr>
-						<td>
-							<label for="cert_cert">{TR_CERTIFICATE}</label>
-						</td>
-						<td>
-							<textarea name="cert_cert" id="cert_cert" cols="35" rows="5" style="height:150px;">{CERT}</textarea>
-						</td>
+						<td><label for="cert_cert">{TR_CERTIFICATE}</label></td>
+						<td><textarea name="cert_cert" id="cert_cert">{CERT}</textarea></td>
 					</tr>
 					<tr>
-						<td>
-							<label for="ca_cert">{TR_INTERM_CERTIFICATE}</label>
-						</td>
-						<td>
-							<textarea name="ca_cert" id="ca_cert" cols="35" rows="5" style="height:150px;">{CA_CERT}</textarea>
-						</td>
+						<td><label for="ca_cert">{TR_INTERM_CERTIFICATE}</label></td>
+						<td><textarea name="ca_cert" id="ca_cert">{CA_CERT}</textarea></td>
 					</tr>
 				</table>
 				<div class="buttons">
 					<!-- BDP: cert_enable -->
-					<input name="Send" type="submit" value="{TR_SAVE}" />
+					<input name="send" type="submit" value="{TR_SAVE}" />
 					<!-- EDP: cert_enable -->
-					<input name="Delete" type="submit" value="{TR_DELETE}" />
-					<input name="Button" type="button" onclick="MM_goToURL('parent','domains_manage.php');return document.MM_returnValue" value="{TR_CANCEL}" />
+					<input name="delete" type="submit" value="{TR_DELETE}" />
+					<input name="cancel" type="button" onclick="MM_goToURL('parent','domains_manage.php');return document.MM_returnValue" value="{TR_CANCEL}" />
 				</div>
 			</form>
