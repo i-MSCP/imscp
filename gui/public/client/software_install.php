7f3a73cf268dd99a9136a3aaf174cecbfca0ebbc
Laurent Declercq <l.declercq@nuxwin.com>
2012-01-10 13:23:58 +0100

    GUI: Small fixes (HTML, Navigation, typo + refactoing)

--- gui/public/client/software_install.php
+++ gui/public/client/software_install.php
@@ -27,25 +27,33 @@
  */
 
 /************************************************************************************
- *  Functions
+ *  Script functions
  */
 
 /**
- * @param $tpl
- * @param $user_id
- * @return
+ * Generate Page.
+ *
+ * @param iMSCP_pTemplate $tpl Template engine instance
+ * @param int $customerId Customer unique identifier
+ * @return int Software unique identifier
  */
-function gen_page_lists($tpl, $user_id) {
-	if (!isset($_GET['id']) || $_GET['id'] === '' || !is_numeric($_GET['id'])) {
-		set_page_message(tr('Software not found!'), 'error');
+function client_generatePage($tpl, $customerId)
+{
+	if (!isset($_GET['id']) || $_GET['id'] == '' || !is_numeric($_GET['id'])) {
+		set_page_message(tr('Wrong request.'), 'error');
 		redirectTo('software.php');
-		exit;
+		exit; // Uselesss but avoid IDE warning about possible undefined variable
 	} else {
-		$software_id = $_GET['id'];
+		$softwareId = intval($_GET['id']);
 	}
-    list($dmn_id,$dmn_name,,,$dmn_created_id,,,,,,$dmn_sqld_limit) = get_domain_default_props($user_id);
-	get_software_props_install ($tpl, $dmn_id, $software_id, $dmn_created_id, $dmn_sqld_limit);
-	return $software_id;
+
+	$domainProperties = get_domain_default_props($customerId, true);
+
+	get_software_props_install(
+		$tpl, $domainProperties['domain_id'], $softwareId, $domainProperties['domain_created_id'],
+		$domainProperties['domain_sqld_limit']);
+
+	return $softwareId;
 }
 
 /************************************************************************************
@@ -68,78 +76,67 @@ if (!customerHasFeature('aps')) {
 $cfg = iMSCP_Registry::get('config');
 
 $tpl = new iMSCP_pTemplate();
-$tpl->define_dynamic('layout', 'shared/layouts/ui.tpl');
-$tpl->define_dynamic('page', 'client/software_install.tpl');
-$tpl->define_dynamic('page_message', 'layout');
-$tpl->define_dynamic('software_item', 'page');
-$tpl->define_dynamic('show_domain_list', 'page');
-$tpl->define_dynamic('software_install', 'page');
-$tpl->define_dynamic('no_software', 'page');
-$tpl->define_dynamic('installdb_item', 'page');
-$tpl->define_dynamic('select_installdb', 'page');
-$tpl->define_dynamic('require_installdb', 'page');
-$tpl->define_dynamic('select_installdbuser', 'page');
-$tpl->define_dynamic('installdbuser_item', 'page');
-$tpl->define_dynamic('softwaredbuser_message', 'page');
-$tpl->define_dynamic('create_db', 'page');
-$tpl->define_dynamic('create_message_db', 'page');
+$tpl->define_dynamic(
+	array(
+		'layout' => 'shared/layouts/ui.tpl',
+		'page' => 'client/software_install.tpl',
+		'page_message' => 'layout',
+		'software_item' => 'page',
+		'show_domain_list' => 'page',
+		'software_install' => 'page',
+		'no_software' => 'page',
+		'installdb_item' => 'page',
+		'select_installdb' => 'page',
+		'require_installdb' => 'page',
+		'select_installdbuser' => 'page',
+		'installdbuser_item' => 'page',
+		'softwaredbuser_message' => 'page',
+		'create_db' => 'page',
+		'create_message_db' => 'page'));
 
 if (isset($_POST['Submit2'])) {
-	$id = $_GET['id'];
-	$domain_path = "";
+	$id = intval($_GET['id']);
+	$domain_path = '';
 	$other_dir = clean_input($_POST['other_dir'], true);
 
 	$query = "
 		SELECT
-			`software_master_id`, `software_db`, `software_name`, `software_version`,
-			`software_language`, `software_depot`
+			`software_master_id`, `software_db`, `software_name`, `software_version`, `software_language`,
+			`software_depot`
 		FROM
 			`web_software`
 		WHERE
 			`software_id` = ?
 	";
-	$rs = exec_query($query, $_GET['id']);
+	$stmt = exec_query($query, $_GET['id']);
 
 	$install_username = clean_input($_POST['install_username'], true);
 	$install_password = clean_input($_POST['install_password'], true);
 	$install_email = clean_input($_POST['install_email'], true);
-	if(isset($_POST['createdir']) && $_POST['createdir'] === '1') {
+
+	if(isset($_POST['createdir']) && $_POST['createdir'] == '1') {
 		$createdir = clean_input($_POST['createdir'], true);
-		set_page_message(tr('The directory -%1$s- was created.', $other_dir), 'success');
+		set_page_message(tr('The directory %s was created.', $other_dir), 'success');
 	} else {
 		$createdir = '0';
 	}
-	//Check dir exists
+
+	// Check dir exists
     $domain = $_SESSION['user_logged'];
     $vfs = new iMSCP_VirtualFileSystem($domain);
     $list = $vfs->ls($other_dir);
-    //Check dir exists
 
-	list($dmn_id,
-		$dmn_name,
-		$dmn_gid,
-		$dmn_uid,
-		$dmn_created_id,
-		$dmn_created,
-		$dmn_last_modified,
-		$dmn_mailacc_limit,
-		$dmn_ftpacc_limit,
-		$dmn_traff_limit,
-		$dmn_sqld_limit,
-		$dmn_sqlu_limit,
-		$dmn_status,
-		$dmn_als_limit,
-		$dmn_subd_limit,
-		$dmn_ip_id,
-		$dmn_disk_limit,
-		$dmn_disk_usage,
-		$dmn_php,
-		$dmn_cgi) = get_domain_default_props($_SESSION['user_id']);
+    // Check dir exists
+	list(
+		$dmn_id, $dmn_name, $dmn_gid, $dmn_uid, $dmn_created_id, $dmn_created, $dmn_last_modified, $dmn_mailacc_limit,
+		$dmn_ftpacc_limit, $dmn_traff_limit, $dmn_sqld_limit, $dmn_sqlu_limit, $dmn_status, $dmn_als_limit,
+		$dmn_subd_limit, $dmn_ip_id, $dmn_disk_limit, $dmn_disk_usage, $dmn_php, $dmn_cgi
+	) = get_domain_default_props($_SESSION['user_id']);
 
 
-	$querypath = "
+	$query = "
 		SELECT
-			`software_name` as swname, `software_version` as swversion
+			`software_name` `swname`, `software_version` `swversion`
 		FROM
 			`web_software_inst`
 		WHERE
@@ -147,44 +144,29 @@ if (isset($_POST['Submit2'])) {
 		AND
 			`path` = ?
 	";
-	$rspath = exec_query($querypath, array($dmn_id, $other_dir));
+	$rspath = exec_query($query, array($dmn_id, $other_dir));
 
-	list ($posted_domain_id, $posted_aliasdomain_id, $posted_subdomain_id, $posted_aliassubdomain_id, $posted_mountpath) = explode(';', $_POST['selected_domain']);
+	list (
+		$posted_domain_id, $posted_aliasdomain_id, $posted_subdomain_id, $posted_aliassubdomain_id, $posted_mountpath
+	) = explode(';', $_POST['selected_domain']);
+
 	if(($posted_aliasdomain_id + $posted_subdomain_id + $posted_aliassubdomain_id) > 0){
 		if($posted_aliasdomain_id > 0){
-			$querydomainpath = "
-				SELECT
-					`alias_mount` as domainpath
-				FROM
-					`domain_aliasses`
-				WHERE
-					`alias_id` = ?
-			";
-			$rsdomainpath = exec_query($querydomainpath, $posted_aliasdomain_id);
+			$query = "SELECT `alias_mount` `domainpath` FROM `domain_aliasses` WHERE `alias_id` = ?";
+			$rsdomainpath = exec_query($query, $posted_aliasdomain_id);
+
 			$domain_path = $rsdomainpath->fields['domainpath'];
 			$domain_path = str_replace("/", "\/", $domain_path);
 		} elseif($posted_subdomain_id > 0){
-			$querydomainpath = "
-				SELECT
-					`subdomain_mount` as domainpath
-				FROM
-					`subdomain`
-				WHERE
-					`subdomain_id` = ?
-			";
-			$rsdomainpath = exec_query($querydomainpath, $posted_subdomain_id);
+			$query = "SELECT `subdomain_mount` `domainpath` FROM `subdomain` WHERE `subdomain_id` = ?";
+			$rsdomainpath = exec_query($query, $posted_subdomain_id);
+
 			$domain_path = $rsdomainpath->fields['domainpath'];
 			$domain_path = str_replace("/", "\/", $domain_path);
 		} elseif($posted_aliassubdomain_id > 0){
-			$querydomainpath = "
-				SELECT
-					`subdomain_alias_mount` as domainpath
-				FROM
-					`subdomain_alias`
-				WHERE
-					`subdomain_alias_id` = ?
-			";
-			$rsdomainpath = exec_query($querydomainpath, $posted_aliassubdomain_id);
+			$query = "SELECT `subdomain_alias_mount` `domainpath` FROM `subdomain_alias` WHERE `subdomain_alias_id` = ?";
+			$rsdomainpath = exec_query($query, $posted_aliassubdomain_id);
+
 			$domain_path = $rsdomainpath->fields['domainpath'];
 			$domain_path = str_replace("/", "\/", $domain_path);
 		} else {
@@ -194,48 +176,48 @@ if (isset($_POST['Submit2'])) {
 		$domain_path = $posted_mountpath;
 	}
 
-	if($rs->fields['software_db'] == "1") {
+	if($stmt->fields['software_db'] == "1") {
 		$selected_db = clean_input($_POST['selected_db'], true);
 		$sql_user = clean_input($_POST['sql_user'], true);
-		$querydbuser = "SELECT `sqlu_pass` FROM `sql_user` WHERE `sqlu_name` = ?";
-		$rsdatabase = exec_query($querydbuser, $sql_user);
+		$query = "SELECT `sqlu_pass` FROM `sql_user` WHERE `sqlu_name` = ?";
+		$rsdatabase = exec_query($query, $sql_user);
 
 		$db_connection_ok = check_db_connection($selected_db, $sql_user, $rsdatabase->fields['sqlu_pass']);
 		$sql_pass = $rsdatabase->fields['sqlu_pass'];
 	}
 
-	if($rs->fields['software_db'] == "1" && !$db_connection_ok) {
-		set_page_message(tr('Please select the correct user for your database!'), 'error');
+	if($stmt->fields['software_db'] == '1' && !$db_connection_ok) {
+		set_page_message(tr('Please select a valid  SQL user for the database.'), 'error');
 	} elseif(empty($install_username) || empty($install_password) || empty($install_email)) {
-		set_page_message(tr('You have to fill out inputs!'), 'error');
+		set_page_message(tr('All fields are required.'), 'error');
 	} elseif (!chk_password($install_password)){
 		if ($cfg->PASSWD_STRONG) {
 			set_page_message(sprintf(tr('The password must be at least %s long and contain letters and numbers to be valid.'), $cfg->PASSWD_CHARS), 'error');
 		} else {
 			set_page_message(sprintf(tr('Password data is shorter than %s signs or includes not permitted signs!'), $cfg->PASSWD_CHARS), 'error');
 		}
-	} elseif(!preg_match("/htdocs/",$other_dir)){
-		set_page_message(tr('You cant\'t install outside from htdocs!'), 'error');
+	} elseif(!preg_match("/htdocs/", $other_dir)){
+		set_page_message(tr("You cant't install the software outside the htdocs directory."), 'error');
 	} elseif(($posted_aliasdomain_id + $posted_subdomain_id + $posted_aliassubdomain_id) > 0 && !preg_match("/".$domain_path."/",$other_dir)){
-		set_page_message(tr('You choose a directory, which doesn\'t match with the domain directory!'), 'error');
-	} elseif(!$list && $createdir === '0'){
-			set_page_message(tr('The directory -%1$s- doesn\'t exist. Please create it!', $other_dir), 'error');
+		set_page_message(tr("You have chosen a directory which doesn't matches the domain directory."), 'error');
+	} elseif(!$list && $createdir == '0'){
+		set_page_message(tr("The directory %s doesn't exist. Please create it first.", $other_dir), 'error');
 	} elseif ($rspath->recordCount() > 0) {
-		set_page_message(tr('Please select another directory! %1$s (%2$s) is installed there!', $rspath->fields['swname'], $rspath->fields['swversion']), 'error');
+		set_page_message(tr('Please select another directory. %s (%s) is installed there.', $rspath->fields['swname'], $rspath->fields['swversion']), 'error');
 	} else {
-		$sw_db_required = $rs->fields['software_db'];
-		$sw_software_name = $rs->fields['software_name'];
-		$sw_software_version = $rs->fields['software_version'];
-		$software_master_id = $rs->fields['software_master_id'];
-		$software_depot = $rs->fields['software_depot'];
-		$software_language = $rs->fields['software_language'];
-
+		$sw_db_required = $stmt->fields['software_db'];
+		$sw_software_name = $stmt->fields['software_name'];
+		$sw_software_version = $stmt->fields['software_version'];
+		$software_master_id = $stmt->fields['software_master_id'];
+		$software_depot = $stmt->fields['software_depot'];
+		$software_language = $stmt->fields['software_language'];
 
 		$query = "SELECT `software_prefix` FROM `web_software` WHERE `software_id` = ?";
-		$rs = exec_query($query, $_GET['id']);
+		$stmt = exec_query($query, $_GET['id']);
 
-		$prefix = $rs->fields['software_prefix'];
-		if($sw_db_required == "1") {
+		$prefix = $stmt->fields['software_prefix'];
+
+		if($sw_db_required == '1') {
 			$query="
 				INSERT INTO `web_software_inst` (
                     `domain_id`, `alias_id`, `subdomain_id`, `subdomain_alias_id`,
@@ -247,39 +229,42 @@ if (isset($_POST['Submit2'])) {
                     ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
                 )
 			";
-			$rs = exec_query($query, array($dmn_id, $posted_aliasdomain_id,
-                                          $posted_subdomain_id,
-                                          $posted_aliassubdomain_id, $id,
-                                          $software_master_id, $sw_software_name,
-                                          $sw_software_version, $software_language,
-                                          $other_dir, $prefix, $selected_db,
-                                          $sql_user, $sql_pass, $install_username,
-                                          $install_password,
-                                          $install_email, $cfg->ITEM_ADD_STATUS,
-                                          $software_depot));
+			$stmt = exec_query(
+				$query,
+				array(
+					$dmn_id, $posted_aliasdomain_id, $posted_subdomain_id, $posted_aliassubdomain_id, $id,
+					$software_master_id, $sw_software_name, $sw_software_version, $software_language,
+					$other_dir, $prefix, $selected_db, $sql_user, $sql_pass, $install_username, $install_password,
+					$install_email, $cfg->ITEM_ADD_STATUS, $software_depot));
 		} else {
 			$query="
 				INSERT INTO `web_software_inst` (
-                    `domain_id`, `alias_id`, `subdomain_id`, `subdomain_alias_id`,
-                    `software_id`, `software_master_id`, `software_name`,
-                    `software_version`, `software_language`, `path`, `software_prefix`,
-                    `db`, `database_user`, `database_tmp_pwd`, `install_username`,
-                    `install_password`, `install_email`, `software_status`, `software_depot`
+                    `domain_id`, `alias_id`, `subdomain_id`, `subdomain_alias_id`, `software_id`, `software_master_id`,
+                    `software_name`, `software_version`, `software_language`, `path`, `software_prefix`, `db`,
+                    `database_user`, `database_tmp_pwd`, `install_username`, `install_password`, `install_email`,
+                    `software_status`, `software_depot`
                 ) VALUES (
                     ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
                 )
 			";
-			$rs = exec_query($query, array($dmn_id, $posted_aliasdomain_id, $posted_subdomain_id, $posted_aliassubdomain_id, $id, $software_master_id, $sw_software_name, $sw_software_version, $software_language, $other_dir, "not_required", "not_required", "not_required", "not_required", $install_username, $install_password, $install_email, $cfg->ITEM_ADD_STATUS, $software_depot));
+			$stmt = exec_query(
+				$query,
+				array(
+					$dmn_id, $posted_aliasdomain_id, $posted_subdomain_id, $posted_aliassubdomain_id, $id,
+					$software_master_id, $sw_software_name, $sw_software_version, $software_language, $other_dir,
+					"not_required", "not_required", "not_required", "not_required", $install_username, $install_password,
+					$install_email, $cfg->ITEM_ADD_STATUS, $software_depot));
 		}
+
 		send_request();
 		redirectTo('software.php');
 	}
 
-	if($rs->fields['software_db'] == "1") {
+	if($stmt->fields['software_db'] == '1') {
 		$tpl->assign(
 			array(
 				 'VAL_OTHER_DIR' => $other_dir,
-				 'CHECKED_CREATEDIR' => ($createdir === '1') ? $cfg->HTML_CHECKED : '',
+				 'CHECKED_CREATEDIR' => ($createdir == '1') ? $cfg->HTML_CHECKED : '',
 				 'VAL_INSTALL_USERNAME' => $install_username,
 				 'VAL_INSTALL_PASSWORD' => $install_password,
 				 'VAL_INSTALL_EMAIL' => $install_email));
@@ -287,7 +272,7 @@ if (isset($_POST['Submit2'])) {
 		$tpl->assign(
 			array(
 				 'VAL_OTHER_DIR' => $other_dir,
-				 'CHECKED_CREATEDIR' => ($createdir === '1') ? $cfg->HTML_CHECKED : '',
+				 'CHECKED_CREATEDIR' => ($createdir == '1') ? $cfg->HTML_CHECKED : '',
 				 'VAL_INSTALL_USERNAME' => $install_username,
 				 'VAL_INSTALL_PASSWORD' => $install_password,
 				 'VAL_INSTALL_EMAIL' => $install_email));
@@ -304,20 +289,11 @@ if (isset($_POST['Submit2'])) {
 
 $tpl->assign(
 	array(
-		 'TR_PAGE_TITLE' => tr('i-MSCP - Install Software'),
-		 'THEME_CHARSET' => tr('encoding'),
-		 'ISP_LOGO' => layout_getUserLogo()));
-
-$software_id = gen_page_lists($tpl, $_SESSION['user_id']);
-
-generateNavigation($tpl);
-
-//get_client_software_permission ($tpl, $_SESSION['user_id']);
-
-$tpl -> assign(
-	array(
+		'TR_PAGE_TITLE' => tr('i-MSCP - Install Software'),
+		'THEME_CHARSET' => tr('encoding'),
+		'ISP_LOGO' => layout_getUserLogo(),
 		'TR_INSTALL_SOFTWARE' => tr('Install Software'),
-		'SOFTWARE_ID' => $software_id,
+		'SOFTWARE_ID' => client_generatePage($tpl, $_SESSION['user_id']),
 		'TR_NAME' => tr('Software'),
 		'TR_TYPE' => tr('Type'),
 		'TR_DB' => tr('Database required'),
@@ -337,6 +313,7 @@ $tpl -> assign(
 		'TR_INSTALL_PWD' => tr('Login password'),
 		'TR_INSTALL_EMAIL' => tr('Email address')));
 
+generateNavigation($tpl);
 generatePageMessage($tpl);
 
 $tpl->parse('LAYOUT_CONTENT', 'page');
--- gui/themes/default/client/software_install.tpl
+++ gui/themes/default/client/software_install.tpl
@@ -1,121 +1,116 @@
 
-		<script language="JavaScript" type="text/JavaScript">
-		/*<![CDATA[*/
-			function OpenTree() {
-				libwindow=window.open("ftp_choose_dir.php","Hello","menubar=no,width=470,height=350,scrollbars=yes");
+			<script language="JavaScript" type="text/JavaScript">
+				/*<![CDATA[*/
+				function openTree() {
+					libwindow=window.open("ftp_choose_dir.php","Hello","menubar=no,width=470,height=350,scrollbars=yes");
+				}
 
-			}
-			function set_installpath() {
-				var inputvars = document.forms[0].elements['selected_domain'].value;
-				inputvars = inputvars.toLowerCase();
-				var splitinputvars = inputvars.split(";");
-				document.forms[0].elements['other_dir'].value = splitinputvars[4];
-			}
-		/*]]>*/
-		</script>
-			<table>
-				<tr>
-					<td>
-						<form method="post" action="{SOFTWARE_INSTALL_BUTTON}">
-							<table>
-								<tr>
-									<th colspan="2">{TR_INSTALLATION}</th>
-								</tr>
-                    			<!-- BDP: software_item -->
-                    			<tr>
-									<td width="200">{TR_NAME}</td>
-		      						<td>{TR_SOFTWARE_NAME}</td>
-		    					</tr>
-								<tr>
-									<td width="200">{TR_TYPE}</td>
-									<td>{SOFTWARE_TYPE}</td>
-								</tr>
-								<tr>
-									<td width="200">{TR_DB}</td>
-									<td>{SOFTWARE_DB}</td>
-								</tr>
-								<tr>
-									<td width="200">{TR_SELECT_DOMAIN}</td>
-									<td>
-										<select name="selected_domain" id="selected_domain" onChange="set_installpath();">
-											<option value="{DOMAINSTANDARD_NAME_VALUES}">{DOMAINSTANDARD_NAME}</option>
-											<!-- BDP: show_domain_list -->
-											<option {SELECTED_DOMAIN} value="{DOMAIN_NAME_VALUES}">{DOMAIN_NAME}</option>
-											<!-- EDP: show_domain_list -->
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td width="200">{TR_PATH}</td>
-									<td>
-										<input type="text" name="other_dir" value="{VAL_OTHER_DIR}" style="width:170px" />&nbsp;<a href="javascript:OpenTree();">{CHOOSE_DIR}</a>&nbsp;(<input type="checkbox" name="createdir" value="1"{CHECKED_CREATEDIR} />{CREATEDIR_MESSAGE})
-									</td>
-		    					</tr>
-								<!-- BDP: require_installdb -->
-		    					<tr>
-									<td width="200">{TR_SELECT_DB}</td>
-									<td>
-										<!-- BDP: select_installdb -->
-										<select name="selected_db" id="selected_db">
-											<!-- BDP: installdb_item -->
-											<option {SELECTED_DB} value="{DB_NAME}">{DB_NAME}</option>
-											<!-- EDP: installdb_item -->
-										</select> 
-										<!-- EDP: select_installdb -->
-										<!-- BDP: create_db -->
-										<input name="Submit3" type="submit" class="button" onClick="MM_goToURL('parent','{ADD_DB_LINK}');return document.MM_returnValue" value="{BUTTON_ADD_DB}" /><!-- BDP: create_message_db -->&nbsp;<font color="#FF0000">{ADD_DATABASE_MESSAGE}</font><!-- EDP: create_message_db -->
-										<!-- EDP: create_db -->
-									</td>
-								</tr>
-								<tr>
-									<td width="200">{TR_SQL_USER}</td>
-									<td>
-										<!-- BDP: select_installdbuser -->
-										<select name="sql_user" id="sql_user">
-											<!-- BDP: installdbuser_item -->
-											<option {SELECTED_DBUSER} value="{SQLUSER_NAME}">{SQLUSER_NAME}</option>
-											<!-- EDP: installdbuser_item -->
-										</select> 
-										<!-- EDP: select_installdbuser -->
-										<!-- BDP: create_message_db -->
-										<font color="#FF0000">{ADD_DATABASE_MESSAGE}</font>
-										<!-- EDP: create_message_db -->
-										<!-- BDP: softwaredbuser_message -->
-										<font color="{STATUS_COLOR}">{SQLUSER_STATUS_MESSAGE}</font>
-										<!-- EDP: softwaredbuser_message -->
-									</td>
-								</tr>
-								<!-- EDP: require_installdb -->
-								<tr>
-								<th colspan="2">{TR_INSTALLATION_INFORMATION}</th>
-								</tr>
-								<tr>
-									<td width="200">{TR_INSTALL_USER}</td>
-									<td><input type="text" name="install_username" value="{VAL_INSTALL_USERNAME}" style="width:170px" /></td>
-								</tr>
-								<tr>
-									<td width="200">{TR_INSTALL_PWD}</td>
-									<td><input type="password" name="install_password" value="{VAL_INSTALL_PASSWORD}" style="width:170px" /></td>
-		    					</tr>
-								<tr>
-									<td width="200">{TR_INSTALL_EMAIL}</td>
-									<td>
-										<input type="text" name="install_email" value="{VAL_INSTALL_EMAIL}" style="width:170px" />
-									</td>
-		    					</tr>
-								<tr>
-									<td colspan="2">
-										<div class="buttons">
-											<input name="Submit" type="submit" class="button" onClick="MM_goToURL('parent','software.php');return document.MM_returnValue" value="{TR_BACK}" />
-											<!-- BDP: software_install -->
-											<input name="Submit2" type="submit" class="button" value="{TR_INSTALL}" />
-	                   						<!-- EDP: software_install -->
-                   						</div>
-									</td>
-								</tr>
-								<!-- EDP: software_item -->
-							</table>
-						</form>
-					</td>
-				</tr>
-			</table>
+				function setInstallPath() {
+					var inputvars = document.forms[0].elements['selected_domain'].value;
+					inputvars = inputvars.toLowerCase();
+					var splitinputvars = inputvars.split(";");
+					document.forms[0].elements['other_dir'].value = splitinputvars[4];
+				}
+				/*]]>*/
+			</script>
+			<form method="post" action="{SOFTWARE_INSTALL_BUTTON}">
+				<table class="firstColFixed">
+					<tr>
+						<th colspan="2">{TR_INSTALLATION}</th>
+					</tr>
+					<!-- BDP: software_item -->
+					<tr>
+						<td>{TR_NAME}</td>
+						<td>{TR_SOFTWARE_NAME}</td>
+					</tr>
+					<tr>
+						<td>{TR_TYPE}</td>
+						<td>{SOFTWARE_TYPE}</td>
+					</tr>
+					<tr>
+						<td>{TR_DB}</td>
+						<td>{SOFTWARE_DB}</td>
+					</tr>
+					<tr>
+						<td><label for="selected_domain">{TR_SELECT_DOMAIN}</label></td>
+						<td>
+							<select name="selected_domain" id="selected_domain" onchange="setInstallPath();">
+								<option value="{DOMAINSTANDARD_NAME_VALUES}">{DOMAINSTANDARD_NAME}</option>
+								<!-- BDP: show_domain_list -->
+								<option {SELECTED_DOMAIN} value="{DOMAIN_NAME_VALUES}">{DOMAIN_NAME}</option>
+								<!-- EDP: show_domain_list -->
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><label for="path">{TR_PATH}</label></td>
+						<td>
+							<input type="text" name="other_dir" id="path" value="{VAL_OTHER_DIR}"/>
+							<a href="javascript:openTree();">{CHOOSE_DIR}</a>
+							<input type="checkbox" name="createdir" id="createdir" value="1" {CHECKED_CREATEDIR}/>
+							<label for="createdir">{CREATEDIR_MESSAGE}</label>
+						</td>
+					</tr>
+					<!-- BDP: require_installdb -->
+					<tr>
+						<td><label for="selected_db">{TR_SELECT_DB}</label></td>
+						<td>
+							<!-- BDP: select_installdb -->
+							<select name="selected_db" id="selected_db">
+								<!-- BDP: installdb_item -->
+								<option {SELECTED_DB} value="{DB_NAME}">{DB_NAME}</option>
+								<!-- EDP: installdb_item -->
+							</select>
+							<!-- EDP: select_installdb -->
+							<!-- BDP: create_db -->
+							<input name="Submit3" type="submit" class="button" onClick="MM_goToURL('parent','{ADD_DB_LINK}');return document.MM_returnValue" value="{BUTTON_ADD_DB}"/>
+							<!-- EDP: create_db -->
+						</td>
+					</tr>
+					<tr>
+						<td><label for="sql_user">{TR_SQL_USER}<label</td>
+						<td>
+							<!-- BDP: select_installdbuser -->
+							<select name="sql_user" id="sql_user">
+								<!-- BDP: installdbuser_item -->
+								<option {SELECTED_DBUSER} value="{SQLUSER_NAME}">{SQLUSER_NAME}</option>
+								<!-- EDP: installdbuser_item -->
+							</select>
+							<!-- EDP: select_installdbuser -->
+
+							<!-- BDP: create_message_db -->
+							<span style="color:#ff0000">{ADD_DATABASE_MESSAGE}</span>
+							<!-- EDP: create_message_db -->
+							<!-- BDP: softwaredbuser_message -->
+							<span style="color:{STATUS_COLOR}">{SQLUSER_STATUS_MESSAGE}</span>
+							<!-- EDP: softwaredbuser_message -->
+						</td>
+					</tr>
+				</table>
+				<!-- EDP: require_installdb -->
+
+				<table class="firstColFixed">
+					<tr>
+						<th colspan="2">{TR_INSTALLATION_INFORMATION}</th>
+					</tr>
+					<tr>
+						<td><label for="install_username">{TR_INSTALL_USER}</label></td>
+						<td><input type="text" name="install_username" id="install_username" value="{VAL_INSTALL_USERNAME}"/></td>
+					</tr>
+					<tr>
+						<td><label for="install_password">{TR_INSTALL_PWD}</label></td>
+						<td><input type="password" name="install_password" id="install_password" value="{VAL_INSTALL_PASSWORD}"/></td>
+					</tr>
+					<tr>
+						<td><label for="install_email">{TR_INSTALL_EMAIL}</label></td>
+						<td><input type="text" name="install_email" id="install_email" value="{VAL_INSTALL_EMAIL}"/></td>
+					</tr>
+					<!-- EDP: software_item -->
+				</table>
+				<div class="buttons">
+					<input name="Submit" type="submit" class="button" onClick="MM_goToURL('parent','software.php');return document.MM_returnValue" value="{TR_BACK}"/>
+					<!-- BDP: software_install -->
+					<input name="Submit2" type="submit" class="button" value="{TR_INSTALL}"/>
+					<!-- EDP: software_install -->
+				</div>
+			</form>
