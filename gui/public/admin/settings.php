ad1428fd1d01bdf3c940ca7570122eadd79753f1
Sascha Bay <thecry@i-mscp.net>
2012-01-11 11:52:24 +0100

    GUI: Small fixes (Typo)

--- gui/public/admin/settings.php
+++ gui/public/admin/settings.php
@@ -635,7 +635,7 @@ $tpl->assign(
 		 'TR_MAX_DNAMES_LABELS' => tr('Maximal number of labels for domain names<br />(<small>Excluding SLD & TLD</small>)'),
 		 'TR_MAX_SUBDNAMES_LABELS' => tr('Maximum number of labels for subdomains'),
 		 'TR_PHPINI_BASE_SETTINGS' => tr('PHP Settings (system wide)'),
-		 'TR_PHPINI_ALLOW_URL_FOPEN' => tr('Value for the %s< directive', true, '<span class="bold">allow_url_fopen</span>'),
+		 'TR_PHPINI_ALLOW_URL_FOPEN' => tr('Value for the %s directive', true, '<span class="bold">allow_url_fopen</span>'),
 		 'TR_PHPINI_REGISTER_GLOBALS' => tr('Value for the %s directive', true, '<span class="bold">register_globals</span>'),
 		 'TR_PHPINI_DISPLAY_ERRORS' => tr('Value for the %s directive', true, '<span class="bold">display_errors</span>'),
 		 'TR_PHPINI_ERROR_REPORTING' => tr('Value for the %s directive', true, '<span class="bold">error_reporting</span>'),
