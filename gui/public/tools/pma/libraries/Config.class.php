971bcae2d6f83b75b8a04aebeb6b6cdbc3f72b6b
Torsten Widmann <goover@i-mscp.net>
2012-02-15 17:09:26 +0100

    Updated PMA to 3.4.10

--- gui/public/tools/pma/libraries/Config.class.php
+++ gui/public/tools/pma/libraries/Config.class.php
@@ -96,7 +96,7 @@ class PMA_Config
      */
     function checkSystem()
     {
-        $this->set('PMA_VERSION', '3.4.9');
+        $this->set('PMA_VERSION', '3.4.10');
         /**
          * @deprecated
          */
--- gui/public/tools/pma/libraries/export/php_array.php
+++ gui/public/tools/pma/libraries/export/php_array.php
@@ -87,7 +87,7 @@ function PMA_exportHeader()
  */
 function PMA_exportDBHeader($db)
 {
-    PMA_exportOutputHandler('//' . $GLOBALS['crlf'] . '// Database "' . $db . '"' . $GLOBALS['crlf'] . '//' . $GLOBALS['crlf']);
+    PMA_exportOutputHandler('//' . $GLOBALS['crlf'] . '// Database ' . PMA_backquote($db) . $GLOBALS['crlf'] . '//' . $GLOBALS['crlf']);
     return true;
 }
 
@@ -142,6 +142,19 @@ function PMA_exportData($db, $table, $crlf, $error_url, $sql_query)
     }
     unset($i);
 
+    // fix variable names (based on http://www.php.net/manual/language.variables.basics.php)
+    if (preg_match('/^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$/', $table) == false) {
+        // fix invalid chars in variable names by replacing them with underscores
+        $tablefixed = preg_replace('/[^a-zA-Z0-9_\x7f-\xff]/', '_', $table);
+
+        // variable name must not start with a number or dash...
+        if (preg_match('/^[a-zA-Z_\x7f-\xff]/', $tablefixed) == false) {
+            $tablefixed = '_' . $tablefixed;
+        }
+    } else {
+        $tablefixed = $table;
+    }
+
     $buffer = '';
     $record_cnt = 0;
     while ($record = PMA_DBI_fetch_row($result)) {
@@ -150,16 +163,15 @@ function PMA_exportData($db, $table, $crlf, $error_url, $sql_query)
 
         // Output table name as comment if this is the first record of the table
         if ($record_cnt == 1) {
-            $buffer .= $crlf . '// ' . $db . '.' . $table . $crlf;
-            $buffer .= '$' . $table . ' = array(' . $crlf;
+            $buffer .= $crlf . '// ' . PMA_backquote($db) . '.' . PMA_backquote($table) . $crlf;
+            $buffer .= '$' . $tablefixed . ' = array(' . $crlf;
             $buffer .= '  array(';
         } else {
             $buffer .= ',' . $crlf . '  array(';
         }
 
-
         for ($i = 0; $i < $columns_cnt; $i++) {
-            $buffer .= "'" . $columns[$i]. "'=>" . var_export($record[$i], true) . (($i + 1 >= $columns_cnt) ? '' : ',');
+            $buffer .= var_export($columns[$i], true) . " => " . var_export($record[$i], true) . (($i + 1 >= $columns_cnt) ? '' : ',');
         }
 
         $buffer .= ')';
