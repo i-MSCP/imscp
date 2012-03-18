1eae22ccc0ae8ddb5ae4699eb0a7bb2edffd31ca
Daniel Andreca <sci2tech@gmail.com>
2012-03-19 01:48:55 +0200

    Fixed #391: HELO-Mismatch (policyd-weight)

---
+++ engine/PerlLib/Addons/policyd.pm
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+# i-MSCP - internet Multi Server Control Panel
+# Copyright (C) 2010 - 2011 by internet Multi Server Control Panel
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# @category		i-MSCP
+# @copyright	2010 - 2012 by i-MSCP | http://i-mscp.net
+# @author		Daniel Andreca <sci2tech@gmail.com>
+# @version		SVN: $Id$
+# @link			http://i-mscp.net i-MSCP Home Site
+# @license		http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+
+package Addons::policyd;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use iMSCP::Debug;
+
+use vars qw/@ISA/;
+
+@ISA = ('Common::SingletonClass');
+use Common::SingletonClass;
+
+sub _init{
+
+	my $self				= shift;
+
+	$self->{cfgDir}	= "$main::imscpConfig{'CONF_DIR'}/policyd";
+	$self->{bkpDir}	= "$self->{cfgDir}/backup";
+	$self->{wrkDir}	= "$self->{cfgDir}/working";
+	$self->{tplDir}	= "$self->{cfgDir}/parts";
+
+	0;
+}
+
+sub factory{ return Addons::policyd->new(); }
+
+sub install{
+
+	use Addons::policyd::installer;
+
+	my $self	= shift;
+	my $rs		= 0;
+	Addons::policyd::installer->new()->install();
+
+	$rs;
+}
+
+1;
