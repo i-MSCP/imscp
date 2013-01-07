767557fc698bf2067336ea546b4c2ea620c137e9
Laurent Declercq <l.declercq@nuxwin.com>
2013-01-07 03:51:26 +0100

    Removed useless license headers in configuration files

--- configs/debian/database/spamassassin.sql
+++ configs/debian/database/spamassassin.sql
@@ -1,34 +1,3 @@
---
--- i-MSCP - internet Multi Server Control Panel
---
--- Copyright (C) 2006-2010 by isp Control Panel - http://ispcp.net
--- Copyright (C) 2010-2013 by internet Multi Server Control Panel - http://i-mscp.net
---
--- The contents of this file are subject to the Mozilla Public License
--- Version 1.1 (the "License"); you may not use this file except in
--- compliance with the License. You may obtain a copy of the License at
--- http://www.mozilla.org/MPL/
---
--- Software distributed under the License is distributed on an "AS IS"
--- basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
--- License for the specific language governing rights and limitations
--- under the License.
---
--- The Original Code is "ispCP ω (OMEGA) a Virtual Hosting Control Panel".
---
--- The Initial Developer of the Original Code is ispCP Team.
--- Portions created by Initial Developer are Copyright (C) 2006-2010 by
--- isp Control Panel. All Rights Reserved.
---
--- Portions created by the i-MSCP Team are Copyright (C) 2010-2013 by
--- internet Multi Server Control Panel. All Rights Reserved.
---
--- The i-MSCP Home Page is:
---
---    http://i-mscp.net
---
--- --------------------------------------------------------
-
 create database `{SPAMASSASSIN_DATABASE}` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 use `{SPAMASSASSIN_DATABASE}`;
