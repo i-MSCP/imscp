7f26a6c08ffa9293d1edc839c22a5a35ee158ffb
Laurent Declercq <l.declercq@nuxwin.com>
2013-10-02 11:56:36 +0200

    Added permanent redirect status code for SSL redirection (panel)

--- engine/PerlLib/Servers/httpd/apache_php_fpm/installer.pm
+++ engine/PerlLib/Servers/httpd/apache_php_fpm/installer.pm
@@ -828,7 +828,7 @@ sub _buildMasterVhostFiles
 						getBloc($customTagBegin, $customTagEnding, $$fileContent) .
 						"    RewriteEngine On\n" .
 						"    RewriteCond %{HTTPS} off\n" .
-						"    RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI}\n" .
+						"    RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R=301,L]\n" .
 						$customTagEnding;
 
 					$$fileContent = replaceBloc($customTagBegin, $customTagEnding, $customBlock, $$fileContent);
