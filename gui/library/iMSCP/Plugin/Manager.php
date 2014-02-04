4cfa4517baf21cb8e5cc4056bb76e62ea6d58ed3
Laurent Declercq <l.declercq@nuxwin.com>
2014-02-04 09:53:17 +0100

    Forgot to update plugin API (Ouch........)

--- gui/library/iMSCP/Plugin/Manager.php
+++ gui/library/iMSCP/Plugin/Manager.php
@@ -42,7 +42,7 @@ class iMSCP_Plugin_Manager
 	/**
 	 * @const string Plugin API version
 	 */
-	const PLUGIN_API_VERSION = '0.2.4';
+	const PLUGIN_API_VERSION = '0.2.5';
 
 	/**
 	 * @const int Action failure
