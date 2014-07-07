ee3849c76b8975fb692d17c73260a8d57d315a0e
vanliew <vanliew@adobe.com>
2014-07-07 19:20:04 +0200

    Adds tls option for SRV DNS records

--- gui/public/client/dns_edit.php
+++ gui/public/client/dns_edit.php
@@ -469,7 +469,7 @@ function client_generatePage($tpl, $dnsRecordId)
 			'DNS_ADDRESS' => tohtml(client_getPost('dns_A_address', $address)),
 			'DNS_ADDRESS_V6' => tohtml(client_getPost('dns_AAAA_address', $addressv6)),
 			'SELECT_DNS_SRV_PROTOCOL' => client_create_options(
-				array('tcp', 'udp'), client_getPost('srv_proto', $srvProto)
+				array('tcp', 'udp', 'tls'), client_getPost('srv_proto', $srvProto)
 			),
 			'DNS_SRV_NAME' => tohtml(client_getPost('dns_srv_name', decode_idna($srvName))),
 			'DNS_SRV_TTL' => tohtml(client_getPost('dns_srv_ttl', $srvTTL)),
