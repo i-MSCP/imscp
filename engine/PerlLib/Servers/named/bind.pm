076cacf8c427a756f1c16a16b0c3c2b9093dc072
Laurent Declercq <l.declercq@nuxwin.com>
2014-09-25 07:26:26 +0200

    Fixed: Wrong IP added in DNS zone files when the BASE_SERVER_IP and BASE_SERVER_PUBLIC_IP parameters are referring to the same IP (private IP)

--- engine/PerlLib/Servers/named/bind.pm
+++ engine/PerlLib/Servers/named/bind.pm
@@ -173,11 +173,11 @@ sub postaddDmn
 	return $rs if $rs;
 
 	if($self->{'config'}->{'BIND_MODE'} eq 'master') {
-		my $ipMngr = iMSCP::Net->getInstance();
-
-		my $domainIp = ($ipMngr->getAddrType($data->{'DOMAIN_IP'}) eq 'PUBLIC')
+		my $domainIp = ($main::imscpConfig{'BASE_SERVER_IP'} eq $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'})
 			? $data->{'DOMAIN_IP'}
 			: $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'};
+
+		my $ipMngr = iMSCP::Net->getInstance();
 
 		# Add DNS entry for domain alternative URL in master zone file
 		$rs = $self->addDmn(
@@ -359,11 +359,11 @@ sub addSub
 				$subEntry = replaceBloc("; sub SPF entry BEGIN\n", "; sub SPF entry ENDING\n", '', $subEntry);
 			}
 
-			my $ipMngr = iMSCP::Net->getInstance();
-
-			my $domainIp = ($ipMngr->getAddrType($data->{'DOMAIN_IP'}) eq 'PUBLIC')
+			my $domainIp = ($main::imscpConfig{'BASE_SERVER_IP'} eq $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'})
 				? $data->{'DOMAIN_IP'}
 				: $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'};
+
+			my $ipMngr = iMSCP::Net->getInstance();
 
 			# Process other entries
 			$subEntry = process(
@@ -450,11 +450,11 @@ sub postaddSub
 	return $rs if $rs;
 
 	if($self->{'config'}->{'BIND_MODE'} eq 'master') {
-		my $ipMngr = iMSCP::Net->getInstance();
-
-		my $domainIp = ($ipMngr->getAddrType($data->{'DOMAIN_IP'}) eq 'PUBLIC')
+		my $domainIp = (($main::imscpConfig{'BASE_SERVER_IP'} eq $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'}))
 			? $data->{'DOMAIN_IP'}
 			: $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'};
+
+		my $ipMngr = iMSCP::Net->getInstance();
 
 		# Adding DNS entry for subdomain alternative URL in master zone file
 		$rs = $self->addDmn(
@@ -928,9 +928,7 @@ sub _addDmnDb
 	my $dmnNsEntry = getBloc("; dmn NS entry BEGIN\n", "; dmn NS entry ENDING\n", $tplDbFileContent);
 	my $dmnNsAEntry = getBloc("; dmn NS A entry BEGIN\n", "; dmn NS A entry ENDING\n", $tplDbFileContent);
 
-	my $ipMngr = iMSCP::Net->getInstance();
-
-	my $domainIp = ($ipMngr->getAddrType($data->{'DOMAIN_IP'}) eq 'PUBLIC')
+	my $domainIp = (($main::imscpConfig{'BASE_SERVER_IP'} eq $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'}))
 		? $data->{'DOMAIN_IP'}
 		: $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'};
 
@@ -938,6 +936,8 @@ sub _addDmnDb
 		$domainIp,
 		($self->{'config'}->{'SECONDARY_DNS'} eq 'no') ? () : split ';', $self->{'config'}->{'SECONDARY_DNS'}
 	);
+
+	my $ipMngr = iMSCP::Net->getInstance();
 
 	my ($dmnNsEntries, $dmnNsAentries, $nsNumber) = (undef, undef, 1);
 
@@ -968,9 +968,7 @@ sub _addDmnDb
 	my $dmnMailEntry = '';
 
 	if($data->{'MAIL_ENABLED'}) {
-		my $baseServerIp = ($ipMngr->getAddrType($main::imscpConfig{'BASE_SERVER_IP'}) eq 'PUBLIC')
-			? $main::imscpConfig{'BASE_SERVER_IP'}
-			: $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'};
+		my $baseServerIp = $main::imscpConfig{'BASE_SERVER_PUBLIC_IP'};
 
 		$dmnMailEntry = process(
 			{
