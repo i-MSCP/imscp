df6550fb7073908a89c87bc6eb0fdf3e2bc28d62
Laurent Declercq <l.declercq@nuxwin.com>
2014-11-11 19:09:05 +0100

    Small fix (Moved maintenance mode translation string) (cherry picked from commit 22e544e) (cherry picked from commit d8108cf)

--- gui/public/index.php
+++ gui/public/index.php
@@ -101,7 +101,9 @@ if ($cfg['MAINTENANCEMODE'] && !isset($_REQUEST['admin'])) {
 			'TR_PAGE_TITLE' => tr('i-MSCP - Multi Server Control Panel / Maintenance'),
 			'CONTEXT_CLASS' => ' no_header',
 			'BOX_MESSAGE_TITLE' => tr('System under maintenance'),
-			'BOX_MESSAGE' => nl2br(tohtml($cfg['MAINTENANCEMODE_MESSAGE'])),
+			'BOX_MESSAGE' => (isset($cfg['MAINTENANCEMODE_MESSAGE']))
+				? nl2br(tohtml($cfg['MAINTENANCEMODE_MESSAGE']))
+				: tr("We are sorry, but the system is currently under maintenance.\nPlease try again later."),
 			'TR_BACK' => tr('Administrator login'),
 			'BACK_BUTTON_DESTINATION' => '/index.php?admin=1'
 		)
