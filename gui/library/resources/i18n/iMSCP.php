47f12e05970b49592c6ec6a5f884a3c53c62c17e
Laurent Declercq <l.declercq@nuxwin.com>
2014-11-11 13:51:56 +0100

    Added gui/library/resources/i18n/iMSCP.php file for indirect translation strings

---
+++ gui/library/resources/i18n/iMSCP.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * i-MSCP - internet Multi Server Control Panel
+ * Copyright (C) 2010-2014 by i-MSCP team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @category    iMSCP
+ * @package     iMSCP_Core
+ * @subpackage  I18n
+ * @copyright   2010-2014 by i-MSCP team
+ * @author      Laurent Declercq <laurent.declercq@i-mscp.net>
+ * @link        http://www.i-mscp.net i-MSCP Home Site
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL v2
+ */
+
+
+// Note this file is not intended to be loaded. It will simply be parsed by the gettext
+// tools when updating translation files.
+
+tr('_: Localised language');
+tr('Your i-MSCP version is not compatible with this plugin. Try with a newer version.');
