1b1cc9a18f26538a5db0d07bd2af17e1587923fb
Laurent Declercq <l.declercq@nuxwin.com>
2014-12-11 12:48:59 +0100

    Fixed: Unable to start the Ftp server - unknown protocol: 'TLSv1.1' ( ProFTPD ) Fixed: Unable to do any operation on the user web directory - ( immutable bit ) (cherry picked from commit 78bd761) (cherry picked from commit 9759b5f)

--- engine/PerlLib/Servers/httpd/apache_php_fpm.pm
+++ engine/PerlLib/Servers/httpd/apache_php_fpm.pm
@@ -1929,7 +1929,7 @@ sub _addFiles
 
 		if($data->{'WEB_FOLDER_PROTECTION'} eq 'yes') {
 			setImmutable($webDir);
-			setImmutable($parentDir) if $data->{'DOMAIN_TYPE'} ne 'dmn';
+			setImmutable($parentDir) if $parentDir ne $main::imscpConfig{'USER_WEB_DIR'};
 		}
 
 		# Permissions, owner and group - Ending
