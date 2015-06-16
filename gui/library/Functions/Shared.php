9f3f5444d88f5f69c61085fd1c7370ba97fc17d2
Laurent Declercq <l.declercq@nuxwin.com>
2015-06-16 17:39:02 +0200

    Fixed: Connection error message for the i-MSCP daemon is not clear enough (frontend)

--- gui/library/Functions/Shared.php
+++ gui/library/Functions/Shared.php
@@ -2660,7 +2660,9 @@ function send_request()
 
 		socket_close($socket);
 	} else {
-		write_log(sprintf('Unable to send request: %s', socket_strerror(socket_last_error())), E_USER_ERROR);
+		write_log(sprintf(
+			'Unable to connect to the i-MSCP daemon: %s', socket_strerror(socket_last_error())), E_USER_ERROR
+		);
 		$ret = false;
 	}
 
