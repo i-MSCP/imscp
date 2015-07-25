32d71e4bc4b1d8077c9c4042867b031463834a91
Laurent Declercq <l.declercq@nuxwin.com>
2015-07-25 22:14:44 +0200

    Added: Unit tests for the iMSCP::Dir library (In progress)

---
+++ engine/PerlLib/test/iMSCP/DirTests.pm
@@ -0,0 +1,245 @@
+package iMSCP::DirTests;
+
+use lib '/usr/local/src/imscp/engine/PerlLib';
+use Test::Simple tests => 20;
+use iMSCP::Dir;
+
+# TODO make this configurable
+my $assetsDir = '/usr/local/src/imscp/engine/PerlLib/test/TestAsset';
+
+#
+## iMSCP::Dir::getFiles() tests
+#
+
+sub getFilesDieOnMissingDirnameParameter
+{
+	local $@;
+	eval { iMSCP::Dir->new()->getFiles() };
+	$@ && $@ =~ /Missing 'dirname' parameter/;
+}
+
+sub getFilesDieIfCannotOpenDirname
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => "$assetsDir/tmp/inexistent" )->getFiles() };
+	$@ && $@ =~ /Could not open/;
+}
+
+sub getFilesReturnExpectedFilenames
+{
+	@expectedFileNames = ('f1.php', 'f2.txt', 'f3.txt');
+	@fileNames = iMSCP::Dir->new( dirname => "$assetsDir/files" )->getFiles();
+	@fileNames == @expectedFileNames;
+}
+
+sub getFilesReturnExpectedFilteredFileType
+{
+	@expectedFileNames = ('f2.txt', 'f3.txt');
+	@fileNames = iMSCP::Dir->new( dirname => "$assetsDir/files", fileType => '.txt' )->getFiles();
+	@fileNames == @expectedFileNames;
+}
+
+#
+## iMSCP::Dir::getDirs() tests
+#
+
+sub getDirsDieOnMissingDirnameParameter
+{
+	local $@;
+	eval { iMSCP::Dir->new()->getFiles(); };
+	$@ && $@ =~ /Missing 'dirname' parameter/;
+}
+
+sub getDirsDieIfCannotOpenDirname
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '$assetsDir/tmp/inexistent' )->getDirs() };
+	$@ && $@ =~ /Could not open/;
+}
+
+sub getDirsReturnExpectedDirnames
+{
+	@expectedDirnames = ('d1', 'd2', 'd3');
+	@dirnames = iMSCP::Dir->new( dirname => "$assetsDir/files" )->getDirs();
+	@dirnames == @expectedDirnames;
+}
+
+## iMSCP::Dir::getAll() tests
+
+sub getAllDieOnMissingDirnameParameter
+{
+	local $@;
+	eval { iMSCP::Dir->new()->getAll() };
+	$@ && $@ =~ /Missing 'dirname' parameter/;
+}
+
+sub getAllDieIfCannotOpenDirname
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '$assetsDir/tmp/inexistent' )->getAll() };
+	$@ && $@ =~ /Could not open/;
+}
+
+sub getAllReturnExpectedDirnames
+{
+	@expectedDirnames = ('f2.txt', 'd1', 'd3', 'f1.php', 'd2', 'f3.txt');
+	@dirnames = iMSCP::Dir->new( dirname => "$assetsDir/files" )->getAll();
+	@dirnames == @expectedDirnames;
+}
+
+#
+## iMSCP::Dir::isEmpty() tests
+#
+
+sub isEmptyDieOnMissingDirnameParameter
+{
+	local $@;
+	eval { iMSCP::Dir->new()->isEmpty() };
+	$@ && $@ =~ /Missing 'dirname' parameter/;
+}
+
+sub isEmptyDieIfCannotOpenDirname
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '$assetsDir/tmp/inexistent' )->isEmpty() };
+	$@ && $@ =~ /Could not open/;
+}
+
+sub isEmptyReturnFalseIfDirnameIsNotEmpty
+{
+	! iMSCP::Dir->new( dirname => "$assetsDir/files" )->isEmpty();
+}
+
+#
+## iMSCP::Dir::make() tests
+#
+
+sub makeDieOnMissingDirnameAttribute
+{
+	local $@;
+	eval { iMSCP::Dir->new()->make(); };
+	$@ && $@ =~ /Attribute 'dirname' is not defined/;
+}
+
+sub makeDieIfDirnameAlreadyExistsAsFile
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => "$assetsDir/tmp/dummy" )->make() };
+	$@ && $@ =~ /Already exists as file/;
+}
+
+sub makeCanCreateDir
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '/tmp/imscp_dir_test')->make() };
+	! $@
+}
+
+sub makeCanCreatePath
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '/tmp/imscp_dir_test/sub1/sub2')->make() };
+	unless($@) {
+		@expectedDirnames = ('sub1');
+		@dirnames = iMSCP::Dir->new( dirname => '/tmp/imscp_dir_test')->getDirs();
+
+		if(@dirnames == @expectedDirnames) {
+			@expectedDirnames = ('sub2');
+			@dirnames = iMSCP::Dir->new( dirname => '/tmp/imscp_dir_test/sub1')->getDirs();
+			@dirnames == @expectedDirnames;
+		} else {
+			0;
+		}
+	} else {
+		0;
+	}
+}
+
+#
+## iMSCP::Dir::remove() tests
+#
+
+sub removeDieOnMissingDirnameParameter
+{
+	local $@;
+	eval { iMSCP::Dir->new()->remove() };
+	$@ && $@ =~ /Missing 'dirname' parameter/;
+}
+
+sub removeCanRemoveDir
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '/tmp/imscp_dir_test/sub1/sub2')->remove() };
+	! $@
+}
+
+sub removeCanRemovePath
+{
+	local $@;
+	eval { iMSCP::Dir->new( dirname => '/tmp/imscp_dir_test')->remove() };
+	! $@
+}
+
+#
+## iMSCP::Dir::rcopy() tests
+#
+
+# TODO
+
+#
+## iMSCP::Dir::moveDir() tests
+#
+
+# TODO
+
+#
+## iMSCP::Dir::owner() tests
+#
+
+# TODO
+
+#
+## iMSCP::Dir::mode() tests
+#
+
+# TODO
+
+
+#
+# Run tests
+#
+
+sub runTests
+{
+	ok( getFilesDieOnMissingDirnameParameter(), 'iMSCP::Dir::getFiles() die on missing dirname parameter' );
+	ok( getFilesDieIfCannotOpenDirname(), 'iMSCP::Dir::getFiles() die if cannot open dirname' );
+	ok( getFilesReturnExpectedFilenames(), 'iMSCP::Dir::getFiles() return expected filenames' );
+	ok(getFilesReturnExpectedFilteredFileType(), 'iMSCP::Dir::getFiles() return expected filtered file type' );
+
+	ok( getDirsDieOnMissingDirnameParameter(), 'iMSCP::Dir::getDirs() die on missing dirname parameter' );
+	ok( getDirsDieIfCannotOpenDirname(), 'iMSCP::Dir::getDirs() die if cannot open dirname' );
+	ok( getDirsReturnExpectedDirnames(), 'iMSCP::Dir::getDirs() return expected dirnames' );
+
+	ok( getAllDieOnMissingDirnameParameter(), 'iMSCP::Dir::getAll() die on missing dirname parameter' );
+	ok( getAllDieIfCannotOpenDirname(), 'iMSCP::Dir::getAll() die if cannot open dirname' );
+	ok( getAllReturnExpectedDirnames(), 'iMSCP::Dir::getAll() return expected dirnames and filenames' );
+
+	ok( isEmptyDieOnMissingDirnameParameter(), 'iMSCP::Dir::isEmpty() die on missing dirname parameter' );
+	ok( isEmptyDieIfCannotOpenDirname(), 'iMSCP::Dir::isEmpty() die if cannot open dirname' );
+	ok( isEmptyReturnFalseIfDirnameIsNotEmpty(), 'iMSCP::Dir::isEmpty() return false if dirname is not empty' );
+
+	ok( makeDieOnMissingDirnameAttribute(), 'iMSCP::Dir::make() die on missing dirname attribute' );
+	ok( makeDieIfDirnameAlreadyExistsAsFile(), 'iMSCP::Dir::make() die if dirname already exists as file' );
+	ok( makeCanCreateDir(), 'iMSCP::Dir::make() can create dir' );
+	ok( makeCanCreatePath(), 'iMSCP::Dir::make() can create path' );
+
+	ok( removeDieOnMissingDirnameParameter(), 'iMSCP::Dir::remove() die on missing dirname parameter' );
+	ok( removeCanRemoveDir(), 'iMSCP::Dir::make() can remove dir' );
+	ok( removeCanRemovePath(), 'iMSCP::Dir::make() can remove path' );
+}
+
+# TODO Should be run from test executor
+runTests();
+
+1;
+__END__
