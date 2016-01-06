63fd9131fb099a08d17d639c426ca40f516dceda
Laurent Declercq <l.declercq@nuxwin.com>
2016-01-06 15:02:09 +0100

    Fixed: iMSCP::Rights::_set: cmod: cannot access '/etc/courier/dhparams.pem' : No such file or directory [ci skip]

--- engine/PerlLib/Servers/po/courier/installer.pm
+++ engine/PerlLib/Servers/po/courier/installer.pm
@@ -239,11 +239,15 @@ sub setEnginePermissions
 	});
 	return $rs if $rs;
 
-	setRights("$self->{'config'}->{'AUTHLIB_CONF_DIR'}/dhparams.pem", {
-		user => $self->{'config'}->{'AUTHDAEMON_USER'},
-		group => $main::imscpConfig{'ROOT_GROUP'},
-		mode => '0600'
-	});
+	if(-f "$self->{'config'}->{'AUTHLIB_CONF_DIR'}/dhparams.pem") {
+		setRights("$self->{'config'}->{'AUTHLIB_CONF_DIR'}/dhparams.pem", {
+			user => $self->{'config'}->{'AUTHDAEMON_USER'},
+			group => $main::imscpConfig{'ROOT_GROUP'},
+			mode => '0600'
+		});
+	}
+
+	0;
 }
 
 =head1 EVENT LISTENERS
