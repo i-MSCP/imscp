54e5cec17e127582d54176c6121e0eaf4f265a4a
Laurent Declercq <l.declercq@nuxwin.com>
2016-02-09 09:23:10 +0100

    Typo [ci skip]

--- gui/library/Functions/Reseller.php
+++ gui/library/Functions/Reseller.php
@@ -504,7 +504,7 @@ function reseller_limits_check($resellerId, $hp)
 
 	if ($maxMailLimit != 0) {
 		if ($newMailLimit == 0) {
-			set_page_message(tr('You have an mail account limit. You cannot add a user with unlimited email accounts.'), 'error');
+			set_page_message(tr('You have a mail account limit. You cannot add a user with unlimited email accounts.'), 'error');
 		} else if ($currentMailLimit + $newMailLimit > $maxMailLimit) {
 			set_page_message(tr('You are exceeding your email account limit.'), 'error');
 		}
