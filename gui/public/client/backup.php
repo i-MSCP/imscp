5da32146f2736f504914893cd6a236d0585ddaf7
Laurent Declercq <l.declercq@nuxwin.com>
2016-03-03 18:53:44 +0100

    CS fix [ci skip]

--- gui/public/client/backup.php
+++ gui/public/client/backup.php
@@ -97,7 +97,7 @@ $tpl->assign(
 		'TR_FTP_LOG_ON' => tr('Login with your FTP account'),
 		'TR_SWITCH_TO_BACKUP' => tr('Switch to the backups directory'),
 		'TR_DOWNLOAD_FILE' => tr('Download the archives stored in this directory'),
-		'TR_USUALY_NAMED' => is_null($name)? '' : tr('(usually named') . ' ' . tohtml($name) . ')',
+		'TR_USUALY_NAMED' => is_null($name) ? '' : tr('(usually named') . ' ' . tohtml($name) . ')',
 		'TR_RESTORE_BACKUP' => tr('Restore backup'),
 		'TR_RESTORE_DIRECTIONS' => tr('Click the Restore button and the system will restore the last daily backup'),
 		'TR_RESTORE' => tr('Restore'),
