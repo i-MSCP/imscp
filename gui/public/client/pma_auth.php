034e48282646eb815d292e81c211f87e6646e982
Laurent Declercq <l.declercq@nuxwin.com>
2016-04-06 14:16:14 +0200

    Fixed: PhpMyAdmin auto-login feature doesn't longer work with version 4.6.0 [ci skip]

--- gui/public/client/pma_auth.php
+++ gui/public/client/pma_auth.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * i-MSCP - internet Multi Server Control Panel
- * Copyright (C) 2010-2015 by Laurent Declercq <l.declercq@nuxwin.com>
+ * Copyright (C) 2010-2016 by Laurent Declercq <l.declercq@nuxwin.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -31,25 +31,18 @@
  */
 function _client_pmaGetLoginCredentials($dbUserId)
 {
-	$stmt = exec_query(
-		'
-			SELECT
-				sqlu_name, sqlu_pass
-			FROM
-				sql_user t1
-			INNER JOIN
-				domain t2 ON(t2.domain_admin_id = ?)
-			INNER JOIN
-				sql_database t3 ON(t3.domain_id = t2.domain_id)
-			WHERE
-				t1.sqld_id = t3.sqld_id
-			AND
-				t1.sqlu_id = ?
-		',
-		array((int)$_SESSION['user_id'], $dbUserId)
-	);
+    $stmt = exec_query(
+        '
+            SELECT sqlu_name, sqlu_pass FROM sql_user t1
+            INNER JOIN domain t2 ON(t2.domain_admin_id = ?)
+            INNER JOIN sql_database t3 ON(t3.domain_id = t2.domain_id)
+            WHERE t1.sqld_id = t3.sqld_id
+            AND t1.sqlu_id = ?
+        ',
+        array($_SESSION['user_id'], $dbUserId)
+    );
 
-	return $stmt->fetchRow(PDO::FETCH_NUM);
+    return $stmt->fetchRow(PDO::FETCH_NUM);
 }
 
 /**
@@ -61,9 +54,9 @@ function _client_pmaGetLoginCredentials($dbUserId)
  */
 function _client_pmaCreateCookies($cookies)
 {
-	foreach ((array)$cookies as $cookie) {
-		header("Set-Cookie: $cookie", false);
-	}
+    foreach ((array)$cookies as $cookie) {
+        header("Set-Cookie: $cookie", false);
+    }
 }
 
 /**
@@ -78,15 +71,13 @@ function _client_pmaCreateCookies($cookies)
  */
 function _client_pmaSetLanguage($location)
 {
-	/** @var Zend_Translate_Adapter $translator */
-	$translator = iMSCP_Registry::get('translator')->getAdapter();
-
-	$uriComponents = parse_url($location);
-	parse_str($uriComponents['query'], $queryParts);
-	$queryParts['lang'] = substr($translator->getLocale(), 0, 2);
-	$uriComponents['query'] = http_build_query($queryParts);
-
-	return http_build_url($location, $uriComponents);
+    /** @var Zend_Translate_Adapter $translator */
+    $translator = iMSCP_Registry::get('translator')->getAdapter();
+    $uriComponents = parse_url($location);
+    parse_str($uriComponents['query'], $queryParts);
+    $queryParts['lang'] = substr($translator->getLocale(), 0, 2);
+    $uriComponents['query'] = http_build_query($queryParts);
+    return http_build_url($location, $uriComponents);
 }
 
 /**
@@ -97,79 +88,74 @@ function _client_pmaSetLanguage($location)
  */
 function client_pmaAuth($dbUserId)
 {
-	$credentials = _client_pmaGetLoginCredentials($dbUserId);
+    $credentials = _client_pmaGetLoginCredentials($dbUserId);
 
-	if ($credentials) {
-		$postData = http_build_query(
-			array(
-				'pma_username' => $credentials[0],
-				'pma_password' => stripcslashes($credentials[1])
-			)
-		);
-	} else {
-		set_page_message(tr('Unknown SQL user'), 'error');
-		return false;
-	}
+    if (!$credentials) {
+        set_page_message(tr('Unknown SQL user'), 'error');
+        return false;
+    }
 
-	$contextOptions = array();
+    $postData = http_build_query(array(
+        'server' => 1,
+        'pma_username' => $credentials[0],
+        'pma_password' => stripcslashes($credentials[1]),
+        'token' => '',
+    ));
+    $contextOptions = array();
 
-	// Prepares PhpMyadmin absolute Uri to use
-	if (isSecureRequest()) {
-		$contextOptions = array(
-			'ssl' => array(
-				'verify_peer' => false,
-				'allow_self_signed' => true
-			)
-		);
-	}
+    // Prepares PhpMyadmin absolute Uri to use
+    if (isSecureRequest()) {
+        $contextOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'allow_self_signed' => true
+            )
+        );
+    }
 
-	$pmaBaseUrl = getBaseUrl() . '/pma/';
-	$port = getUriPort();
+    $pmaBaseUrl = getBaseUrl() . '/pma/';
+    $port = getUriPort();
+    $contextOptions = array_merge($contextOptions, array(
+        'http' => array(
+            'method' => 'POST',
+            'protocol_version' => '1.1',
+            'header' => array(
+                'Host: ' . $_SERVER['SERVER_NAME'] . (($port) ? ':' . $port : ''),
+                'Content-Type: application/x-www-form-urlencoded',
+                'Content-Length: ' . strlen($postData),
+                'User-Agent: i-MSCP',
+                'Connection: close',
+            ),
+            'content' => $postData,
+            'max_redirects' => 1
+        )
+    ));
 
-	$contextOptions = array_merge($contextOptions, array(
-		'http' => array(
-			'method' => 'POST',
-			'protocol_version' => '1.1',
-			'header' => array(
-				'Host: ' . $_SERVER['SERVER_NAME'] . (($port) ? ':' . $port : ''),
-				'Content-Type: application/x-www-form-urlencoded',
-				'Content-Length: ' . strlen($postData),
-				'User-Agent: i-MSCP',
-				'Connection: close',
-			),
-			'content' => $postData,
-			'max_redirects' => 1
-		)
-	));
+    stream_context_set_default($contextOptions);
 
-	stream_context_set_default($contextOptions);
+    // Gets the headers from PhpMyAdmin
+    $headers = get_headers($pmaBaseUrl, true);
+    if ($headers && isset($headers['Location'])) {
+        _client_pmaCreateCookies($headers['Set-Cookie']);
+        redirectTo(_client_pmaSetLanguage(str_replace('./index.php', '/pma/index.php', $headers['Location'])));
+    }
 
-	// Gets the headers from PhpMyAdmin
-	$headers = get_headers($pmaBaseUrl, true);
+    set_page_message(tr('An error occurred during authentication.'), 'error');
 
-	if ($headers && isset($headers['Location'])) {
-		_client_pmaCreateCookies($headers['Set-Cookie']);
-		redirectTo(_client_pmaSetLanguage($headers['Location']));
-	}
-
-	set_page_message(tr('An error occurred during authentication.'), 'error');
-
-	return false;
+    return false;
 }
 
 /***********************************************************************************************************************
  * Main
  */
 
-// Include core library
 require_once 'imscp-lib.php';
 
 iMSCP_Events_Aggregator::getInstance()->dispatch(iMSCP_Events::onClientScriptStart);
-
 check_login('user');
 
 if (!customerHasFeature('sql') || !isset($_GET['id'])) {
-	showBadRequestErrorPage();
+    showBadRequestErrorPage();
 } elseif (!client_pmaAuth((int)$_GET['id'])) {
-	redirectTo('sql_manage.php');
+    redirectTo('sql_manage.php');
 }
