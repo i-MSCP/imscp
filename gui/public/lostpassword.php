d30c8aef3691869b8da07ff2397ee00feb50aae9
Laurent Declercq <l.declercq@nuxwin.com>
2016-05-14 19:34:07 +0200

    CS fixes [ci skip]

--- gui/public/lostpassword.php
+++ gui/public/lostpassword.php
@@ -39,12 +39,12 @@ $cfg = iMSCP_Registry::get('config');
 
 // Lost password feature is disabled ?
 if (!$cfg['LOSTPASSWORD']) {
-	redirectTo('/index.php');
+    showNotFoundErrorPage();
 }
 
 // Check for gd library availability
 if (!check_gd()) {
-	throw new iMSCP_Exception(tr("PHP GD extension not loaded."));
+    throw new iMSCP_Exception(tr("PHP GD extension not loaded."));
 }
 
 // Remove old unique keys
@@ -52,62 +52,62 @@ removeOldKeys($cfg['LOSTPASSWORD_TIMEOUT']);
 
 $tpl = new iMSCP_pTemplate();
 $tpl->define_dynamic(array(
-	'layout' => 'shared/layouts/simple.tpl',
-	'page' => 'lostpassword.tpl',
-	'page_message' => 'layout'
+    'layout' => 'shared/layouts/simple.tpl',
+    'page' => 'lostpassword.tpl',
+    'page_message' => 'layout'
 ));
 
 $tpl->assign(array(
-	'TR_PAGE_TITLE' => tr('i-MSCP - Multi Server Control Panel / Lost Password'),
-	'CONTEXT_CLASS' => '',
-	'productLongName' => tr('internet Multi Server Control Panel'),
-	'productLink' => 'http://www.i-mscp.net',
-	'productCopyright' => tr('© 2010-2016 i-MSCP Team<br>All Rights Reserved'),
-	'TR_CAPCODE' => tr('Security code'),
-	'GET_NEW_IMAGE' => tr('Get a new image'),
-	'TR_IMGCAPCODE' => '<img id="captcha" src="imagecode.php" width="' . $cfg['LOSTPASSWORD_CAPTCHA_WIDTH'] .
-		'" height="' . $cfg['LOSTPASSWORD_CAPTCHA_HEIGHT'] . '" alt="captcha image" />',
-	'TR_USERNAME' => tr('Username'),
-	'TR_SEND' => tr('Send'),
-	'TR_CANCEL' => tr('Cancel')
+    'TR_PAGE_TITLE' => tr('i-MSCP - Multi Server Control Panel / Lost Password'),
+    'CONTEXT_CLASS' => '',
+    'productLongName' => tr('internet Multi Server Control Panel'),
+    'productLink' => 'http://www.i-mscp.net',
+    'productCopyright' => tr('© 2010-2016 i-MSCP Team<br>All Rights Reserved'),
+    'TR_CAPCODE' => tr('Security code'),
+    'GET_NEW_IMAGE' => tr('Get a new security code'),
+    'CAPTCHA_WIDTH' => $cfg['LOSTPASSWORD_CAPTCHA_WIDTH'],
+    'CAPTCHA_HEIGHT' => $cfg['LOSTPASSWORD_CAPTCHA_HEIGHT'],
+    'TR_USERNAME' => tr('Username'),
+    'TR_SEND' => tr('Send'),
+    'TR_CANCEL' => tr('Cancel')
 ));
 
-// A request for new password was validated ( User clicked on the link he has received by mail )
+// A request for new password was validated (user clicked on the link he has received by mail)
 if (isset($_GET['key']) && $_GET['key'] != '') {
-	// Check key
-	clean_input($_GET['key']);
+    // Check key
+    clean_input($_GET['key']);
 
-	// Sending new password
-	if (sendPassword($_GET['key'])) {
-		set_page_message(tr('Your new password has been sent. Check your email.'), 'success');
-		redirectTo('index.php');
-	} else {
-		set_page_message(tr('New password has not been sent. Ask your administrator.'), 'error');
-	}
+    // Sending new password
+    if (sendPassword($_GET['key'])) {
+        set_page_message(tr('Your new password has been sent. Check your email.'), 'success');
+        redirectTo('index.php');
+    }
+
+    set_page_message(tr('New password has not been sent. Ask your administrator.'), 'error');
 } elseif (!empty($_POST)) { // Request for new password
-	$bruteForce = new iMSCP_Plugin_Bruteforce(iMSCP_Registry::get('pluginManager'), 'captcha');
+    $bruteForce = new iMSCP_Plugin_Bruteforce(iMSCP_Registry::get('pluginManager'), 'captcha');
 
-	if ($bruteForce->isWaiting() || $bruteForce->isBlocked()) {
-		set_page_message($bruteForce->getLastMessage(), 'error');
-		redirectTo('lostpassword.php');
-	} else {
-		$bruteForce->recordAttempt();
-	}
+    if ($bruteForce->isWaiting() || $bruteForce->isBlocked()) {
+        set_page_message($bruteForce->getLastMessage(), 'error');
+        redirectTo('lostpassword.php');
+    }
 
-	if (!empty($_POST['uname']) && isset($_SESSION['image']) && isset($_POST['capcode'])) {
-		clean_input($_POST['uname']);
-		clean_input($_POST['capcode']);
+    $bruteForce->recordAttempt();
 
-		if ($_SESSION['image'] != $_POST['capcode']) {
-			set_page_message(tr('Wrong security code'), 'error');
-		} elseif (!requestPassword($_POST['uname'])) {
-			set_page_message(tr('Wrong username.'), 'error');
-		} else {
-			set_page_message(tr('Your request for new password has been registered. You will receive an email with instructions to complete the process.'), 'success');
-		}
-	} else {
-		set_page_message(tr('All fields are required.'), 'error');
-	}
+    if (!empty($_POST['uname']) && isset($_SESSION['image']) && isset($_POST['capcode'])) {
+        clean_input($_POST['uname']);
+        clean_input($_POST['capcode']);
+
+        if ($_SESSION['image'] != $_POST['capcode']) {
+            set_page_message(tr('Wrong security code'), 'error');
+        } elseif (!requestPassword($_POST['uname'])) {
+            set_page_message(tr('Wrong username.'), 'error');
+        } else {
+            set_page_message(tr('Your request for new password has been registered. You will receive an email with instructions to complete the process.'), 'success');
+        }
+    } else {
+        set_page_message(tr('All fields are required.'), 'error');
+    }
 }
 
 generatePageMessage($tpl);
--- gui/themes/default/lostpassword.tpl
+++ gui/themes/default/lostpassword.tpl
@@ -1,26 +1,28 @@
 
 <div id="login">
-	<form name="lostpasswordFrm" action="lostpassword.php" method="post">
-		<table>
-			<tr>
-				<td colspan="2" class="center">
-					<a href="lostpassword.php" title="{GET_NEW_IMAGE}">{TR_IMGCAPCODE}</a>
-				</td>
-			</tr>
-			<tr>
-				<td class="left"><label for="uname">{TR_USERNAME}</label></td>
-				<td class="right"><input type="text" name="uname" id="uname" tabindex="1"></td>
-			</tr>
-			<tr>
-				<td class="left"><label for="capcode">{TR_CAPCODE}</label></td>
-				<td class="right"><input type="text" name="capcode" id="capcode" tabindex="2"></td>
-			</tr>
-			<tr>
-				<td colspan="2" class="right">
-					<button name="submit" type="submit" tabindex="3">{TR_SEND}</button>
-					<a class="link_as_button" href="index.php" tabindex="4">{TR_CANCEL}</a>
-				</td>
-			</tr>
-		</table>
-	</form>
+    <form name="lostpasswordFrm" action="lostpassword.php" method="post">
+        <table>
+            <tr>
+                <td colspan="2" class="center">
+                    <a href="lostpassword.php" title="{GET_NEW_IMAGE}">
+                        <img id="captcha" src="imagecode.php" width="{CAPTCHA_WIDTH}" height="{CAPTCHA_HEIGHT}" alt="captcha image"/>
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td class="left"><label for="uname">{TR_USERNAME}</label></td>
+                <td class="right"><input type="text" name="uname" id="uname" tabindex="1"></td>
+            </tr>
+            <tr>
+                <td class="left"><label for="capcode">{TR_CAPCODE}</label></td>
+                <td class="right"><input type="text" name="capcode" id="capcode" tabindex="2"></td>
+            </tr>
+            <tr>
+                <td colspan="2" class="right">
+                    <button name="submit" type="submit" tabindex="3">{TR_SEND}</button>
+                    <a class="link_as_button" href="index.php" tabindex="4">{TR_CANCEL}</a>
+                </td>
+            </tr>
+        </table>
+    </form>
 </div>
