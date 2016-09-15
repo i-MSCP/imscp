5896abc7911ee0da4a4b2196ce534c41ff6e1b8c
Laurent Declercq <l.declercq@nuxwin.com>
2016-09-15 18:30:41 +0200

    #IP-1617 Fixed [ci skip]

--- gui/library/iMSCP/Uri/Redirect.php
+++ gui/library/iMSCP/Uri/Redirect.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * i-MSCP - internet Multi Server Control Panel
- * Copyright (C) 2010-2015 by Laurent Declercq <l.declercq@nuxwin.com>
+ * Copyright (C) 2010-2016 by Laurent Declercq <l.declercq@nuxwin.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,51 +23,29 @@
  */
 class iMSCP_Uri_Redirect extends Zend_Uri_Http
 {
-	/**
-	 * Creates a iMSCP_Uri_Redirect from the given string
-	 *
-	 * @param  string $uri String to create URI from, must start with prefix http://, https:// or 'ftp://
-	 * @throws iMSCP_Uri_Exception When the given URI is not a string or is not valid
-	 * @throws Zend_Uri_Exception
-	 * @return iMSCP_Uri_Redirect
-	 */
-	public static function fromString($uri)
-	{
-		if (is_string($uri) === false) {
-			throw new Zend_Uri_Exception(sprintf('%s is not a string', $uri));
-		}
+    /**
+     * Creates a iMSCP_Uri_Redirect from the given string
+     *
+     * @param  string $uri String to create URI from, must start with prefix http://, https:// or 'ftp://
+     * @throws iMSCP_Uri_Exception When the given URI is not a string or is not valid
+     * @throws Zend_Uri_Exception
+     * @return iMSCP_Uri_Redirect
+     */
+    public static function fromString($uri)
+    {
+        if (is_string($uri) === false) {
+            throw new Zend_Uri_Exception(sprintf('%s is not a string', $uri));
+        }
 
-		$uri = explode(':', $uri, 2);
-		$scheme = strtolower($uri[0]);
-		$schemeSpecific = isset($uri[1]) === true ? $uri[1] : '';
+        $uri = explode(':', $uri, 2);
+        $scheme = strtolower($uri[0]);
+        $schemeSpecific = isset($uri[1]) === true ? $uri[1] : '';
 
-		if (in_array($scheme, array('http', 'https', 'ftp')) === false) {
-			throw new iMSCP_Uri_Exception(sprintf('Invalid scheme: %s', $scheme));
-		}
+        if (in_array($scheme, array('http', 'https', 'ftp')) === false) {
+            throw new iMSCP_Uri_Exception(sprintf('Invalid scheme: %s', $scheme));
+        }
 
-		$schemeHandler = new iMSCP_Uri_Redirect($scheme, $schemeSpecific);
-		return $schemeHandler;
-	}
-
-	/**
-	 * Returns true if and only if the host string passes validation. If no host is passed, then the host contained in
-	 * the instance variable is used.
-	 *
-	 * @param  string $host The HTTP host
-	 * @return boolean
-	 * @uses   Zend_Filter
-	 */
-	public function validateHost($host = null)
-	{
-		if ($host === null) {
-			$host = $this->_host;
-		}
-
-		// If the host is empty, then it is considered invalid
-		if (strlen($host) === 0) {
-			return false;
-		}
-
-		return isValidDomainName($host);
-	}
+        $schemeHandler = new iMSCP_Uri_Redirect($scheme, $schemeSpecific);
+        return $schemeHandler;
+    }
 }
