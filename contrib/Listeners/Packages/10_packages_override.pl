8da22c41b6c1b546cf3f675e6d6edeed8fe08364
Ninos Ego <me@ninosego.de>
2016-09-16 03:36:18 +0200

    Added: packages override listener-file + debian jessie php7.0 package sample

---
+++ contrib/Listeners/Packages/10_packages_override.pl
@@ -0,0 +1,41 @@
+# i-MSCP Listener::Apache2::DualStack listener file
+# Copyright (C) 2015-2016 Laurent Declercq <l.declercq@nuxwin.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+#
+## Replace package file with custom one.
+#
+
+package Listener::Packages::Override;
+
+use strict;
+use warnings;
+use iMSCP::EventManager;
+
+# Path to your own package file
+my $DISTRO_PACKAGES_FILE = '/path/to/your/own/package/file';
+
+# Please don't edit anything below this line
+
+iMSCP::EventManager->getInstance()->register('onBuildPackageList', sub {
+	my $pkgFile = shift;
+	$$pkgFile = $DISTRO_PACKAGES_FILE;
+	0;
+});
+
+
+1;
+__END__
