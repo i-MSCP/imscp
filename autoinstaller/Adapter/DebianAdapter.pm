84c0d1d5e612ea3b321e47d559ad6fff8042dfbe
Laurent Declercq <l.declercq@nuxwin.com>
2016-11-09 11:40:05 +0100

    Fixed: Wrong lock file path

--- autoinstaller/Adapter/DebianAdapter.pm
+++ autoinstaller/Adapter/DebianAdapter.pm
@@ -313,7 +313,7 @@ sub _init
     $instance = $self;
 
     # Prevent dpkg post-invoke tasks to be triggered in setup context
-    my $dpkgLockFile = 'imscp-dpkg-post-invoke.lock';
+    my $dpkgLockFile = '/tmp/imscp-dpkg-post-invoke.lock';
     open $self->{'dpkg_lock_file'}, '>', $dpkgLockFile or die( sprintf( 'Could not open %s file', $dpkgLockFile ) );
     flock( $self->{'dpkg_lock_file'}, LOCK_EX ) or die( sprintf( 'Could not acquire exclusive lock on %s', $dpkgLockFile ) );
 
@@ -1084,13 +1084,13 @@ sub _rebuildAndInstallPackage
 =cut
 
 END {
-    debug( sprintf( 'Releasing exclusive lock on %s', 'imscp-dpkg-post-invoke.lock' ) );
+    debug( sprintf( 'Releasing exclusive lock on %s', '/tmp/imscp-dpkg-post-invoke.lock' ) );
     flock( $instance->{'dpkg_lock_file'}, LOCK_UN ) or die(
-        sprintf( 'Could not release exclusive lock on %s', 'imscp-dpkg-post-invoke.lock' )
+        sprintf( 'Could not release exclusive lock on %s', '/tmp/imscp-dpkg-post-invoke.lock' )
     );
     close $instance->{'dpkg_lock_file'};
     delete $instance->{'dpkg_lock_file'};
-    unlink 'imscp-dpkg-post-invoke.lock';
+    unlink '/tmp/imscp-dpkg-post-invoke.lock';
 }
 
 =back
