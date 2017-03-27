e1ec87dc9c09d4905be8ea330928407f93036479
Laurent Declercq <l.declercq@nuxwin.com>
2017-03-27 16:49:10 +0200

    Fixed: Vhost files for forwarded SSL domains are badly generated (Httpd server impl.)

--- engine/PerlLib/Servers/httpd/apache_php_fpm.pm
+++ engine/PerlLib/Servers/httpd/apache_php_fpm.pm
@@ -866,16 +866,20 @@ sub buildConf
 
     $data ||= { };
 
-    if (grep($_ eq $filename, ( 'domain.tpl', 'domain_disabled.tpl'))) {
-        if (grep($_ eq $data->{'VHOST_TYPE'}, ( 'domain', 'domain_disabled' ))) {
+    if (grep( $_ eq $filename, ( 'domain.tpl', 'domain_disabled.tpl') )) {
+        if (grep( $_ eq $data->{'VHOST_TYPE'}, ( 'domain', 'domain_disabled' ) )) {
             # Remove ssl and forward sections
             $cfgTpl = replaceBloc("# SECTION ssl BEGIN.\n", "# SECTION ssl END.\n", '', $cfgTpl);
             $cfgTpl = replaceBloc("# SECTION forward BEGIN.\n", "# SECTION forward END.\n", '', $cfgTpl);
-        } elsif (grep($_ eq $data->{'VHOST_TYPE'}, ('domain_fwd', 'domain_disabled_fwd'))) {
-            # Remove ssl and domain sections
-            $cfgTpl = replaceBloc("# SECTION ssl BEGIN.\n", "# SECTION ssl END.\n", '', $cfgTpl);
+        } elsif (grep( $_ eq $data->{'VHOST_TYPE'}, ( 'domain_fwd', 'domain_ssl_fwd', 'domain_disabled_fwd' ) )) {
+            # Remove ssl if needed
+            unless ($data->{'VHOST_TYPE'} eq 'domain_ssl_fwd') {
+                $cfgTpl = replaceBloc("# SECTION ssl BEGIN.\n", "# SECTION ssl END.\n", '', $cfgTpl);
+            }
+
+            # Remove domain section
             $cfgTpl = replaceBloc("# SECTION domain BEGIN.\n", "# SECTION domain END.\n", '', $cfgTpl);
-        } elsif (grep($_ eq $data->{'VHOST_TYPE'}, ('domain_ssl', 'domain_disabled_ssl'))) {
+        } elsif (grep( $_ eq $data->{'VHOST_TYPE'}, ( 'domain_ssl', 'domain_disabled_ssl' ) )) {
             # Remove forward section
             $cfgTpl = replaceBloc("# SECTION forward BEGIN.\n", "# SECTION forward END.\n", '', $cfgTpl);
         }
