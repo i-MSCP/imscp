4a48df8525a4939a4dd72542f6670d487c5a4e79
Laurent Declercq <l.declercq@nuxwin.com>
2017-04-30 02:47:34 +0200

    Fixed: Cannot find action... (Module::Plugin)

--- engine/PerlLib/Modules/Plugin.pm
+++ engine/PerlLib/Modules/Plugin.pm
@@ -427,8 +427,11 @@ sub _executePluginAction
         return 0 unless $self->{'pluginInstance'};
     }
 
+    my $sub = $self->{'pluginInstance'}->can( $action );
+    return 0 unless $sub;
+
     debug( sprintf( "Executing %s( ) action on %s", $action, ref $self->{'pluginInstance'} ) );
-    my $rs = eval { $self->{'pluginInstance'}->$action( $fromVersion, $toVersion ); };
+    my $rs = eval { $sub->($self->{'pluginInstance'}, $fromVersion, $toVersion ); };
     if ($@) {
         error( $@ );
         return 1;
