61802b5c62992ae953e6544f236ba34b5881bc5b
Laurent Declercq <l.declercq@nuxwin.com>
2017-12-11 12:28:23 +0100

    Added: Distribution and i-MSCP information in the system information page (admin UI)

--- gui/public/admin/system_info.php
+++ gui/public/admin/system_info.php
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-use iMSCP_Registry as Registry;
 use iMSCP\TemplateEngine;
+use iMSCP_Registry as Registry;
 
 /***********************************************************************************************************************
  * Main
@@ -33,10 +33,12 @@ Registry::get('iMSCP_Application')->getEventsManager()->dispatch(iMSCP_Events::o
 $tpl = new TemplateEngine();
 $tpl->define([
     'layout'       => 'shared/layouts/ui.tpl',
-    'page'         => 'admin/system_info.tpl',
+    'page'         => 'admin/system_info.phtml',
     'page_message' => 'layout',
     'device_block' => 'page'
 ]);
+
+$tpl->config = Registry::get('config');
 
 $sysinfo = new iMSCP_SystemInfo();
 
@@ -48,13 +50,13 @@ $tpl->assign([
     'CPU_BOGOMIPS'    => tohtml($sysinfo->cpu['bogomips']),
     'UPTIME'          => tohtml($sysinfo->uptime),
     'KERNEL'          => tohtml($sysinfo->kernel),
-    'LOAD'            => sprintf('%s %s %s', $sysinfo->load[0], $sysinfo->load[1], $sysinfo->load[2]),
-    'RAM_TOTAL'       => bytesHuman($sysinfo->ram['total'] * 1024),
-    'RAM_USED'        => bytesHuman($sysinfo->ram['used'] * 1024),
-    'RAM_FREE'        => bytesHuman($sysinfo->ram['free'] * 1024),
-    'SWAP_TOTAL'      => bytesHuman($sysinfo->swap['total'] * 1024),
-    'SWAP_USED'       => bytesHuman($sysinfo->swap['used'] * 1024),
-    'SWAP_FREE'       => bytesHuman($sysinfo->swap['free'] * 1024)
+    'LOAD'            => tohtml(sprintf('%s %s %s', $sysinfo->load[0], $sysinfo->load[1], $sysinfo->load[2])),
+    'RAM_TOTAL'       => tohtml(bytesHuman($sysinfo->ram['total'] * 1024)),
+    'RAM_USED'        => tohtml(bytesHuman($sysinfo->ram['used'] * 1024)),
+    'RAM_FREE'        => tohtml(bytesHuman($sysinfo->ram['free'] * 1024)),
+    'SWAP_TOTAL'      => tohtml(bytesHuman($sysinfo->swap['total'] * 1024)),
+    'SWAP_USED'       => tohtml(bytesHuman($sysinfo->swap['used'] * 1024)),
+    'SWAP_FREE'       => tohtml(bytesHuman($sysinfo->swap['free'] * 1024))
 ]);
 
 $devices = $sysinfo->filesystem;
@@ -64,43 +66,56 @@ foreach ($devices as $device) {
         'MOUNT'     => tohtml($device['mount']),
         'TYPE'      => tohtml($device['fstype']),
         'PARTITION' => tohtml($device['disk']),
-        'PERCENT'   => $device['percent'],
-        'FREE'      => bytesHuman($device['free'] * 1024),
-        'USED'      => bytesHuman($device['used'] * 1024),
-        'SIZE'      => bytesHuman($device['size'] * 1024)
+        'PERCENT'   => tohtml($device['percent']),
+        'FREE'      => tohtml(bytesHuman($device['free'] * 1024)),
+        'USED'      => tohtml(bytesHuman($device['used'] * 1024)),
+        'SIZE'      => tohtml(bytesHuman($device['size'] * 1024))
     ]);
     $tpl->parse('DEVICE_BLOCK', '.device_block');
 }
 
 $tpl->assign([
-    'TR_PAGE_TITLE'       => tr('Admin / System Tools / System Information'),
-    'TR_SYSTEM_INFO'      => tr('System data'),
-    'TR_KERNEL'           => tr('Kernel Version'),
-    'TR_UPTIME'           => tr('Uptime'),
-    'TR_LOAD'             => tr('Load (1 Min, 5 Min, 15 Min)'),
-    'TR_CPU_INFO'         => tr('CPU Information'),
-    'TR_CPU'              => tr('Processor data'),
-    'TR_CPU_MODEL'        => tr('Model'),
-    'TR_CPU_CORES'        => tr('Cores'),
-    'TR_CPU_CLOCK_SPEED'  => tr('Clock speed (MHz)'),
-    'TR_CPU_CACHE'        => tr('Cache'),
-    'TR_CPU_BOGOMIPS'     => tr('Bogomips'),
-    'TR_MEMORY_INFO'      => tr('Memory information'),
-    'TR_RAM'              => tr('RAM'),
-    'TR_TOTAL'            => tr('Total'),
-    'TR_USED'             => tr('Used'),
-    'TR_FREE'             => tr('Free'),
-    'TR_SWAP'             => tr('Swap'),
-    'TR_FILE_SYSTEM_INFO' => tr('Filesystem system Info'),
-    'TR_MOUNT'            => tr('Mount'),
-    'TR_TYPE'             => tr('Type'),
-    'TR_PARTITION'        => tr('Partition'),
-    'TR_PERCENT'          => tr('Percent'),
-    'TR_SIZE'             => tr('Size')
+    'TR_PAGE_TITLE'               => tohtml(tr('Admin / System Tools / System Information')),
+    'TR_DISTRIBUTION_INFO'        => tohtml(tr('Distribution')),
+    'TR_DISTRIBUTION_ID'          => tohtml(tr('ID')),
+    'TR_DISTRIBUTION_RELEASE'     => tohtml(tr('Release')),
+    'TR_DISTRIBUTION_CODENAME'    => tohtml(tr('Codename')),
+    'TR_iMSCP_INFO'               => tohtml(tr('i-MSCP Info')),
+    'TR_IMSCP_RELEASE'            => tohtml(tr('Release')),
+    'TR_IMSCP_CODENAME'           => tohtml(tr('Codename')),
+    'TR_IMSCP_PLUGIN_API_VERSION' => tohtml(tr('Plugin API version')),
+    'TR_IMSCP_HTTPD_SERVER'       => tohtml(tr('HTTPD')),
+    'TR_IMSCP_FTPD_SERVER'        => tohtml(tr('FTPD')),
+    'TR_IMSCP_MTA_SERVER'         => tohtml(tr('MTA')),
+    'TR_IMSCP_PHP_SERVER'         => tohtml(tr('PHP')),
+    'TR_IMSCP_PO_SERVER'          => tohtml(tr('IMAP/POP')),
+    'TR_IMSCP_SQL_SERVER'         => tohtml(tr('SQL')),
+    'TR_SYSTEM_INFO'              => tohtml(tr('System')),
+    'TR_KERNEL'                   => tohtml(tr('Kernel Version')),
+    'TR_UPTIME'                   => tohtml(tr('Uptime')),
+    'TR_LOAD'                     => tohtml(tr('Load (1 Min, 5 Min, 15 Min)')),
+    'TR_CPU_INFO'                 => tohtml(tr('Processor Info')),
+    'TR_CPU'                      => tohtml(tr('Processor')),
+    'TR_CPU_MODEL'                => tohtml(tr('Model')),
+    'TR_CPU_CORES'                => tohtml(tr('Cores')),
+    'TR_CPU_CLOCK_SPEED'          => tohtml(tr('Clock speed (MHz)')),
+    'TR_CPU_CACHE'                => tohtml(tr('Cache')),
+    'TR_CPU_BOGOMIPS'             => tohtml(tr('Bogomips')),
+    'TR_MEMORY_INFO'              => tohtml(tr('Memory Info')),
+    'TR_RAM'                      => tohtml(tr('Memory data')),
+    'TR_TOTAL'                    => tohtml(tr('Total')),
+    'TR_USED'                     => tohtml(tr('Used')),
+    'TR_FREE'                     => tohtml(tr('Free')),
+    'TR_SWAP'                     => tohtml(tr('Swap data')),
+    'TR_FILE_SYSTEM_INFO'         => tohtml(tr('Filesystem Info')),
+    'TR_MOUNT'                    => tohtml(tr('Mount point')),
+    'TR_TYPE'                     => tohtml(tr('Type')),
+    'TR_PARTITION'                => tohtml(tr('Partition')),
+    'TR_PERCENT'                  => tohtml(tr('Percent')),
+    'TR_SIZE'                     => tohtml(tr('Size'))
 ]);
 
-Registry::get('iMSCP_Application')->getEventsManager()->registerListener('onGetJsTranslations', function ($e) {
-    /* @var $e iMSCP_Events_Event */
+Registry::get('iMSCP_Application')->getEventsManager()->registerListener('onGetJsTranslations', function (iMSCP_Events_Event $e) {
     $e->getParam('translations')->core['dataTable'] = getDataTablesPluginTranslations();
 });
 
