3a1f46830a3e9f29c394462c61785eee96fe35c0
Laurent Declercq <l.declercq@nuxwin.com>
2017-12-28 18:28:53 +0100

    Remove SQL server -- Preparation for multi distribution support

---
+++ engine/PerlLib/Servers/sqld/Remote/Abstract.pm
@@ -0,0 +1,239 @@
+=head1 NAME
+
+ Servers::sqld::Remote::Abstract - i-MSCP Remote SQL server abstract implementation
+
+=cut
+
+# i-MSCP - internet Multi Server Control Panel
+# Copyright (C) 2010-2018 by Laurent Declercq <l.declercq@nuxwin.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+package Servers::sqld::Remote::Abstract;
+
+use strict;
+use warnings;
+use autouse 'iMSCP::Rights' => qw/ setRights /;
+use Class::Autouse qw/ :nostat iMSCP::Dir iMSCP::File /;
+use iMSCP::Database;
+use iMSCP::Debug qw/ error /;
+use version;
+use parent 'Servers::sqld::Mysql::Abstract';
+
+=head1 DESCRIPTION
+
+ i-MSCP Remote SQL server abstract implementation.
+
+=head1 PUBLIC METHODS
+
+=over 4
+
+=item postinstall( )
+
+ See Servers::sqld::Mysql::Abstract::postinstall()
+
+=cut
+
+sub postinstall
+{
+    my ($self) = @_;
+    
+    # Nothing to do here
+    0;
+}
+
+=item restart( )
+
+ See Servers::sqld::Mysql::Abstract::restart()
+
+=cut
+
+sub restart
+{
+    my ($self) = @_;
+
+    # Nothing to do here
+    0;
+}
+
+=item setEnginePermissions( )
+
+ See Servers::sqld::Mysql::Abstract::setEnginePermissions()
+
+=cut
+
+sub setEnginePermissions
+{
+    my ($self) = @_;
+
+    my $rs = setRights( "$self->{'config'}->{'SQLD_CONF_DIR'}/my.cnf",
+        {
+            user  => $main::imscpConfig{'ROOT_USER'},
+            group => $main::imscpConfig{'ROOT_GROUP'},
+            mode  => '0644'
+        }
+    );
+    $rs ||= setRights( "$self->{'config'}->{'SQLD_CONF_DIR'}/conf.d/imscp.cnf",
+        {
+            user  => $main::imscpConfig{'ROOT_USER'},
+            group => $main::imscpConfig{'ROOT_GROUP'},
+            mode  => '0640'
+        }
+    );
+}
+
+=item createUser( $user, $host, $password )
+
+ See Servers::sqld::Mysql::Abstract::createUser()
+
+=cut
+
+sub createUser
+{
+    my ($self, $user, $host, $password) = @_;
+
+    defined $user or die( '$user parameter is not defined' );
+    defined $host or die( '$host parameter is not defined' );
+    defined $password or die( '$password parameter is not defined' );
+
+    eval {
+        my $dbh = iMSCP::Database->getInstance()->getRawDb();
+        local $dbh->{'RaiseError'} = 1;
+        $dbh->do(
+            'CREATE USER ?@? IDENTIFIED BY ?'
+                . ( ( $self->getVendor() ne 'mariadb' && version->parse( $self->getVersion()) >= version->parse( '5.7.6' ) )
+                ? ' PASSWORD EXPIRE NEVER' : ''
+            ),
+            undef, $user, $host, $password
+        );
+    };
+    !$@ or die( sprintf( "Couldn't create the %s\@%s SQL user: %s", $user, $host, $@ ));
+    0;
+}
+
+=back
+
+=head1 PRIVATE METHODS
+
+=over 4
+
+=item _buildConf( )
+
+ See Servers::sqld::Mysql::Abstract::_buildConf()
+
+=cut
+
+sub _buildConf
+{
+    my ($self) = @_;
+
+    my $rs = $self->{'eventManager'}->trigger( 'beforeRemoteSqldBuildConf' );
+    return $rs if $rs;
+
+    eval {
+        # Make sure that the conf.d directory exists
+        iMSCP::Dir->new( dirname => "$self->{'config'}->{'SQLD_CONF_DIR'}/conf.d" )->make( {
+            user  => $main::imscpConfig{'ROOT_USER'},
+            group => $main::imscpConfig{'ROOT_GROUP'},
+            mode  => 0755
+        } );
+    };
+    if ( $@ ) {
+        error( $@ );
+        return 1;
+    }
+
+    # Create the /etc/mysql/my.cnf file if missing
+    unless ( -f "$self->{'config'}->{'SQLD_CONF_DIR'}/my.cnf" ) {
+        $rs = $self->{'eventManager'}->trigger( 'onLoadTemplate', 'remote_sqld', 'my.cnf', \ my $cfgTpl, {} );
+        return $rs if $rs;
+
+        unless ( defined $cfgTpl ) {
+            $cfgTpl = "!includedir $self->{'config'}->{'SQLD_CONF_DIR'}/conf.d/\n";
+        } elsif ( $cfgTpl !~ m%^!includedir\s+$self->{'config'}->{'SQLD_CONF_DIR'}/conf.d/\n%m ) {
+            $cfgTpl .= "!includedir $self->{'config'}->{'SQLD_CONF_DIR'}/conf.d/\n";
+        }
+
+        my $file = iMSCP::File->new( filename => "$self->{'config'}->{'SQLD_CONF_DIR'}/my.cnf" );
+        $file->set( $cfgTpl );
+        $rs = $file->save();
+        $rs ||= $file->owner( $main::imscpConfig{'ROOT_USER'}, $main::imscpConfig{'ROOT_GROUP'} );
+        $rs ||= $file->mode( 0644 );
+        return $rs if $rs;
+    }
+
+    $rs ||= $self->{'eventManager'}->trigger( 'onLoadTemplate', 'remote_sqld', 'imscp.cnf', \ my $cfgTpl, {} );
+    return $rs if $rs;
+
+    unless ( defined $cfgTpl ) {
+        $cfgTpl = iMSCP::File->new( filename => "$self->{'cfgDir'}/imscp.cnf" )->get();
+        unless ( defined $cfgTpl ) {
+            error( sprintf( "Couldn't read the %s file", "$self->{'cfgDir'}/imscp.cnf" ));
+            return 1;
+        }
+    }
+
+    my $file = iMSCP::File->new( filename => "$self->{'config'}->{'SQLD_CONF_DIR'}/conf.d/imscp.cnf" );
+    $file->set( $cfgTpl );
+    $rs = $file->save();
+    $rs ||= $file->owner( $main::imscpConfig{'ROOT_USER'}, $main::imscpConfig{'ROOT_GROUP'} );
+    $rs ||= $file->mode( 0644 );
+    $rs ||= $self->{'eventManager'}->trigger( 'afterRemoteSqldBuildConf' );
+}
+
+=item _updateServerConfig( )
+
+ See Servers::sqld::Mysql::Abstract::_updateServerConfig()
+
+=cut
+
+sub _updateServerConfig
+{
+    my ($self) = @_;
+
+    # FIXME
+    if ( $main::imscpConfig{'SQL_PACKAGE'} eq 'Servers::sqld::mariadb' ) {
+        return 0 if version->parse( "$self->{'config'}->{'SQLD_VERSION'}" ) < version->parse( '10.0' );
+    } elsif ( version->parse( "$self->{'config'}->{'SQLD_VERSION'}" ) < version->parse( '5.6.6' ) ) {
+        return 0;
+    }
+
+    eval {
+        my $dbh = iMSCP::Database->getInstance()->getRawDb();
+        local $dbh->{'RaiseError'};
+
+        # Disable unwanted plugins (bc reasons)
+        for ( qw/ cracklib_password_check simple_password_check validate_password / ) {
+            $dbh->do( "UNINSTALL PLUGIN $_" ) if $dbh->selectrow_hashref( "SELECT name FROM mysql.plugin WHERE name = '$_'" );
+        }
+    };
+    if ( $@ ) {
+        error( $@ );
+        return 1;
+    }
+
+    0;
+}
+
+=back
+
+=head1 AUTHOR
+
+ Laurent Declercq <l.declercq@nuxwin.com>
+
+=cut
+
+1;
+__END__
