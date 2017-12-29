b361b8717bc8d9e1f7a1b11f2eddd5e4aeb76514
Laurent Declercq <l.declercq@nuxwin.com>
2017-12-29 11:57:15 +0100

    Fixed: Wrong package names

--- engine/PerlLib/Servers/sqld/Mariadb/Debian.pm
+++ engine/PerlLib/Servers/sqld/Mariadb/Debian.pm
@@ -21,7 +21,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-package Servers::po::Dovecot::Debian;
+package Servers::po::Mariadb::Debian;
 
 use strict;
 use warnings;
