d66e2e6ebfb24715d4146bdec36a59318d4b5256
Laurent Declercq <l.declercq@nuxwin.com>
2019-04-15 02:09:07 +0200

    Bump required composer version (1.8.5) Typo

--- autoinstaller/Functions.pm
+++ autoinstaller/Functions.pm
@@ -91,7 +91,7 @@ sub loadConfig
         # Merge old configuration in new configuration, excluding upstream defined values
         while ( my ($key, $value) = each( %main::imscpOldConfig ) ) {
             next unless exists $::imscpConfig{$key};
-            next if $key =~ /^(?:Build|Version|CodeName|PluginApi|THEME_ASSETS_VERSION)$/;
+            next if $key =~ /^(?:Build|Version|CodeName|PluginApi|THEME_ASSETS_VERSION|COMPOSER_VERSION)$/;
             $::imscpConfig{$key} = $value;
         }
 
